695115362827d52e57672e2f16877b4f4ddb8ed1
tarzzz <tarun.gaba7@gmail.com>
2019-04-17 17:26:29 -0400

    Run black

--- predeploy.py
+++ predeploy.py
@@ -2,8 +2,8 @@ import os
 import shutil
 import subprocess
 
-files = ['requirements.txt', 'Procfile', 'DOKKU_SCALE']
+files = ["requirements.txt", "Procfile", "DOKKU_SCALE"]
 for f in files:
-    shutil.copyfile(os.path.join('apps', os.environ['DASH_APP_NAME'], f), f)
+    shutil.copyfile(os.path.join("apps", os.environ["DASH_APP_NAME"], f), f)
 
-subprocess.run('python -m pip install -r requirements.txt'.split(' '))
+subprocess.run("python -m pip install -r requirements.txt".split(" "))
