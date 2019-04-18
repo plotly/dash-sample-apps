e339e40006afcd58f5d82bc516691739332c610d
tarzzz <tarun.gaba7@gmail.com>
2019-04-18 16:12:50 -0400

    Fix deploy.

--- predeploy.py
+++ predeploy.py
@@ -8,7 +8,14 @@ from apps_directory_mapping import APPNAME_TO_DIRECTORY
 files = ["requirements.txt", "Procfile", "DOKKU_SCALE"]
 for f in files:
     shutil.copyfile(
-        os.path.join("apps", APPNAME_TO_DIRECTORY[os.environ["DASH_APP_NAME"]], f), f
+        os.path.join(
+            "apps",
+            APPNAME_TO_DIRECTORY.get(
+                os.environ["DASH_APP_NAME"], os.environ["DASH_APP_NAME"]
+            ),
+            f,
+        ),
+        f,
     )
 
 subprocess.run("python -m pip install -r requirements.txt".split(" "))
