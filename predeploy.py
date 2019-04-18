ecccb8e947cc635dc87e495b95e9d3863f851010
tarzzz <tarun.gaba7@gmail.com>
2019-04-18 15:51:44 -0400

    Try to get it working

--- predeploy.py
+++ predeploy.py
@@ -2,10 +2,19 @@ import os
 import shutil
 import subprocess
 
-files = ["requirements.txt", "Procfile", "DOKKU_SCALE"]
-for f in files:
-    app_file_path = os.path.join("apps", os.environ["DASH_APP_NAME"], f)
-    if os.path.exists(app_file_path):
-        shutil.copyfile(os.path.join("apps", os.environ["DASH_APP_NAME"], f), f)
+APPNAME_TO_DIRECTORY = {
+    'lastodash': 'lastodash',
+}
 
-subprocess.run("python -m pip install -r requirements.txt".split(" "))
+files = ['requirements.txt', 'Procfile', 'DOKKU_SCALE']
+for f in files:
+    shutil.copyfile(
+        os.path.join(
+            'apps',
+            APPNAME_TO_DIRECTORY[os.environ['DASH_APP_NAME']],
+            f
+        ),
+        f
+    )
+
+subprocess.run('python -m pip install -r requirements.txt'.split(' '))
