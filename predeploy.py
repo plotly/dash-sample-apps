480c5c270501ab1c0bcb640ffdcdcbde961e090d
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-05-10 14:49:28 -0400

    Modify predeploy to only use DOKKU_SCALE file if it exists.

--- predeploy.py
+++ predeploy.py
@@ -5,17 +5,19 @@ import subprocess
 from apps_directory_mapping import APPNAME_TO_DIRECTORY
 
 
-files = ["requirements.txt", "Procfile", "DOKKU_SCALE"]
-for f in files:
-    shutil.copyfile(
-        os.path.join(
-            "apps",
-            APPNAME_TO_DIRECTORY.get(
-                os.environ["DASH_APP_NAME"], os.environ["DASH_APP_NAME"]
-            ),
-            f,
-        ),
-        f,
+files = ["requirements.txt", "Procfile"]
+
+app_path = os.path.join(
+    'apps',
+    APPNAME_TO_DIRECTORY.get(
+        os.environ['DASH_APP_NAME'], os.environ['DASH_APP_NAME']
     )
+)
+
+for f in files:
+    shutil.copyfile(os.path.join(app_path, f), f)
+
+if "DOKKU_SCALE" in os.listdir(app_path):
+    shutil.copyfile(os.path.join(app_path, "DOKKU_SCALE"), "DOKKU_SCALE")
 
 subprocess.run("python -m pip install -r requirements.txt".split(" "))
