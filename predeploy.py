d38d528fae99398d10eb8b0997ad3e79f2dc16d5
Ryan Patrick Kyle <ryan@plot.ly>
2019-05-15 17:12:31 -0400

    :black_square: reformatted with black

--- predeploy.py
+++ predeploy.py
@@ -6,14 +6,11 @@ from apps_directory_mapping import APPNAME_TO_DIRECTORY
 
 
 files = ["requirements.txt", "Procfile"]
-rfiles = ["Aptfile", "init.R"] 
+rfiles = ["Aptfile", "init.R"]
 
 app_name = os.environ["DASH_APP_NAME"]
 
-app_path = os.path.join(
-    "apps",
-    APPNAME_TO_DIRECTORY.get(app_name, app_name),
-)
+app_path = os.path.join("apps", APPNAME_TO_DIRECTORY.get(app_name, app_name))
 
 for f in files:
     shutil.copyfile(os.path.join(app_path, f), f)
@@ -21,8 +18,8 @@ for f in files:
 if "DOKKU_SCALE" in os.listdir(app_path):
     shutil.copyfile(os.path.join(app_path, "DOKKU_SCALE"), "DOKKU_SCALE")
 
-if '-' in app_name and app_name.split('-')[0] == "dashr":
+if "-" in app_name and app_name.split("-")[0] == "dashr":
     for rfile in rfiles:
-        shutil.copyfile(os.path.join(app_path, rfile), rfile)    
+        shutil.copyfile(os.path.join(app_path, rfile), rfile)
 
 subprocess.run("python -m pip install -r requirements.txt".split(" "))
