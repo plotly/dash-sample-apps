6426a701e3ddb5ebf40f04ab4c35e40755fc1034
Ryan Patrick Kyle <rpkyle@users.noreply.github.com>
2019-05-15 17:19:51 -0400

    Update predeploy script (#83)
    * :hammer: update predeploy script to copy R files
    
    * :sparkles: create init.R, pin versions (#12825)
    
    * :sparkles: create Aptfile for R app system deps
    
    * reformatted with black

--- predeploy.py
+++ predeploy.py
@@ -6,16 +6,20 @@ from apps_directory_mapping import APPNAME_TO_DIRECTORY
 
 
 files = ["requirements.txt", "Procfile"]
+rfiles = ["Aptfile", "init.R"]
 
-app_path = os.path.join(
-    "apps",
-    APPNAME_TO_DIRECTORY.get(os.environ["DASH_APP_NAME"], os.environ["DASH_APP_NAME"]),
-)
+app_name = os.environ["DASH_APP_NAME"]
+
+app_path = os.path.join("apps", APPNAME_TO_DIRECTORY.get(app_name, app_name))
 
 for f in files:
     shutil.copyfile(os.path.join(app_path, f), f)
 
 if "DOKKU_SCALE" in os.listdir(app_path):
     shutil.copyfile(os.path.join(app_path, "DOKKU_SCALE"), "DOKKU_SCALE")
+
+if "-" in app_name and app_name.split("-")[0] == "dashr":
+    for rfile in rfiles:
+        shutil.copyfile(os.path.join(app_path, rfile), rfile)
 
 subprocess.run("python -m pip install -r requirements.txt".split(" "))
