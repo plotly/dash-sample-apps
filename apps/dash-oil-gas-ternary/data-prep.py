d4810d97358ba9987f3e980a9231a0fcdedfc156
Yunke Xiao <yunke0123@hotmail.com>
2019-05-16 17:47:53 -0400

    Black format applied

--- apps/dash-oil-gas-ternary/data-prep.py
+++ apps/dash-oil-gas-ternary/data-prep.py
@@ -6,30 +6,32 @@ df = pd.read_csv("data/EKY_shale/EKY_shale_production/Well_ID-Table_1.csv")
 df = df.applymap(lambda x: x.strip() if type(x) == str else x)
 
 # Drop non-gas well
-df = df[df['result'] == 'GAS']
+df = df[df["result"] == "GAS"]
 
 # Drop non-identified well
-ind = df[df['tdfm'] == '000'].index
+ind = df[df["tdfm"] == "000"].index
 df = df.drop(ind)
 
-form_id = df['tdfm'].unique()
+form_id = df["tdfm"].unique()
 
 # Replace form id to form name
 df_fm = pd.read_csv("data/EKY_shale/EKY_shale_production/FmCodes-Table_1.csv")
 df_fm = df_fm.applymap(lambda x: x.strip() if type(x) == str else x)
 
-df['fm_name'] = df['tdfm']
+df["fm_name"] = df["tdfm"]
 
 for formation in form_id:
-    formation_name = df_fm.loc[df_fm['fm_code'] == formation, 'fm_name'].item().strip()
-    df['fm_name'] = df['fm_name'].apply(lambda x: formation_name if x == formation else x)
+    formation_name = df_fm.loc[df_fm["fm_code"] == formation, "fm_name"].item().strip()
+    df["fm_name"] = df["fm_name"].apply(
+        lambda x: formation_name if x == formation else x
+    )
 
 # Generate random mineral composition for ternary plot
-ternary_ax_title = ['Quartz', 'Carbonate', 'Clay']
+ternary_ax_title = ["Quartz", "Carbonate", "Clay"]
 rand_composition = np.rint(np.random.dirichlet((10, 5, 3), len(df)) * 100).transpose()
 
-df['Quartz'] = rand_composition[0]
-df['Carbonate'] = rand_composition[1]
-df['Clay'] = rand_composition[2]
+df["Quartz"] = rand_composition[0]
+df["Carbonate"] = rand_composition[1]
+df["Clay"] = rand_composition[2]
 
-df.to_csv('test_composition.csv')
+df.to_csv("test_composition.csv")
