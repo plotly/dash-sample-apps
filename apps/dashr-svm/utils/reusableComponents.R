ff080e12a644015015ab9babd650703c9f6d21b3
Lucas Goldstone <lu.goldstone@gmail.com>
2019-05-16 15:21:25 -0400

    Add dashr-svm app

---
+++ apps/dashr-svm/utils/reusableComponents.R
@@ -0,0 +1,59 @@
+# Custom components to avoid too many repetitive inline style paramenters
+
+card <- function(children, ...){
+  htmlSection(
+    children,
+    style = list(
+      padding = "20",
+      margin = "5",
+      color = "#a5b1cd",
+      "user-select" = "none",
+      "-moz-user-select" = "none",
+      "-webkit-user-select" = "none",
+      "-ms-user-select" = "none"
+    )
+  )
+}
+
+namedDropdown <- function(name, ...){
+  htmlDiv(
+    style = list(margin = "10px 0px"),
+    children = list(
+      htmlP(
+        children = sprintf("%s:", name),
+        style = list(marginLeft = "3px")
+      ),
+      dccDropdown(...)
+    )
+  )
+}
+
+namedSlider <- function(name, ...){
+  htmlDiv(
+    style = list(padding = "20px 10px 25px 4px"),
+    children = list(
+      htmlP(sprintf("%s:", name)),
+      htmlDiv(
+        style = list(marginLeft = "6px"),
+        children = dccSlider(...)
+      )
+    )
+  )
+}
+
+formattedSlider <- function(style, ...){
+  htmlDiv(
+    style = style,
+    children = dccSlider(...)
+  )
+}
+
+namedRadioItems <- function(name, ...){
+  htmlDiv(
+    style = list(padding = "20px 10px 25px 4px"),
+    children = list(
+      htmlP(children = sprintf("%s:", name)),
+      dccRadioItems(...)
+    )
+  )
+}
