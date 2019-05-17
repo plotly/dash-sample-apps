a8fc568cd55bc62241b5dc1430bb87d22838083b
celinehuang <celine756@hotmail.com>
2019-05-17 12:24:49 -0400

    changed logo and removed files
    Former-commit-id: fcd6d000fe34acf1a5518cf5bcd605dd872fc942

--- apps/dash-tsne/demo.py
+++ apps/dash-tsne/demo.py
@@ -120,24 +120,23 @@ demo_layout = html.Div(
     children=[
         # Header
         html.Div(
-            className="row",
+            className="row header",
             children=[
-                html.H2("t-SNE Explorer", className="title", id="app-title"),
-                html.Img(
-                    id="plotly-image",
-                    src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/logo/new-branding/dash-logo-by-plotly-stripe.png",
-                    style={
-                        "height": "90px",
-                        "float": "right",
-                        "margin-top": "10px",
-                        "margin-right": "30px",
-                    },
-                ),
+                html.Div([
+                    html.Img(
+                        className="logo", id="plotly-image",
+                        src="/assets/dash-logo-stripe.png",
+                    ),
+                ], className="one-half column header__img"),
+                html.Div([
+                    html.H3("t-SNE Explorer",
+                            className="header__title", id="app-title"),
+                ], className="one-half column header__title__container"),
             ],
         ),
         # Demo Description
         html.Div(
-            className="row, background",
+            className="row background",
             id="learn-more-button",
             style={"padding": "50px 45px"},
             children=[
@@ -190,7 +189,8 @@ demo_layout = html.Div(
                                     max=1000,
                                     step=None,
                                     val=500,
-                                    marks={i: str(i) for i in [250, 500, 750, 1000]},
+                                    marks={i: str(i)
+                                           for i in [250, 500, 750, 1000]},
                                 ),
                                 NamedSlider(
                                     name="Perplexity",
@@ -199,7 +199,8 @@ demo_layout = html.Div(
                                     max=100,
                                     step=None,
                                     val=30,
-                                    marks={i: str(i) for i in [3, 10, 30, 50, 100]},
+                                    marks={i: str(i)
+                                           for i in [3, 10, 30, 50, 100]},
                                 ),
                                 NamedSlider(
                                     name="Initial PCA Dimensions",
@@ -217,7 +218,8 @@ demo_layout = html.Div(
                                     max=200,
                                     step=None,
                                     val=100,
-                                    marks={i: str(i) for i in [10, 50, 100, 200]},
+                                    marks={i: str(i)
+                                           for i in [10, 50, 100, 200]},
                                 ),
                                 html.Div(
                                     id="div-wordemb-controls",
@@ -241,7 +243,8 @@ demo_layout = html.Div(
                                         dcc.Dropdown(
                                             id="dropdown-word-selected",
                                             placeholder="Select word to display its neighbors",
-                                            style={"background-color": "#f2f3f4"},
+                                            style={
+                                                "background-color": "#f2f3f4"},
                                         ),
                                     ],
                                 ),
@@ -252,7 +255,8 @@ demo_layout = html.Div(
                 html.Div(
                     className="six columns",
                     children=[
-                        dcc.Graph(id="graph-3d-plot-tsne", style={"height": "98vh"})
+                        dcc.Graph(id="graph-3d-plot-tsne",
+                                  style={"height": "98vh"})
                     ],
                 ),
                 html.Div(
@@ -402,7 +406,8 @@ def demo_callbacks(app):
                 )
 
     @app.callback(
-        Output("div-wordemb-controls", "style"), [Input("dropdown-dataset", "value")]
+        Output("div-wordemb-controls",
+               "style"), [Input("dropdown-dataset", "value")]
     )
     def show_wordemb_controls(dataset):
         if dataset in WORD_EMBEDDINGS:
@@ -469,7 +474,8 @@ def demo_callbacks(app):
                 return go.Figure()
 
             # Plot layout
-            axes = dict(title="", showgrid=True, zeroline=False, showticklabels=False)
+            axes = dict(title="", showgrid=True,
+                        zeroline=False, showticklabels=False)
 
             layout = go.Layout(
                 margin=dict(l=0, r=0, b=0, t=0),
@@ -517,7 +523,8 @@ def demo_callbacks(app):
             path = f"demo_embeddings/{dataset}/iterations_{iterations}/perplexity_{perplexity}/pca_{pca_dim}/learning_rate_{learning_rate}"
 
             try:
-                embedding_df = pd.read_csv(path + f"/data.csv", encoding="ISO-8859-1")
+                embedding_df = pd.read_csv(
+                    path + f"/data.csv", encoding="ISO-8859-1")
 
             except FileNotFoundError as error:
                 print(
@@ -541,22 +548,26 @@ def demo_callbacks(app):
                 # Retrieve the image corresponding to the index
                 image_vector = data_dict[dataset].iloc[clicked_idx]
                 if dataset == "cifar_gray_3000":
-                    image_np = image_vector.values.reshape(32, 32).astype(np.float64)
+                    image_np = image_vector.values.reshape(
+                        32, 32).astype(np.float64)
                 else:
-                    image_np = image_vector.values.reshape(28, 28).astype(np.float64)
+                    image_np = image_vector.values.reshape(
+                        28, 28).astype(np.float64)
 
                 # Encode image into base 64
                 image_b64 = numpy_to_b64(image_np)
 
                 return html.Img(
                     src="data:image/png;base64, " + image_b64,
-                    style={"height": "25vh", "display": "block", "margin": "auto"},
+                    style={"height": "25vh",
+                           "display": "block", "margin": "auto"},
                 )
         return None
 
     @app.callback(
         Output("div-plot-click-wordemb", "children"),
-        [Input("graph-3d-plot-tsne", "clickData"), Input("dropdown-dataset", "value")],
+        [Input("graph-3d-plot-tsne", "clickData"),
+         Input("dropdown-dataset", "value")],
     )
     def display_click_word_neighbors(clickData, dataset):
         if dataset in WORD_EMBEDDINGS and clickData:
@@ -600,7 +611,8 @@ def demo_callbacks(app):
 
     @app.callback(
         Output("div-plot-click-message", "children"),
-        [Input("graph-3d-plot-tsne", "clickData"), Input("dropdown-dataset", "value")],
+        [Input("graph-3d-plot-tsne", "clickData"),
+         Input("dropdown-dataset", "value")],
     )
     def display_click_message(clickData, dataset):
         """
