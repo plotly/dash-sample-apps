bece303bcbf8fba65e3165ad34edaf91db6e6c39
Ryan Patrick Kyle <ryan@plot.ly>
2019-05-18 01:08:34 -0400

    test

--- apps/dashr-test/init.R
+++ apps/dashr-test/init.R
@@ -1,6 +1,7 @@
 # R script to run author supplied code, typically used to install additional R packages
 # contains placeholders which are inserted by the compile script
 # NOTE: this script is executed in the chroot context; check paths!
+#
 
 r <- getOption("repos")
 r["CRAN"] <- "http://cloud.r-project.org"
