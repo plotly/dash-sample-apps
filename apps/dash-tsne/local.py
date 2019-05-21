f642e5b1564e2ef4f9eef8a4481ac6709226c300
celinehuang <celine756@hotmail.com>
2019-05-21 09:57:36 -0400

    reformatted with Black
    Former-commit-id: 24a0c0ba84ed93b1e3cc63dae9af1b130982414a

--- apps/dash-tsne/local.py
+++ apps/dash-tsne/local.py
@@ -111,11 +111,9 @@ local_layout = html.Div(
                 html.Div(
                     [
                         # Data about the graph
-                        html.Div(id="kl-divergence",
-                                 style={"display": "none"}),
+                        html.Div(id="kl-divergence", style={"display": "none"}),
                         html.Div(id="end-time", style={"display": "none"}),
-                        html.Div(id="error-message",
-                                 style={"display": "none"}),
+                        html.Div(id="error-message", style={"display": "none"}),
                         # The graph
                         dcc.Graph(
                             id="tsne-3d-plot",
@@ -129,13 +127,11 @@ local_layout = html.Div(
                 html.Div(
                     [
                         html.H4("t-SNE Parameters", id="tsne_h4"),
-                        input_field("Perplexity:",
-                                    "perplexity-state", 20, 50, 5),
+                        input_field("Perplexity:", "perplexity-state", 20, 50, 5),
                         input_field(
                             "Number of Iterations:", "n-iter-state", 400, 1000, 250
                         ),
-                        input_field("Learning Rate:",
-                                    "lr-state", 200, 1000, 10),
+                        input_field("Learning Rate:", "lr-state", 200, 1000, 10),
                         input_field(
                             "Initial PCA dimensions:", "pca-state", 30, 10000, 3
                         ),
@@ -188,14 +184,12 @@ local_layout = html.Div(
                                 ),
                                 html.Div(
                                     id="training-status-message",
-                                    style={"margin-bottom": "0px",
-                                           "margin-top": "0px"},
+                                    style={"margin-bottom": "0px", "margin-top": "0px"},
                                 ),
                                 html.P(id="error-status-message"),
                             ],
                             id="output-messages",
-                            style={"margin-bottom": "2px",
-                                   "margin-top": "2px"},
+                            style={"margin-bottom": "2px", "margin-top": "2px"},
                         ),
                     ],
                     className="four columns",
@@ -411,11 +405,9 @@ def local_callbacks(app):
 
         return [
             # Data about the graph
-            html.Div([kl_divergence], id="kl-divergence",
-                     style={"display": "none"}),
+            html.Div([kl_divergence], id="kl-divergence", style={"display": "none"}),
             html.Div([end_time], id="end-time", style={"display": "none"}),
-            html.Div([error_message], id="error-message",
-                     style={"display": "none"}),
+            html.Div([error_message], id="error-message", style={"display": "none"}),
             # The graph
             dcc.Graph(
                 id="tsne-3d-plot",
@@ -455,8 +447,7 @@ def local_callbacks(app):
             ]
 
     @app.callback(
-        Output("error-status-message",
-               "children"), [Input("error-message", "children")]
+        Output("error-status-message", "children"), [Input("error-message", "children")]
     )
     def show_error_message(error_message):
         if error_message is not None:
