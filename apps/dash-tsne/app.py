c1b57ab4a89429798cee1f5d790a934a761afaf4
celinehuang <celine756@hotmail.com>
2019-05-28 13:50:16 -0400

    removed unnecessary css and code

--- apps/dash-tsne/app.py
+++ apps/dash-tsne/app.py
@@ -4,6 +4,7 @@ import dash
 
 from demo import create_layout, demo_callbacks
 
+# for the Local version, import local_layout and local_callbacks
 # from local import local_layout, local_callbacks
 
 app = dash.Dash(
