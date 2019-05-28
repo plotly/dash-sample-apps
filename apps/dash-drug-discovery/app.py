22130602818fcc4d573b1b69f7793e2cdf365f4e
Chris Woo <chriskfwoo@gmail.com>
2019-05-28 11:33:32 -0400

    refactor plot functions, pydocs

--- apps/dash-drug-discovery/app.py
+++ apps/dash-drug-discovery/app.py
@@ -6,6 +6,8 @@ import dash_core_components as dcc
 
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
+from helpers import make_dash_table, create_plot
+
 
 app = dash.Dash(
     __name__,
@@ -21,176 +23,17 @@ df = pd.read_csv(DATA_PATH.joinpath("small_molecule_drugbank.csv")).drop(
     ["Unnamed: 0"], axis=1
 )
 
-
-def add_markers(figure_data, molecules, plot_type="scatter3d"):
-    indices = []
-    drug_data = figure_data[0]
-    for m in molecules:
-        hover_text = drug_data["text"]
-        for i in range(len(hover_text)):
-            if m == hover_text[i]:
-                indices.append(i)
-
-    if plot_type == "histogram2d":
-        plot_type = "scatter"
-
-    traces = []
-    for point_number in indices:
-        trace = dict(
-            x=[drug_data["x"][point_number]],
-            y=[drug_data["y"][point_number]],
-            marker=dict(color="red", size=16, opacity=0.6, symbol="cross"),
-            type=plot_type,
-        )
-
-        if plot_type == "scatter3d":
-            trace["z"] = [drug_data["z"][point_number]]
-
-        traces.append(trace)
-
-    return traces
-
-
-BACKGROUND = "rgb(230, 230, 230)"
-
-COLORSCALE = [
-    [0, "rgb(244,236,21)"],
-    [0.3, "rgb(249,210,41)"],
-    [0.4, "rgb(134,191,118)"],
-    [0.5, "rgb(37,180,167)"],
-    [0.65, "rgb(17,123,215)"],
-    [1, "rgb(54,50,153)"],
-]
-
-
-def scatter_plot_3d(
+STARTING_DRUG = "Levobupivacaine"
+DRUG_DESCRIPTION = df.loc[df["NAME"] == STARTING_DRUG]["DESC"].iloc[0]
+DRUG_IMG = df.loc[df["NAME"] == STARTING_DRUG]["IMG_URL"].iloc[0]
+FIGURE = create_plot(
     x=df["PKA"],
     y=df["LOGP"],
     z=df["SOL"],
     size=df["MW"],
     color=df["MW"],
-    xlabel="LogP",
-    ylabel="pkA",
-    zlabel="Solubility (mg/ml)",
-    plot_type="scatter3d",
-    markers=[],
-):
-    def axis_template_3d(title, type="linear"):
-        return dict(
-            showbackground=True,
-            backgroundcolor=BACKGROUND,
-            gridcolor="rgb(255, 255, 255)",
-            title=title,
-            type=type,
-            zerolinecolor="rgb(255, 255, 255)",
-        )
-
-    def axis_template_2d(title):
-        return dict(
-            xgap=10,
-            ygap=10,
-            backgroundcolor=BACKGROUND,
-            gridcolor="rgb(255, 255, 255)",
-            title=title,
-            zerolinecolor="rgb(255, 255, 255)",
-            color="#444",
-        )
-
-    def blackout_axis(axis):
-        axis["showgrid"] = False
-        axis["zeroline"] = False
-        axis["color"] = "white"
-        return axis
-
-    data = [
-        dict(
-            x=x,
-            y=y,
-            z=z,
-            mode="markers",
-            marker=dict(
-                colorscale=COLORSCALE,
-                colorbar={"title": "Molecular<br>Weight"},
-                line=dict(color="#444"),
-                reversescale=True,
-                sizeref=45,
-                sizemode="diameter",
-                opacity=0.7,
-                size=size,
-                color=color,
-            ),
-            text=df["NAME"],
-            type=plot_type,
-        )
-    ]
-
-    layout = dict(
-        font=dict(family="Raleway"),
-        hovermode="closest",
-        margin=dict(r=20, t=0, l=0, b=0),
-        showlegend=False,
-        scene=dict(
-            xaxis=axis_template_3d(xlabel),
-            yaxis=axis_template_3d(ylabel),
-            zaxis=axis_template_3d(zlabel, "log"),
-            camera=dict(
-                up=dict(x=0, y=0, z=1),
-                center=dict(x=0, y=0, z=0),
-                eye=dict(x=0.08, y=2.2, z=0.08),
-            ),
-        ),
-    )
-
-    if plot_type in ["histogram2d", "scatter"]:
-        layout["xaxis"] = axis_template_2d(xlabel)
-        layout["yaxis"] = axis_template_2d(ylabel)
-        layout["plot_bgcolor"] = BACKGROUND
-        layout["paper_bgcolor"] = BACKGROUND
-        del layout["scene"]
-        del data[0]["z"]
-
-    if plot_type == "histogram2d":
-        # Scatter plot overlay on 2d Histogram
-        data[0]["type"] = "scatter"
-        data.append(
-            dict(x=x, y=y, type="histogram2d", colorscale="Greys", showscale=False)
-        )
-        layout["plot_bgcolor"] = "black"
-        layout["paper_bgcolor"] = "black"
-        layout["xaxis"] = blackout_axis(layout["xaxis"])
-        layout["yaxis"] = blackout_axis(layout["yaxis"])
-        layout["font"]["color"] = "white"
-
-    if len(markers) > 0:
-        data = data + add_markers(data, markers, plot_type=plot_type)
-
-    return dict(data=data, layout=layout)
-
-
-def make_dash_table(selection):
-    """ Return a dash defintion of an HTML table for a Pandas dataframe. """
-
-    df_subset = df.loc[df["NAME"].isin(selection)]
-    table = []
-
-    for index, row in df_subset.iterrows():
-        rows = []
-        rows.append(html.Td([row["NAME"]]))
-        rows.append(html.Td([html.Img(src=row["IMG_URL"])]))
-        rows.append(html.Td([row["FORM"]]))
-        rows.append(
-            html.Td([html.A(href=row["PAGE"], children="Datasheet", target="_blank")])
-        )
-        table.append(html.Tr(rows))
-
-    return table
-
-
-FIGURE = scatter_plot_3d()
-STARTING_DRUG = "Levobupivacaine"
-DRUG_DESCRIPTION = df.loc[df["NAME"] == STARTING_DRUG]["DESC"].iloc[0]
-DRUG_IMG = df.loc[df["NAME"] == STARTING_DRUG]["IMG_URL"].iloc[0]
-
+    name=df["NAME"],
+)
 
 app.layout = html.Div(
     [
@@ -294,7 +137,7 @@ app.layout = html.Div(
                 html.Div(
                     [
                         html.Table(
-                            make_dash_table([STARTING_DRUG]),
+                            make_dash_table([STARTING_DRUG], df),
                             id="table-element",
                             className="table__container",
                         )
@@ -332,7 +175,16 @@ def highlight_molecule(chem_dropdown_values, plot_type):
     :params plot_type: selected plot graph
     """
 
-    return scatter_plot_3d(markers=chem_dropdown_values, plot_type=plot_type)
+    return create_plot(
+        x=df["PKA"],
+        y=df["LOGP"],
+        z=df["SOL"],
+        size=df["MW"],
+        color=df["MW"],
+        name=df["NAME"],
+        markers=chem_dropdown_values,
+        plot_type=plot_type,
+    )
 
 
 @app.callback(Output("table-element", "children"), [Input("chem_dropdown", "value")])
@@ -343,7 +195,7 @@ def update_table(chem_dropdown_value):
     :params chem_dropdown_values: selected dropdown values
     """
 
-    return make_dash_table(chem_dropdown_value)
+    return make_dash_table(chem_dropdown_value, df)
 
 
 @app.callback(
@@ -356,7 +208,7 @@ def update_table(chem_dropdown_value):
     [Input("clickable-graph", "hoverData")],
 )
 def chem_info_on_hover(hoverData):
-    """ 
+    """
     Display chemical information on graph hover.
     Update the image, link, description.
 
