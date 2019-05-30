686e00281e7b5138e2b1d8d0b49ca3fea45db0bd
Lucas Goldstone <lu.goldstone@gmail.com>
2019-05-30 14:24:25 -0400

    Modify init.R to redeploy

--- apps/dashr-svm/init.R
+++ apps/dashr-svm/init.R
@@ -44,10 +44,10 @@ install.packages("https://cloud.r-project.org/src/contrib/Archive/future/future_
 install.packages("https://cloud.r-project.org/src/contrib/routr_0.3.0.tar.gz", type="source", repos=NULL)
 install.packages("https://cloud.r-project.org/src/contrib/fiery_1.1.1.tar.gz", type="source", repos=NULL)
 
-remotes::install_github("plotly/dashR", dependencies=FALSE)
-remotes::install_github("plotly/dash-html-components")
-remotes::install_github("plotly/dash-core-components")
-
 install.packages("caret")
 install.packages("kernlab")
 install.packages("ROCR")
+
+remotes::install_github("plotly/dashR", dependencies=FALSE)
+remotes::install_github("plotly/dash-html-components")
+remotes::install_github("plotly/dash-core-components")
