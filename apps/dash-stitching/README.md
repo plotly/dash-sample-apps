97862ddc4b700b2c88a518616a5317c0bd4b3f21
Chris Jeon <cjeon97@gmail.com>
2019-05-30 15:16:36 -0400

    Added real source code

---
+++ apps/dash-stitching/README.md
@@ -0,0 +1,13 @@
+# A Dash app for stitching images
+
+<p style="text-align:center;"><img src="assets/stitching.gif" alt="Segmentation app" width="65%"></p>
+
+Install requirements:
+
+``pip install -r requirements.txt``
+
+Run the app:
+
+``python app.py``
+
+(and visit your navigator as instructed).
---
+++ apps/dash-stitching/utils.py
@@ -0,0 +1,13 @@
+import os
+from textwrap import dedent
+
+
+def StaticUrlPath(resource):
+    if not os.path.exists(os.path.join('static', resource)):
+        raise Exception(dedent('''
+            The file "{}" does not exist in the "static" folder.
+        '''.format(resource, resource)))
+    if 'DYNO' in os.environ:
+        return '/{}/static/{}'.format(os.environ['DASH_APP_NAME'], resource)
+    else:
+        return '/static/{}'.format(resource)
