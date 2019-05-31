4bee22c4c8cc0a41304768242a1e3a5c6ee726d3
Dan Yunheum Seol <yunheum.seol@mail.mcgill.ca>
2019-05-31 12:35:06 -0400

    Installing dashplayer

--- apps/dashr-object-detection/init.R
+++ apps/dashr-object-detection/init.R
@@ -10,6 +10,7 @@
 # require(data.table)
 # require(glue)
 # require(compiler)
+install.packages("dashPlayer_0.0.1.tar.gz", type="source", repos=NULL)
 install.packages(c('tidyverse', 'dashR', 'dashCoreComponents', 'dashHtmlComponents', 'plotly', 'rlist', 'stringr', 'data.table', 'glue'))
 
 ##DashPlayer- install the local version inside the repo
