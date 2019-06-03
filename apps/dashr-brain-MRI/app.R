fcc6cfb791f417a383c312dea0deb9415b3127fc
KPhans <kevinphan@plot.ly>
2019-06-03 16:06:20 -0400

    Inline CSS Restyle. Mobile coming soon.

--- apps/dashr-brain-MRI/app.R
+++ apps/dashr-brain-MRI/app.R
@@ -171,9 +171,7 @@ plot_layout = list(
   title = '',
   margin=list(t=0,b=0,l=0,r=0),
   font=list(size=12, color='white'),
-  width='700',
-  height='700',
-  autosize=TRUE,
+  autoresize=TRUE,
   showlegend=FALSE,
   plot_bgcolor='black',
   paper_bgcolor='black',
@@ -182,9 +180,7 @@ plot_layout = list(
              zaxis=axis_template,
              aspectratio=list(x=1, y=1.2, z=1),
              camera=list(eye=list(x=1.25, y=1.25, z=1.25)),
-             annotations=list()
-  )
-)
+             annotations=list()))
 
 styles = list(
   'pre'= list(
@@ -200,94 +196,137 @@ styles = list(
 
 #APP LAYOUT*************************
 
-app <- Dash$new(external_stylesheets = 'https://codepen.io/plotly/pen/YeqjLb.css')
-
 app$layout(htmlDiv(children = list(
+  
+  
+  
+  htmlDiv(list(
+    htmlImg(src="https://plotly.cdn.prismic.io/plotly/41a4661495f29a955d38a998e2d3de914cbe9bd3_dash-logo-stripe.png", "height"="43px"),
+    htmlP('MRI Reconstruction', style = list(color = '#506784', fontSize = '29px', fontFamily = 'Script'))
+  ), style=list("float"="top", 'display' = "flex")),
+  
+  
   htmlP(
-    children = list('Click on the brain to add an annotation.
-                    Drag the black corners of the graph to rotate.',
+    children = list(htmlP('Click on the brain to add an annotation.
+                          Drag the black corners of the graph to rotate.',
+                          style = list(fontFamily = 'Trebuchet MS')),
+                    htmlBr(),
                     htmlA(
-                      children= 'GitHub',
-                      target= '_blank',
+                      children= 'Learn More',
                       href= 'https://github.com/plotly/dash-brain-surface-viewer',
-                      style=list('color' = '#F012BE')
+                      style=list('color' = 'white',fontSize = '13px',
+                                 'text-decoration' = 'None',
+                                 fontFamily = 'Trebuchet MS',
+                                 border = 'solid',
+                                 'border-width' = '0.1px',
+                                 padding = '15px',
+                                 'margin-right' = '30px')
+                    ),
+                    
+                    htmlA(
+                      children= 'View On Github',
+                      href= 'https://github.com/plotly/dash-brain-surface-viewer',
+                      style=list('color' = 'white',fontSize = '13px',
+                                 'text-decoration' = 'None',
+                                 fontFamily = 'Trebuchet MS',
+                                 border = 'solid',
+                                 'border-width' = '0.1px',
+                                 padding = '15px')
                     )
                     
     )
-),
-htmlDiv(list(htmlP(
-  children= 'Click colorscale to change:',
-  style=list('display' ='inline-block', 'fontSize' ='12px')
-),
-htmlDiv(list(
-  dashColorscales(
-    id = 'colorscale-picker',
-    colorscale = DEFAULT_COLORSCALE_NO_INDEX)),
+    ),
+  htmlBr(),
+  
+  
+  htmlDiv(list(htmlP(
+    children= 'Click colorscale to change:',
+    style=list('display' ='inline-block', 'fontSize' ='12px')
+  ),
+  htmlDiv(list(
+    dashColorscales(
+      id = 'colorscale-picker',
+      colorscale = DEFAULT_COLORSCALE_NO_INDEX)
+  ),
   style = list('marginTop' = '-15px', 'marginLeft' = '-30px')
-),
-htmlDiv(list(dccRadioItems(
-  options=list(
-    list('label' = 'Cortical Thickness', 'value' = 'human'),
-    list('label' = 'Mouse Brain', 'value' = 'mouse'),
-    list('label' = 'Brain Atlas', 'value' = 'human_atlas')),
-  value = 'human_atlas',
-  id = 'radio-options',
-  labelStyle = list('display' = 'inline-block'))
-))
-)),
-dccGraph(
-  id = 'brain-graph',
-  figure = list(
-    data = list(data
-    ),
-    layout = plot_layout
   ),
-  config = list('editable' = TRUE, 'scrollZoom' = FALSE),
-  style = list(
-    'userSelect' = 'none',
-    'margin' = 'auto'
-  )
-),
-htmlDiv(list(
+  htmlDiv(list(dccRadioItems(
+    options=list(
+      list('label' = 'Cortical', 'value' = 'human'),
+      list('label' = 'Thickness Mouse Brain', 'value' = 'mouse'),
+      list('label' = 'Brain Atlas', 'value' = 'human_atlas')),
+    value = 'human_atlas',
+    id = 'radio-options',
+    labelStyle = list('display' = 'inline-block'))
+  )),
+  htmlBr(),
   dccMarkdown(
-    children = '**Click Data** \n
-    Click on points in the graph.'
+    children = '**Click Data** | Click on points in the graph.'
   ),
-  htmlPre(id = 'click-data', 
-          style = list(
-            'border' = 'thin lightgrey solid',
-            'padding'= '10px',
-            'marginBottom' = '20px')))),
-
-htmlDiv(list(dccMarkdown(
-  children = '**Relayout Data** \n
-  Drag the graph corners to rotate it.'
-),
-htmlPre(id = 'relayout-data', 
-        style = list(
-          'border' = 'thin lightgrey solid',
-          'padding'= '10px',
-          'marginBottom' = '20px'))
-)),
-htmlP(
-  children = list(
-    htmlA(
-      children='GitHub',
-      target= '_blank',
-      href ='https://github.com/plotly/dash-brain-surface-viewer',
-      style=list('color'= '#F012BE')
-    ),
-    '. Brain data from Mcgill\'s ACE Lab',
-    htmlA(
-      children='Surface Viewer',
-      target= '_blank',
-      href='https://brainbrowser.cbrain.mcgill.ca/surface-viewer#ct',
-      style=list('color'= '#F012BE')
+  htmlDiv(list(
+    
+    htmlPre(id = 'click-data', 
+            style = list(
+              'border' = 'None',
+              'padding'= '10px',
+              'marginBottom' = '20%','resize' = 'both', 'overflow' = 'auto'))),
+    style = list('background-color' = '#282828')),
+  
+  dccMarkdown(
+    children = '**Relayout Data** | Drag the graph corners to rotate it.'
+  ),
+  
+  htmlDiv(list(
+    htmlPre(id = 'relayout-data', 
+            style = list(
+              'border' = 'None',
+              'padding'= '10px',
+              'marginBottom' = '20px',
+              'resize' = 'both', 'overflow' = 'auto'))
+  ),
+  style = list('background-color' = '#282828')),
+  htmlP(
+    children = list(
+      'Dash/Python code on ',
+      htmlA(
+        children='GitHub',
+        target= '_blank',
+        href ='https://github.com/plotly/dash-brain-surface-viewer',
+        style=list('color'= '#F012BE')
+      ),
+      '. Brain data from Mcgill\'s ACE Lab',
+      htmlA(
+        children='Surface Viewer',
+        target= '_blank',
+        href='https://brainbrowser.cbrain.mcgill.ca/surface-viewer#ct',
+        style=list('color'= '#F012BE')
+      )
     )
-  )
+  )),
+  style = list('float' = 'right', 'background-color' = "#1E1E1E", 'width' = '29%', 'height' = '1300px', padding = '10px')),
+  
+  htmlDiv(
+    dccGraph(
+      id = 'brain-graph',
+      figure = list(
+        data = list(data
+        ),
+        layout = plot_layout
+      ),
+      
+      config = list('editable' = TRUE, 'scrollZoom' = FALSE),
+      style = list(
+        'userSelect' = 'none',
+        'width' = '100%',
+        'height' = '900px',
+        "flex"= "5",
+        "position"= "relative"
+      )
+    ),
+    
+    style = list('float' = 'left', 'width' = '60%', 'height' = '50%', padding = 'None'))
 )
-),
-style = list('margin' = '0 auto')
+#style = list('margin' = '0 auto')
 ))
 
 #CALLBACKS
