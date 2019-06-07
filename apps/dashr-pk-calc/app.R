2a1759796db8b6727739cf999c88466be1ef3639
CanerIrfanoglu <canerirfanoglu@gmail.com>
2019-06-07 17:40:43 -0400

    Added deployment requirements & README

--- apps/dashr-pk-calc/app.R
+++ apps/dashr-pk-calc/app.R
@@ -1,5 +1,12 @@
-setwd("/Users/Caner/Desktop/plotly/dashR-pk-calc")
-# Setting path to projects folder for having assets on path
+appName <- Sys.getenv("DASH_APP_NAME")
+if (appName != ""){
+  pathPrefix <- sprintf("/%s/", appName)
+  
+  Sys.setenv(DASH_ROUTES_PATHNAME_PREFIX = pathPrefix,
+             DASH_REQUESTS_PATHNAME_PREFIX = pathPrefix)
+  
+  setwd(sprintf("/app/apps/%s", appName))
+}
 
 library(dashR)
 library(dashCoreComponents)
@@ -314,7 +321,11 @@ app$callback(output = list(id = "results-graph", property = "figure"),
   }
 )
 
-app$run_server(debug = TRUE)
+if (appName != "") {
+  app$run_server(host = "0.0.0.0", port = Sys.getenv('PORT', 8050)) 
+} else {
+  app$run_server()
+}
 
 
 
