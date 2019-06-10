6fa7e26681d785f6767caec8e28dad5f09855058
celinehuang <celine756@hotmail.com>
2019-06-10 17:30:40 -0400

    added mobile layout with new header
    Former-commit-id: e622a58c74732b89f3d359a3dcf40868664847f3

--- apps/dash-oil-and-gas/points.py
+++ apps/dash-oil-and-gas/points.py
@@ -25,12 +25,10 @@ columns2 = [
     "Oil Produced, bbl",
     "Reporting Year",
 ]
-df1 = pd.read_csv(
-    "data/Oil_and_Gas_Annual_Production__1985_-_2000.csv", low_memory=False
-)[columns1]
-df2 = pd.read_csv(
-    "data/Oil_and_Gas_Annual_Production__Beginning_2001.csv", low_memory=False
-)[columns2]
+df1 = pd.read_csv(DATA_PATH.joinpath(
+    "Oil_and_Gas_Annual_Production__1985_-_2000.csv"), low_memory=False)[columns1]
+df2 = pd.read_csv(DATA_PATH.joinpath(
+    "Oil_and_Gas_Annual_Production__Beginning_2001.csv"), low_memory=False)[columns2]
 
 
 # In[]:
@@ -48,16 +46,12 @@ columns = [
     "Reporting Year",
 ]
 for api, df_well in df.groupby("API Well Number"):
-    # print(api)
-    # print(df_well)
     df_well = df_well[columns]
     df_well.index = df_well["Reporting Year"]
     df_well = df_well.to_dict(orient="index")
     dataset[api] = df_well
 
-# print(dataset)
-
 # In[]:
 # Serialise to Pickle
-with open("data/points.pkl", "wb") as f:
+with open(DATA_PATH.joinpath("points.pkl"), "wb") as f:
     pickle.dump(dataset, f)
