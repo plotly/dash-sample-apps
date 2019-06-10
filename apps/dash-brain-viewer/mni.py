df716909dcbe1dbf5bd8980b3dd2a769ef61da7a
Chris Woo <chriskfwoo@gmail.com>
2019-06-10 13:36:52 -0400

    applied the black linter
    Former-commit-id: 448e2bd007ab73872cd6029cd100d70d1257338b

--- apps/dash-brain-viewer/mni.py
+++ apps/dash-brain-viewer/mni.py
@@ -2,10 +2,9 @@ import numpy as np
 
 
 def read_mniobj(file):
-
     def triangulate_polygons(list_vertex_indices):
         for k in range(0, len(list_vertex_indices), 3):
-            yield list_vertex_indices[k: k+3]
+            yield list_vertex_indices[k : k + 3]
 
     with open(file) as fp:
 
@@ -19,8 +18,8 @@ def read_mniobj(file):
                 num_vertices = int(line.split()[6])
                 matrix_vertices = np.zeros([num_vertices, 3])
             elif i <= num_vertices:
-                matrix_vertices[i-1] = list(map(float, line.split()))
-            elif i > 2*num_vertices+5:
+                matrix_vertices[i - 1] = list(map(float, line.split()))
+            elif i > 2 * num_vertices + 5:
                 if not line.strip():
                     k = 1
                 elif k == 1:
@@ -38,7 +37,7 @@ def plotly_triangular_mesh(
     colorscale="Viridis",
     flatshading=False,
     showscale=False,
-    plot_edges=False
+    plot_edges=False,
 ):
 
     x, y, z = vertices.T
@@ -67,22 +66,14 @@ def plotly_triangular_mesh(
             "specular": 1,
             "roughness": 0.1,
             "facenormalsepsilon": 1e-6,
-            "vertexnormalsepsilon": 1e-12
+            "vertexnormalsepsilon": 1e-12,
         },
-        "lightposition": {
-            "x": 100,
-            "y": 200,
-            "z": 0
-        }
+        "lightposition": {"x": 100, "y": 200, "z": 0},
     }
 
     if showscale:
-        mesh["colorbar"] = {
-            "thickness": 20,
-            "ticklen": 4,
-            "len": 0.75
-        }
-    
+        mesh["colorbar"] = {"thickness": 20, "ticklen": 4, "len": 0.75}
+
     if plot_edges is False:
         return [mesh]
 
@@ -92,55 +83,62 @@ def plotly_triangular_mesh(
 
 def create_plot_edges_lines(vertices, faces):
     tri_vertices = vertices[faces]
-    Xe=[]
-    Ye=[]
-    Ze=[]
+    Xe = []
+    Ye = []
+    Ze = []
     for T in tri_vertices:
-        Xe+=[T[k%3][0] for k in range(4)]+[ None]
-        Ye+=[T[k%3][1] for k in range(4)]+[ None]
-        Ze+=[T[k%3][2] for k in range(4)]+[ None]
-    #define the lines to be plotted
-    lines=dict(type='scatter3d',
-                x=Xe,
-                y=Ye,
-                z=Ze,
-                mode='lines',
-                name='',
-                line=dict(color= 'rgb(70,70,70)', width=1)
-            )
+        Xe += [T[k % 3][0] for k in range(4)] + [None]
+        Ye += [T[k % 3][1] for k in range(4)] + [None]
+        Ze += [T[k % 3][2] for k in range(4)] + [None]
+    # define the lines to be plotted
+    lines = dict(
+        type="scatter3d",
+        x=Xe,
+        y=Ye,
+        z=Ze,
+        mode="lines",
+        name="",
+        line=dict(color="rgb(70,70,70)", width=1),
+    )
     return lines
 
 
 def create_mesh_data(option):
 
     DEFAULT_COLORSCALE = [
-        [0, 'rgb(12,51,131)'],
-        [0.25, 'rgb(10,136,186)'],
-        [0.5, 'rgb(242,211,56)'],
-        [0.75, 'rgb(242,143,56)'],
-        [1, 'rgb(217,30,30)']
+        [0, "rgb(12,51,131)"],
+        [0.25, "rgb(10,136,186)"],
+        [0.5, "rgb(242,211,56)"],
+        [0.75, "rgb(242,143,56)"],
+        [1, "rgb(217,30,30)"],
     ]
 
     data = []
     if option == "human":
         vertices, faces = read_mniobj("realct.obj")
-        intensities = np.loadtxt('realct.txt')
-        data = plotly_triangular_mesh(vertices, faces, intensities, colorscale=DEFAULT_COLORSCALE)
+        intensities = np.loadtxt("realct.txt")
+        data = plotly_triangular_mesh(
+            vertices, faces, intensities, colorscale=DEFAULT_COLORSCALE
+        )
 
     elif option == "human_atlas":
         vertices, faces = read_mniobj("surf_reg_model_both.obj")
-        intensities = np.loadtxt('aal_atlas.txt')
-        data = plotly_triangular_mesh(vertices, faces, intensities, colorscale=DEFAULT_COLORSCALE)
+        intensities = np.loadtxt("aal_atlas.txt")
+        data = plotly_triangular_mesh(
+            vertices, faces, intensities, colorscale=DEFAULT_COLORSCALE
+        )
 
     elif option == "mouse":
         vertices, faces = read_mniobj("mouse_surf.obj")
-        intensities = np.loadtxt('mouse_map.txt')
-        data = plotly_triangular_mesh(vertices, faces, intensities, colorscale=DEFAULT_COLORSCALE)
-        
+        intensities = np.loadtxt("mouse_map.txt")
+        data = plotly_triangular_mesh(
+            vertices, faces, intensities, colorscale=DEFAULT_COLORSCALE
+        )
+
         vertices, faces = read_mniobj("mouse_brain_outline.obj")
         outer_mesh = plotly_triangular_mesh(vertices, faces)[0]
-        outer_mesh['opacity'] = 0.5
-        outer_mesh['colorscale'] = 'Greys'
+        outer_mesh["opacity"] = 0.5
+        outer_mesh["colorscale"] = "Greys"
         data.append(outer_mesh)
     else:
         raise ValueError
