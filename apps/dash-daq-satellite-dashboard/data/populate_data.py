8eaff581c163a04f19bc4398e1414483d190dfdb
Yunke Xiao <yunke0123@hotmail.com>
2019-06-11 16:07:36 -0400

    Black format
    Former-commit-id: 6e2205b79b10f51bfa70b883cbc9ec4bfc923c04

--- apps/dash-daq-satellite-dashboard/data/populate_data.py
+++ apps/dash-daq-satellite-dashboard/data/populate_data.py
@@ -4,26 +4,26 @@ from random import randint
 # Setup
 #######################################################################################################################
 # Default data
-file_h = open('non_gps_data_h.csv', 'w')
-file_m = open('non_gps_data_m.csv', 'w')
+file_h = open("non_gps_data_h.csv", "w")
+file_m = open("non_gps_data_m.csv", "w")
 
 # Satellite H45-K1 data
-file_h_0 = open('non_gps_data_h_0.csv', 'w')
-file_m_0 = open('non_gps_data_m_0.csv', 'w')
+file_h_0 = open("non_gps_data_h_0.csv", "w")
+file_m_0 = open("non_gps_data_m_0.csv", "w")
 
 # Satellite L12-5 data
-file_h_1 = open('non_gps_data_h_1.csv', 'w')
-file_m_1 = open('non_gps_data_m_1.csv', 'w')
+file_h_1 = open("non_gps_data_h_1.csv", "w")
+file_m_1 = open("non_gps_data_m_1.csv", "w")
 
 # Initialize the first column
-file_h.write('elevation,temperature,speed,fuel,battery\n')
-file_m.write('elevation,temperature,speed,fuel,battery\n')
+file_h.write("elevation,temperature,speed,fuel,battery\n")
+file_m.write("elevation,temperature,speed,fuel,battery\n")
 
-file_h_0.write('elevation,temperature,speed,fuel,battery\n')
-file_m_0.write('elevation,temperature,speed,fuel,battery\n')
+file_h_0.write("elevation,temperature,speed,fuel,battery\n")
+file_m_0.write("elevation,temperature,speed,fuel,battery\n")
 
-file_h_1.write('elevation,temperature,speed,fuel,battery\n')
-file_m_1.write('elevation,temperature,speed,fuel,battery\n')
+file_h_1.write("elevation,temperature,speed,fuel,battery\n")
+file_m_1.write("elevation,temperature,speed,fuel,battery\n")
 
 
 # Data randomizer function
@@ -35,7 +35,7 @@ def randomize(start, variance):
 
 # Add data to file
 def add_data(data, file):
-    file.write(str(data) + ',')
+    file.write(str(data) + ",")
     return data
 
 
@@ -46,14 +46,14 @@ def add_data(data, file):
 # Data updated by the minute (default data)
 for i in range(60):
     for j in range(4):
-        file_h.write(str(0) + ',')
-    file_h.write(str(0) + '\n')
+        file_h.write(str(0) + ",")
+    file_h.write(str(0) + "\n")
 
 # Data updated by the second (default data)
 for i in range(60):
     for j in range(4):
-        file_m.write(str(0) + ',')
-    file_m.write(str(0) + '\n')
+        file_m.write(str(0) + ",")
+    file_m.write(str(0) + "\n")
 
 # Data updated by the minute (H45-K1 data)
 for i in range(60):
@@ -61,7 +61,7 @@ for i in range(60):
     add_data(randomize(290, 0.2), file_h_0)
     add_data((randomize(280, 0.2) / 10), file_h_0)
     add_data(randomize(80, 0.05), file_h_0)
-    file_h_0.write(str(randomize(80, 0.05)) + '\n')
+    file_h_0.write(str(randomize(80, 0.05)) + "\n")
 
 # Data updated by the second (H45-K1 data)
 for i in range(60):
@@ -69,7 +69,7 @@ for i in range(60):
     add_data(randomize(290, 0.025), file_m_0)
     add_data((randomize(280, 0.05) / 10), file_m_0)
     add_data(randomize(80, 0.02), file_m_0)
-    file_m_0.write(str(randomize(80, 0.02)) + '\n')
+    file_m_0.write(str(randomize(80, 0.02)) + "\n")
 
 # Data updated by the minute (L12-5 data)
 for i in range(60):
@@ -77,7 +77,7 @@ for i in range(60):
     add_data(randomize(350, 0.2), file_h_1)
     add_data((randomize(280, 0.2) / 10), file_h_1)
     add_data(randomize(80, 0.05), file_h_1)
-    file_h_1.write(str(randomize(80, 0.05)) + '\n')
+    file_h_1.write(str(randomize(80, 0.05)) + "\n")
 
 # Data updated by the second (L12-5 data)
 for i in range(60):
@@ -85,4 +85,4 @@ for i in range(60):
     add_data(randomize(290, 0.025), file_m_1)
     add_data((randomize(330, 0.05) / 10), file_m_1)
     add_data(randomize(60, 0.02), file_m_1)
-    file_m_1.write(str(randomize(65, 0.02)) + '\n')
+    file_m_1.write(str(randomize(65, 0.02)) + "\n")
