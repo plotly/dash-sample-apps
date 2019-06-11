524773d986c38c12d4edb47da5e2616b8a136ec0
celinehuang <celine756@hotmail.com>
2019-06-11 11:39:42 -0400

    fixed issues for playground deployment

--- apps/dash-oil-and-gas/app.py
+++ apps/dash-oil-and-gas/app.py
@@ -23,6 +23,7 @@ DATA_PATH = PATH.joinpath("data").resolve()
 app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
 )
+server = app.server
 
 # Create controls
 county_options = [
