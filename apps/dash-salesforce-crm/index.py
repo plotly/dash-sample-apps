49ffe7f649204d6175dd02256dbc834940434c9e
Mskycoder <gnidihz@gmail.com>
2019-06-12 13:50:07 -0400

    moving from standalone repo
    Former-commit-id: a00038d83016018820a5a785993105c1d5c9212a

--- apps/dash-salesforce-crm/index.py
+++ apps/dash-salesforce-crm/index.py
@@ -17,7 +17,7 @@ app.layout = html.Div(
         html.Div([
 
             html.Span("CRM App using Salesforce API", className='app-title'),
-            
+
             html.Div(
                 html.Img(src='https://s3-us-west-1.amazonaws.com/plotly-tutorials/logo/new-branding/dash-logo-by-plotly-stripe-inverted.png',height="100%")
                 ,style={"float":"right","height":"100%"})
@@ -32,9 +32,9 @@ app.layout = html.Div(
                 id="tabs",
                 style={"height":"20","verticalAlign":"middle"},
                 children=[
-                    dcc.Tab(label="Opportunities", value="opportunities_tab"),
-                    dcc.Tab(label="Leads", value="leads_tab"),
-                    dcc.Tab(id="cases_tab",label="Cases", value="cases_tab"),
+                    dcc.Tab(id="opportubnities_tab", label="Opportunities", value="opportunities_tab"),
+                    dcc.Tab(id="leads_tab", label="Leads", value="leads_tab"),
+                    dcc.Tab(id="cases_tab", label="Cases", value="cases_tab"),
                 ],
                 value="leads_tab",
             )
@@ -42,8 +42,8 @@ app.layout = html.Div(
             ],
             className="row tabs_div"
             ),
-       
-                
+
+
         # divs that save dataframe for each tab
         html.Div(
                 sf_manager.get_opportunities().to_json(orient="split"),  # opportunities df
@@ -57,13 +57,11 @@ app.layout = html.Div(
 
         # Tab content
         html.Div(id="tab_content", className="row", style={"margin": "2% 3%"}),
-        
+
         html.Link(href="https://use.fontawesome.com/releases/v5.2.0/css/all.css",rel="stylesheet"),
-        html.Link(href="https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css",rel="stylesheet"),
         html.Link(href="https://fonts.googleapis.com/css?family=Dosis", rel="stylesheet"),
         html.Link(href="https://fonts.googleapis.com/css?family=Open+Sans", rel="stylesheet"),
         html.Link(href="https://fonts.googleapis.com/css?family=Ubuntu", rel="stylesheet"),
-        html.Link(href="https://cdn.rawgit.com/amadoukane96/8a8cfdac5d2cecad866952c52a70a50e/raw/cd5a9bf0b30856f4fc7e3812162c74bfc0ebe011/dash_crm.css", rel="stylesheet")
     ],
     className="row",
     style={"margin": "0%"},
