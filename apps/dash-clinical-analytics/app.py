2ddf1ad8560a4b9028ad252a9f503b87124375aa
ycaokris <yi.cao2@mail.mcgill.ca>
2019-06-12 20:15:23 -0400

    Add meta tags, finish desktop styling
    Former-commit-id: 789e8edf9d26ad3670eb3d701c41ead1ffd609e7

--- apps/dash-clinical-analytics/app.py
+++ apps/dash-clinical-analytics/app.py
@@ -9,7 +9,7 @@ import datetime
 from datetime import datetime as dt
 import pathlib
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}])
 
 server = app.server
 app.config.suppress_callback_exceptions = True
