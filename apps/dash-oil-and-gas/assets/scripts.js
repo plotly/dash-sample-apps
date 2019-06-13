ae2d40d7d2ac2f986cbba8cdec9488ab925ad3ba
celinehuang <celine756@hotmail.com>
2019-06-13 10:07:33 -0400

    added scripts.js to fix the count_graph resizing issue
    Former-commit-id: 281d56026e38ba69c60c1d760f348e3836d288be

---
+++ apps/dash-oil-and-gas/assets/scripts.js
@@ -0,0 +1,20 @@
+var graphToResize = document.getElementById("count_graph");
+
+var resized = false;
+
+window.onload = function() {
+  setTimeout(function() {
+    var graphToResize = document.getElementById("count_graph");
+    graphToResize.on("plotly_afterplot", triggerResize, { once: true });
+  }, 1000);
+};
+
+var triggerResize = function() {
+  if (!resized) {
+    setTimeout(function() {
+      window.dispatchEvent(new Event("resize"));
+      console.log("fired resize");
+    }, 1000);
+    resized = true;
+  }
+};
