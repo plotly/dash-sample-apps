7f437b44112777345b99c2c818fa43b5919a4437
HammadTheOne <30986043+HammadTheOne@users.noreply.github.com>
2019-06-18 15:14:23 -0400

    Update init.R

--- apps/dashr-ideogram/init.R
+++ apps/dashr-ideogram/init.R
@@ -51,9 +51,12 @@ install.packages("https://cloud.r-project.org/src/contrib/fiery_1.1.1.tar.gz", t
 
 
 
-remotes::install_github("plotly/dashR", dependencies=FALSE)
+remotes::install_github("plotly/dash", dependencies=FALSE)
 remotes::install_github("plotly/dash-html-components")
 remotes::install_github("plotly/dash-core-components")
 
 remotes::install_github("plotly/dash-bio", dependencies=FALSE)
 remotes::install_github("plotly/dash-daq", dependencies=FALSE)
+
+install.packages("plotly")
+install.packages("plyr")
