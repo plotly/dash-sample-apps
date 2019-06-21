1f5ac38f3c575944ee65ef1b186c05239624e06a
HammadTheOne <30986043+HammadTheOne@users.noreply.github.com>
2019-06-21 14:30:39 -0400

    Update app.R

--- apps/dashr-financial-report/app.R
+++ apps/dashr-financial-report/app.R
@@ -31,14 +31,7 @@ library('lubridate')
 
 #################################################################################
 
-external_css1 = list("https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css",
-                     "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
-                     "https://codepen.io/bcd/pen/KQrXdb.css",
-                     "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-                     "https://cdn.rawgit.com/plotly/dash-app-stylesheets/5047eb29e4afe01b45b27b1d2f7deda2a942311a/goldman-sachs-report.css")
-
-
-app <- Dash$new(external_stylesheets = external_css1)
+app <- Dash$new()
 
 
 grey_line <- htmlDiv(list(
