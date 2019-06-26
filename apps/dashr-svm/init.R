744e7d9d54813ef15fc0058ff068905b030974f5
Lucas Goldstone <lu.goldstone@gmail.com>
2019-06-26 11:36:25 -0400

    Add cran mirror

--- apps/dashr-svm/init.R
+++ apps/dashr-svm/init.R
@@ -2,6 +2,10 @@
 # contains placeholders which are inserted by the compile script
 # NOTE: this script is executed in the chroot context; check paths!
 
+r <- getOption("repos")
+r["CRAN"] <- "http://cloud.r-project.org"
+options(repos=r)
+
 remotes::install_github("plotly/dash-table", ref="4.0.0-cran")
 remotes::install_github("plotly/dash-html-components", ref="1.0.0-cran")
 remotes::install_github("plotly/dash-core-components", ref="1.0.0-cran")
