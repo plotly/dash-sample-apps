607e8f85161e3fc66275de40e12c6715af14baa0
ycaokris <yi.cao2@mail.mcgill.ca>
2019-06-25 23:17:54 -0400

    black files

--- apps/dash-clinical-analytics/app.py
+++ apps/dash-clinical-analytics/app.py
@@ -248,7 +248,7 @@ def generate_table_row(id, style, col1, col2, col3):
         children=[
             html.Div(
                 id=col1["id"],
-                style={'display': 'table', 'height': '100%'},
+                style={"display": "table", "height": "100%"},
                 className="two columns row-department",
                 children=col1["children"],
             ),
