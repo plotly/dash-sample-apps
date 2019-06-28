e1fd1558d37e6604e52ef7f8cbac90ce788b7585
Dan Seol <yunheum.seol@gmail.com>
2019-06-28 16:15:12 -0400

    reflecting the reviews

--- apps/dashr-yield-curve/app.R
+++ apps/dashr-yield-curve/app.R
@@ -15,14 +15,14 @@ library(rlang)
 #setwd("~/Dash apps/dashr-yield-curve")
 
 appName <- Sys.getenv("DASH_APP_NAME")
-pathPrefix <- sprintf("/%s/", appName)
-
-
-Sys.setenv(DASH_ROUTES_PATHNAME_PREFIX = pathPrefix,
-           DASH_REQUESTS_PATHNAME_PREFIX = pathPrefix)
-setwd(sprintf("/app/apps/%s", appName))
-
-l. <- list
+if (appName != ""){
+  pathPrefix <- sprintf("/%s/", appName)
+  
+  Sys.setenv(DASH_ROUTES_PATHNAME_PREFIX = pathPrefix,
+             DASH_REQUESTS_PATHNAME_PREFIX = pathPrefix)
+  
+  setwd(sprintf("/app/apps/%s", appName))
+}
 
 #Helper functions
 
@@ -62,69 +62,69 @@ last_next <- 0
 
 DT_ <- fread('data/yield_curve.csv') %>% drop_na(., x,y)
 dates <- DT_$y
-Dates <- base::as.Date(dates, format='%m/%d/%Y')
+Dates <- as.Date(dates, format='%m/%d/%Y')
 labels <-(DT_$x)[1:11]
 DT <- DT_[, 3:13]
 colnames(DT) <- labels
 rownames(DT) <- dates
 DM <- DT %>% data.matrix()
 
-UPS <- l.('0'= l.(x=0, y=0, z=1), '1'=l.(x=0, y=0, z=1), '2'=l.(x=0, y=0, z=1), '3'=l.(x=0, y=0, z=1), '4'=l.(x=0, y=0, z=1), '5'=l.(x=0, y=0, z=1))
+UPS <- list('0'= list(x=0, y=0, z=1), '1'=list(x=0, y=0, z=1), '2'=list(x=0, y=0, z=1), '3'=list(x=0, y=0, z=1), '4'=list(x=0, y=0, z=1), '5'=list(x=0, y=0, z=1))
 
-CENTERS <- l.('0'= l.(x=0.3, y=0.8, z=-0.5), 
-              '1'=l.(x=0, y=0, z=-0.37), 
-              '2'=l.(x=0, y=1.1, z=-1.3), 
-              '3'=l.(x=0, y=-0.7, z=0), 
-              '4'=l.(x=0, y=-0.2, z=0), 
-              '5'=l.(x=-0.11, y=-0.5, z=0))
+CENTERS <- list('0'= list(x=0.3, y=0.8, z=-0.5), 
+              '1'=list(x=0, y=0, z=-0.37), 
+              '2'=list(x=0, y=1.1, z=-1.3), 
+              '3'=list(x=0, y=-0.7, z=0), 
+              '4'=list(x=0, y=-0.2, z=0), 
+              '5'=list(x=-0.11, y=-0.5, z=0))
 
-EYES <-  l.('0'= l.(x=2.7, y=2.7, z=0.3), 
-            '1'=l.(x=0.01, y=3.8, z=-0.37), 
-            '2'=l.(x=1.3, y=3, z=0), 
-            '3'=l.(x=2.6, y=-1.6, z=0), 
-            '4'=l.(x=3, y=-0.2, z=0), 
-            '5'=l.(x=-0.1, y=-0.5, z=2.66))
+EYES <-  list('0'= list(x=2.7, y=2.7, z=0.3), 
+            '1'=list(x=0.01, y=3.8, z=-0.37), 
+            '2'=list(x=1.3, y=3, z=0), 
+            '3'=list(x=2.6, y=-1.6, z=0), 
+            '4'=list(x=3, y=-0.2, z=0), 
+            '5'=list(x=-0.1, y=-0.5, z=2.66))
 
-TEXTS <- l.(
-  l.(
-    htmlDiv(dccMarkdown(className='six columns', children=" #### Yield curve 101 "), style=l.(width='120%')), #htmlBr(),
+TEXTS <- list(
+  list(
+    htmlDiv(dccMarkdown(className='six columns', children=" #### Yield curve 101 "), style=list(width='120%')), #htmlBr(),
     htmlDiv(dccMarkdown(className='six columns', children=" The yield curve shows how much it costs the federal government to borrow
                  money for a given amount of time, revealing the relationship between long-
                    and short-term interest rates.  It is, inherently, a forecast for what the economy holds in the future -
                  how much inflation there will be, for example, and how healthy growth will
                  be over the years ahead - all embodied in the price of money today,
-                 tomorrow and many years from now. "), style=l.(width='120%'))), 
+                 tomorrow and many years from now. "), style=list(width='120%'))), 
   
-  l.(htmlDiv(dccMarkdown(className='six columns', children= "#### Where we stand"), style=l.(width='120%')), #htmlBr(),
+  list(htmlDiv(dccMarkdown(className='six columns', children= "#### Where we stand"), style=list(width='120%')), #htmlBr(),
      htmlDiv( dccMarkdown(className='six columns', children="On Wednesday, both short-term and long-term rates were lower than they have
     been for most of history - a reflection of the continuing hangover
     from the financial crisis. The yield curve is fairly flat, which is a sign that investors expect
-    mediocre growth in the years ahead."), style=l.(width='120%'))), 
-  l.(htmlDiv(dccMarkdown(className='six columns', children=' #### Deep in the valley'),style=l.(width='120%')), #htmlBr(),
+    mediocre growth in the years ahead."), style=list(width='120%'))), 
+  list(htmlDiv(dccMarkdown(className='six columns', children=' #### Deep in the valley'),style=list(width='120%')), #htmlBr(),
      htmlDiv(dccMarkdown(className='six columns', children='In response to the last recession, the Federal Reserve has kept short-term
     rates very low - near zero - since 2008. (Lower interest rates stimulate
     the economy, by making it cheaper for people to borrow money, but also
     spark inflation.) Now, the Fed is getting ready to raise rates again, possibly as early as
-    June.'), style=l.('width'='120%'))), 
-  l.(htmlDiv(dccMarkdown(className='six columns', children=' #### Last time, a puzzle'), style=l.(width='120%')), #htmlBr(),
+    June.'), style=list('width'='120%'))), 
+  list(htmlDiv(dccMarkdown(className='six columns', children=' #### Last time, a puzzle'), style=list(width='120%')), #htmlBr(),
      htmlDiv(dccMarkdown(className='six columns', children='The last time the Fed started raising rates was in 2004. From 2004 to 2006,
     short-term rates rose steadily. But long-term rates did not rise very much. The Federal Reserve chairman called this phenomenon a "conundrum," and it
     raised questions about the ability of the Fed to guide the economy.
     Part of the reason long-term rates failed to rise was because of strong
-    foreign demand.') , style=l.('width'='120%'))), 
-  l.(htmlDiv(dccMarkdown(className='six columns', children='#### Long-term rates are low now, too'), style=l.(width='120%')), #htmlBr(),
+    foreign demand.') , style=list('width'='120%'))), 
+  list(htmlDiv(dccMarkdown(className='six columns', children='#### Long-term rates are low now, too'), style=list(width='120%')), #htmlBr(),
      htmlDiv(dccMarkdown(className='six columns', children= 'Foreign buyers have helped keep long-term rates low recently, too - as have
     new rules encouraging banks to hold government debt and expectations that
     economic growth could be weak for a long time. The 10-year Treasury yield was as low as it has ever been in July 2012 and
-    has risen only modestly since.Some economists refer to the economic pessimism as "the new normal."'), style=l.('width'='120%'))), 
-  l.(htmlDiv(dccMarkdown(className='six columns', children=' #### Long-term rates are low now, too'),style=l.(width='120%') ), #htmlBr(),
-     htmlDiv(dccMarkdown(className='six columns', children= 'Here is the same chart viewed from above.'), style=l.(width='120%')))
+    has risen only modestly since.Some economists refer to the economic pessimism as "the new normalist"'), style=list('width'='120%'))), 
+  list(htmlDiv(dccMarkdown(className='six columns', children=' #### Long-term rates are low now, too'),style=list(width='120%') ), #htmlBr(),
+     htmlDiv(dccMarkdown(className='six columns', children= 'Here is the same chart viewed from above.'), style=list(width='120%')))
 )
 
 ##INITALIZE THE GRAPH
-graph <- DM %>% .[,dim(.)[2]:1] %>% plot_ly(x=rev(labels), y=base::as.Date(dates, format='%m/%d/%Y'),z=., type='surface', 
-                        hoverinfo='x+y+z', lighting=l.('ambient'=0.95, 'diffuse'=0.99, 'fresnel'=0.01, 'roughness'=0.01, 'specular'=0.01 ),
-                        colorscale=l.(l.(0, 'rgb(230,245,254)'), l.(0.4,'rgb(123,171,203)'), l.(0.8, 'rgb(40,119,174)'), l.(1, 'rgb(37,61,81)')), zmax=9.18, zmin=0, scene='scene'
+graph <- DM %>% .[,dim(.)[2]:1] %>% plot_ly(x=rev(labels), y=as.Date(dates, format='%m/%d/%Y'),z=., type='surface', 
+                        hoverinfo='x+y+z', lighting=list('ambient'=0.95, 'diffuse'=0.99, 'fresnel'=0.01, 'roughness'=0.01, 'specular'=0.01 ),
+                        colorscale=list(list(0, 'rgb(230,245,254)'), list(0.4,'rgb(123,171,203)'), list(0.8, 'rgb(40,119,174)'), list(1, 'rgb(37,61,81)')), zmax=9.18, zmin=0, scene='scene'
 )
 #graph
 
@@ -138,9 +138,9 @@ app <- Dash$new()
 
 app$layout(
   
-  htmlDiv(l.(
+  htmlDiv(list(
     #
-    htmlDiv(l.(
+    htmlDiv(list(
       ##
               dccMarkdown(
 
@@ -155,36 +155,35 @@ app$layout(
               children='This interactive report is a rendition of a
       [New York Times original](https://www.nytimes.com/interactive/2015/03/19/upshot/3d-yield-curve-economic-growth.html).')
               )
-              
       ##
             ,
             className='row'
             ,
-            style=l.('text-align'='center', 'margin-bottom'= '15px')
+            style=list('text-align'='center', 'margin-bottom'= '15px')
 
             )
     #
     ,
 
-    htmlDiv(l.(
+    htmlDiv(list(
       ##
-      htmlDiv(l.(
+      htmlDiv(list(
         ###
           dccSlider(min=0, max=5, value=0, marks=lapply(0:5, genMarksEmpty), id='slider')
         ###
-          ), className='row',style=l.('margin-bottom'= '10px'))
+          ), className='row',style=list('margin-bottom'= '10px'))
       ##
       ,
       ##
-      htmlDiv(l.(
+      htmlDiv(list(
         ###
-          htmlDiv(l.(
+          htmlDiv(list(
             ####
-              htmlButton('Back', id='back', style=l.('display'='inline-block'),n_clicks=0)#, className='learn-more-button')
+              htmlButton('Back', id='back', style=list('display'='inline-block'),n_clicks=0)#, className='learn-more-button')
             ####
               ,
             ####
-              htmlButton('Next', id='next', style=l.('display'='inline-block'),n_clicks=0)#,  className='learn-more-button')
+              htmlButton('Next', id='next', style=list('display'='inline-block'),n_clicks=0)#,  className='learn-more-button')
             ####
               ),className='two columns offset-by-two')
         ###
@@ -193,19 +192,19 @@ app$layout(
           htmlDiv(id='text', className='six columns', children='')
         ###
 
-          ), className='row', style=l.('margin-bottom'= '10px'))
+          ), className='row', style=list('margin-bottom'= '10px'))
       ##
       ,
       ##
       dccGraph(
         id='graph',
-        style=l.('height'= '60vh'),
+        style=list('height'= '60vh'),
         figure=graph
       )
       ##
       ,
       ##
-      htmlDiv(id='bucket', style=l.(display='none'))
+      htmlDiv(id='bucket', style=list(display='none'))
       ,
       htmlDiv(id='dummy')
      
@@ -239,9 +238,9 @@ app$layout(
 
 
 app$callback(
-  output=l.(id='bucket', property='children')
+  output=list(id='bucket', property='children')
   ,
-  params = l.(input(id='back', property='n_clicks'), input(id='next', property='n_clicks'))
+  params = list(input(id='back', property='n_clicks'), input(id='next', property='n_clicks'))
   ,
   function(beta, nu)
   {
@@ -262,12 +261,9 @@ app$callback(
       n <- as.numeric(nu[[1]])    
     }
     
-   
-    
-    
     V <- (n-b) %% 6
     
-    return(V %>% toJSON(., null='null'))
+    return(toJSON(V, null='null'))
   }
 )
 
@@ -275,12 +271,12 @@ app$callback(
 # 
 app$callback(
 
-  output = l.(id='slider', property='value')
+  output = list(id='slider', property='value')
   ,
-  params = l.(input(id='bucket', property='children'), state(id='slider', property='value'))
+  params = list(input(id='bucket', property='children'), state(id='slider', property='value'))
   ,
   function(V, S){
-    value <- (V %>%fromJSON)
+    value <- (fromJSON(V))
     if(is.na(value)||is.null(value)||length(value)==0||rlang::is_empty(value)){return(0)} else {
 
     value <- value[[1]] %>% as.numeric
@@ -301,9 +297,9 @@ app$callback(
 
 app$callback(
   
-  output=l.(id='dummy', property='children')
+  output=list(id='dummy', property='children')
   ,
-  params=l.(input(id='bucket', property='children'))
+  params=list(input(id='bucket', property='children'))
   ,
   function(n){
     return(n)
@@ -314,9 +310,9 @@ app$callback(
 
 app$callback(
   
-  output=l.(id='text', property='children')
+  output=list(id='text', property='children')
   ,
-  params=l.(input(id='slider', property='value'))
+  params=list(input(id='slider', property='value'))
   ,
   function(value){
     
@@ -330,9 +326,9 @@ app$callback(
 ##LOAD GRAPH
 app$callback(
 
-  output=l.(id='graph', property='figure')
+  output=list(id='graph', property='figure')
   ,
-  params=l.(input(id='slider', property='value'))
+  params=list(input(id='slider', property='value'))
   ,
   function(value)
   {
@@ -365,17 +361,17 @@ app$callback(
     }
     
     if(v %in% 0:4){
-      p <- plotly::add_trace(graph, type='scatter3d', mode='lines', x=x_snd, y=y_snd, z=z_snd, hoverinfo='x+y+z', line=l.(color='#444444'))
+      p <- plotly::add_trace(graph, type='scatter3d', mode='lines', x=x_snd, y=y_snd, z=z_snd, hoverinfo='x+y+z', line=list(color='#444444'))
      
       
     } else{
  
-      p <- plotly::plot_ly(type='contour', x=Dates, y=rev(labels), z=t(DM),  hoverinfo='x+y+z', lighting=l.('ambient'=0.95, 'diffuse'=0.99, 'fresnel'=0.01, 'roughness'=0.01, 'specular'=0.01 ), colorscale=l.(l.(0, 'rgb(230,245,254)'), l.(0.4,'rgb(123,171,203)'), l.(0.8, 'rgb(40,119,174)'), l.(1, 'rgb(37,61,81)')), zmax=9.18, zmin=0, showscale=F,  line=l.(smoothing=1, color='rgba(40,40,40,0.15)'), contours=l.(coloring='heatmap'))
+      p <- plotly::plot_ly(type='contour', x=Dates, y=rev(labels), z=t(DM),  hoverinfo='x+y+z', lighting=list('ambient'=0.95, 'diffuse'=0.99, 'fresnel'=0.01, 'roughness'=0.01, 'specular'=0.01 ), colorscale=list(list(0, 'rgb(230,245,254)'), list(0.4,'rgb(123,171,203)'), list(0.8, 'rgb(40,119,174)'), list(1, 'rgb(37,61,81)')), zmax=9.18, zmin=0, showscale=F,  line=list(smoothing=1, color='rgba(40,40,40,0.15)'), contours=list(coloring='heatmap'))
     
     }
     
-    p <- p %>% plotly::layout(.,autosize=T, font=l.(size=12, color='#CCCCCC'), margin=l.(t=5, l=5, b=5, r=5), showlegend=F, hovermode='closest',
-                              scene=l.(aspectmode='manual', aspectratio=l.(x=2,y=5,z=1.5), camera=l.(up=UPS[[v+1]], center=CENTERS[[v+1]], eye=EYES[[v+1]]),   annotations=l.(l.(
+    p <- p %>% plotly::layout(.,autosize=T, font=list(size=12, color='#CCCCCC'), margin=list(t=5, l=5, b=5, r=5), showlegend=F, hovermode='closest',
+                              scene=list(aspectmode='manual', aspectratio=list(x=2,y=5,z=1.5), camera=list(up=UPS[[v+1]], center=CENTERS[[v+1]], eye=EYES[[v+1]]),   annotations=list(list(
                                 showarrow=F,
                                 y="2015-03-18",
                                 x="1-month",
@@ -384,7 +380,7 @@ app$callback(
                                 xanchor="left",
                                 xshift=10,
                                 opacity=0.7
-                              ), l.(
+                              ), list(
                                 y="2015-03-18",
                                 x="3-month",
                                 z=0.048,
@@ -392,7 +388,7 @@ app$callback(
                                 textangle=0,
                                 ax=0,
                                 ay=-75,
-                                font=l.(
+                                font=list(
                                   color="black",
                                   size=12
                                 ),
@@ -402,7 +398,7 @@ app$callback(
                                 arrowhead=1
                               )),
                              
-                              yaxis=l.(
+                              yaxis=list(
                                 "showgrid"= T,
                                 "title"= "",
                                 "type"= "date",
