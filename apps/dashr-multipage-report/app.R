1ea8ff419f0b331268b26998b9f46ee422e5b76f
Carlone Scott <carlonescott@gmail.com>
2019-07-01 16:23:07 -0400

    Update R

--- apps/dashr-multipage-report/app.R
+++ apps/dashr-multipage-report/app.R
@@ -1797,7 +1797,7 @@ app$layout(htmlDiv(list(
 if (appName != "") {
   app$run_server(host = "0.0.0.0", port = Sys.getenv('PORT', 8050)) 
 } else {
-  app$run_server(debug = T)
+  app$run_server()
 }
 
 
