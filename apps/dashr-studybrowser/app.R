10b0591d99159f776aacd063e95d57fca11b1cb1
Dan Seol <yunheum.seol@gmail.com>
2019-07-02 17:36:51 -0400

    Update app.R

--- apps/dashr-studybrowser/app.R
+++ apps/dashr-studybrowser/app.R
@@ -269,6 +269,6 @@ app$callback(
   
 )
 
-# app$run_server(host = "0.0.0.0", port = Sys.getenv('PORT', 8050))
-app$run_server()
+ app$run_server(host = "0.0.0.0", port = Sys.getenv('PORT', 8050))
+#app$run_server()
 
