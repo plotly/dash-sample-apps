881a311f8f29fa68b66ef22b60bc42844e51dd01
Carlone Scott <carlonescott@plot.ly>
2019-07-02 18:02:05 -0400

    Update init.R

--- apps/dashr-opiod-endemic/init.R
+++ apps/dashr-opiod-endemic/init.R
@@ -11,4 +11,4 @@ remotes::install_github("plotly/dash-core-components")
 remotes::install_github("plotly/dash-table")
 remotes::install_github("plotly/dashR")
 
-install.packages(c("dplyr", "stringr", "glue", "data.table", "plotly"))
+install.packages(c("stringr", "glue"))
