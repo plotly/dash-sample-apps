bb1347259f0cd595ecf17a642ae7e4c0e002d51d
ycaokris <yi.cao2@mail.mcgill.ca>
2019-07-02 13:46:13 -0400

    Disable horizontal scroll across devices

--- apps/dash-medical-provider-charges/app.py
+++ apps/dash-medical-provider-charges/app.py
@@ -11,7 +11,7 @@ import os
 
 app = dash.Dash(
     __name__,
-    meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
+    meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"}],
 )
 server = app.server
 
