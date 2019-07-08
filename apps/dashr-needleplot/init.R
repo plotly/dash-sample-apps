9798c20eaee4d8a322aa97594234dab653159aeb
Lucas Goldstone <lu.goldstone@gmail.com>
2019-07-08 16:06:07 -0400

    Update init.R

--- apps/dashr-needleplot/init.R
+++ apps/dashr-needleplot/init.R
@@ -6,9 +6,10 @@ r <- getOption("repos")
 r["CRAN"] <- "http://cloud.r-project.org"
 options(repos=r)
 
-remotes::install_github("plotly/dash-table", ref="4.0.0-cran")
-remotes::install_github("plotly/dash-html-components", ref="1.0.0-cran")
-remotes::install_github("plotly/dash-core-components", ref="1.0.0-cran")
-remotes::install_github("plotly/dashR", ref="0.1.0-cran")
+remotes::install_github("plotly/dash-html-components")
+remotes::install_github("plotly/dash-core-components")
+remotes::install_github("plotly/dash-table")
+remotes::install_github("plotly/dashR")
+remotes::install_github("plotly/dash-bio")
 
 install.packages("ape")
