3b36a6f3481dd673f64488c5ea371d389f03ecac
Mila Roisin <mila.roisin@gmail.com>
2019-07-09 12:15:15 -0400

    [#177] Basic layout added with functions

--- apps/dashr-shiny-movies/app.R
+++ apps/dashr-shiny-movies/app.R
@@ -13,10 +13,52 @@ library(dashCoreComponents)
 library(dashHtmlComponents)
 library(plotly)
 
-#################################### LOAD DATA & CREATE GLOBAL OBJECTS #############################
+######### LOAD DATA, CREATE FUNCTIONS & GLOBAL OBJECTS ##############
+
 install.packages("ggplot2movies")  #moved its own package to reduce the download size of ggplot2.
 library(ggplot2movies)
 
-#####################################################################################################
+minx <- min(movies$rating)
+maxx <- max(movies$rating)
+
+# size of the bins depend on the input 'bins'
+size <- (maxx - minx) / input$bins
+
+############################################# APP START ##########################
+
+app <- Dash$new()
+
+################################### LAYOUT VARIABLES ##########################
+
+pageTitle <- htmlH1("Movie Ratings!")
+
+plotlyLogo <- htmlA(
+  list(
+    htmlImg(
+      src = "assets/dashR-logo-stripe.png",
+      className = "logo")),
+  href = "https://dashr-docs.herokuapp.com/")
+
+firstP <- htmlP("Number of bins:")
+
+slider <- dccSlider(
+  
+)
+
+##################################################################################################
+app$layout(
+  
+  
+)
+
+
+################## CONDITIONAL STATEMENT FOR APP RUNNING ON CLOUD SERVER & LOCAL #########################
+
+if (appName != "") {
+  app$run_server(host = "0.0.0.0", port = Sys.getenv('PORT', 8050)) 
+} else {
+  app$run_server()
+}
+
 
 
