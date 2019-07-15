d11645148fb8ee8b111fdba4cecc09aad1ff7073
Mila Roisin <mila.roisin@gmail.com>
2019-07-15 15:52:16 -0400

    [#177] Diamonds App Code Restructuring (Clean Code)

--- apps/dashr-shiny-diamond-explorer/app.R
+++ apps/dashr-shiny-diamond-explorer/app.R
@@ -1,88 +1,102 @@
+appName <- Sys.getenv("DASH_APP_NAME")
+pathPrefix <- sprintf("/%s/", appName)
+
+Sys.setenv(DASH_ROUTES_PATHNAME_PREFIX = pathPrefix,
+           DASH_REQUESTS_PATHNAME_PREFIX = pathPrefix)
+
 library(dashR)
 library(dashCoreComponents)
 library(dashHtmlComponents)
 library(plotly)
-#library(dplyr) #package for data manipulation
 
-appName <- Sys.getenv("DASH_APP_NAME")
-if (appName != ""){
-  pathPrefix <- sprintf("/%s/", appName)
-  
-  Sys.setenv(DASH_ROUTES_PATHNAME_PREFIX = pathPrefix,
-             DASH_REQUESTS_PATHNAME_PREFIX = pathPrefix)
-  
-  setwd(sprintf("/app/apps/%s", appName))
-}
+################# LOAD DATA, FUNCTIONS, & CREATE GLOBAL OBJECTS ########
 
 data(diamonds, package = "ggplot2")
 nms <- names(diamonds)
 
-#main application
-external_ss = list('https://codepen.io/chriddyp/pen/bWLwgP.css')
-app <- Dash$new(external_stylesheets=external_ss)
-#myList = list()
-#nms
-lapply(nms, function(x){list(label = x, value = x)})
+lapply(nms, function(x) {
+  list(label = x, value = x)
+})
+
+setwd(
+  "/Users/milaroisin/Documents/GitHub/plot.ly/dash-sample-apps/apps/dashr-shiny-diamond-explorer"
+)
+
+######### CREATE GLOBAL OBJECTS ####
+
+DropDownMenuOptions <- list(
+  list(label = "carat", value = "carat"),
+  list(label = "cut", value = "cut"),
+  list(label = "color", value = "color"),
+  list(label = "clarity", value = "clarity"),
+  list(label = "depth", value = "depth"),
+  list(label = "table", value = "table"),
+  list(label = "price", value = "price"),
+  list(label = "x", value = "x"),
+  list(label = "y", value = "y"),
+  list(label = "z", value = "z")
+)
+
+
+FacetOptions <- list(
+  list(label = "None", value = "None"),
+  list(label = "cut", value = "cut-labels"),
+  list(label = "color", value = "color-labels"),
+  list(label = "clarity", value = "clarity")
+)
+
+SampleSlider <- dccSlider(id = "sample-slider",
+                          min = 1,
+                          max = 53940,
+                          marks = c("1", "10K", "20K", "30K", "40K", "53940"),
+                          value = 10000)
+
+##### CREATE LAYOUT VARIABLES #######
+
+pageTitle <- htmlH2("Diamonds Explorer ")
+
+plotlyLogo <-
+  htmlA(list(htmlImg(id = "banner-image", src = "assets/image.png")), className = "logo",
+        href = "https://dashr-docs.herokuapp.com/")
+
+xDropDown <- dccDropdown(id = "x-dropdown",
+                         options = DropDownMenuOptions,
+                         value = "carat",
+                         clearable = FALSE)
+
+yDropDown <- dccDropdown(id = "y-dropdown",
+                         options = DropDownMenuOptions,
+                         value = "cut",
+                         clearable = FALSE)
+
+FacetDropDown <- dccDropdown(id="facet-row-dropdown",
+                             options = FacetOptions,
+                             value = "None",
+                             clearable = FALSE)
+
+
+###### APP START #######
+
+app = Dash$new()
+
+########### CREATE LAYOUT ##############
 
 app$layout(
-  htmlDiv(htmlImg(src='assets/dash-logo.png')),
-  htmlDiv(htmlH1("Diamonds Explorer")),
-  htmlDiv(htmlLabel("Sample Size")),
   htmlDiv(
-    dccSlider(min = 1, max=nrow(diamonds), step = 500, 
-      marks = as.list(
-        setNames(
-          round(seq(1, nrow(diamonds), length.out = 10),0), 
-          round(seq(1,nrow(diamonds),length.out = 10), 0)
-          )
-        ), value = 1000)
-      ),
-  htmlBr(), htmlBr(),
-  htmlDiv(htmlLabel("X")),
-  htmlDiv(
-    dccDropdown(options = lapply(
-      nms, function(x) {list(label = x, value = x)}
-                                 ),
-                value = "carat")
-    ),
-  htmlBr(),
-  htmlDiv(htmlLabel("Y")),
-  htmlDiv(dccDropdown(options = lapply(nms, function(x) {list(label = x, value = x)}), value = "price")),
-  htmlBr(),
-  htmlDiv(htmlLabel("Color")), 
-  htmlDiv(dccDropdown(options = lapply(nms, function(x) {list(label = x, value = x)}), value="clarity")),
-  htmlBr(),
-  htmlDiv(htmlLabel("Facet Row")),
-  htmlDiv(dccDropdown(options = lapply(nms, function(x) {list(label = x, value = x)}), value="clarity")),
-  htmlBr(),
-  htmlDiv(htmlLabel("Facet Column")),
-  dccDropdown(options = lapply(nms, function(x) {list(label = x, value = x)}), value="None"),
-  htmlBr(),
-  # include marks for R ---- For slider components
-  htmlDiv(htmlLabel("Height of plot (in pixels)")),
-  
-  htmlDiv(
+    className="row",
     list(
-      htmlLabel("Height of plot (in pixels)"),
-      dccSlider(min= 100, max = 2000, marks = c("100", "290", "480", "670", "860", "1,240", "1,620", "2000"), value = "1000")
+      htmlDiv(list(
+        plotlyLogo
+      ))
     )
-  ),
-  
-  htmlBr()
+  )
 )
-  
-#app$callback(
-  # build graph with ggplot syntax
-   #p <- ggplot(dataset(), aes_string(x = input$x, y = input$y, color = input$color)) + 
-            #geom_point()
-   # if at least one facet column/row is specified, add it
-        #facets <- paste(input$facet_row, '~', input$facet_col)
-       # if (facets != '. ~ .') p <- p + facet_grid(facets)
-        
-        #ggplotly(p) %>% 
-       #     layout(height = input$plotHeight, autosize=TRUE))
 
-#adding conditional statement for running app on server and local
+
+################ CALLBACKS #####################
+
+
+########CONDITIONAL STATEMENT FOR APP RUNNING ON CLOUD SERVER & LOCAL
 
 if (appName != "") {
   app$run_server(host = "0.0.0.0", port = Sys.getenv('PORT', 8050)) 
