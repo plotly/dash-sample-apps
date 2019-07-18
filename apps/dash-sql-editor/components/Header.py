6b19bc9998d1833549894d3cd643c473ebf95206
Chris Woo <chriskfwoo@gmail.com>
2019-07-18 16:28:48 -0400

    black the component files

--- apps/dash-sql-editor/components/Header.py
+++ apps/dash-sql-editor/components/Header.py
@@ -5,30 +5,15 @@ from .Column import Column
 def Header(title, app):
     height = 40
     return html.Div(
-        style={
-            'marginTop': 35,
-            'marginBottom': 20,
-            'height': height
-        },
+        style={"marginTop": 35, "marginBottom": 20, "height": height},
         children=[
-            Column(
-                width=6,
-                children=title,
-                style={
-                    'fontSize': 35,
-                    'color': "#50B83C"
-                }
-            ),
+            Column(width=6, children=title, style={"fontSize": 35, "color": "#50B83C"}),
             Column(
                 width=6,
                 children=html.Img(
-                    src=app.get_asset_url('images/dash-logo.png'),
-                    style={
-                        'float': 'right',
-                        'height': height,
-                        'width': 'auto'
-                    }
-                )
-            )
-        ]
+                    src=app.get_asset_url("images/dash-logo.png"),
+                    style={"float": "right", "height": height, "width": "auto"},
+                ),
+            ),
+        ],
     )
--- apps/dash-sql-editor/components/Row.py
+++ apps/dash-sql-editor/components/Row.py
@@ -1,8 +1,5 @@
 import dash_html_components as html
 
+
 def Row(children=None, **kwargs):
-    return html.Div(
-        children,
-        className="row mt-2",
-        **kwargs
-    )
+    return html.Div(children, className="row mt-2", **kwargs)
