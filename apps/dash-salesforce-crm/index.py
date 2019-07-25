29e023b1733b9f4463ce65199fccb7da5dc43130
Mskycoder <gnidihz@gmail.com>
2019-07-24 22:23:12 -0400

    header spacing, tab styling, font-size, tweaks
    Former-commit-id: 0c391795d2f5d48afbbd2eb29d7efae1606d18c6

--- apps/dash-salesforce-crm/index.py
+++ apps/dash-salesforce-crm/index.py
@@ -15,28 +15,24 @@ app.layout = html.Div(
         html.Div(
             className="row header",
             children=[
-                html.Span("CRM App using Salesforce API", className="app-title"),
+                html.Span(
+                    dcc.Markdown("**CRM App** using Salesforce API"),
+                    className="app-title",
+                ),
                 html.Img(src=app.get_asset_url("logo.png")),
+                html.A(
+                    children=html.Button("Learn More"), href="https://plot.ly/dash/"
+                ),
             ],
         ),
         html.Div(
-            [
-                dcc.Tabs(
-                    id="tabs",
-                    style={"height": "20", "verticalAlign": "middle"},
-                    children=[
-                        dcc.Tab(
-                            id="opportubnities_tab",
-                            label="Opportunities",
-                            value="opportunities_tab",
-                        ),
-                        dcc.Tab(id="leads_tab", label="Leads", value="leads_tab"),
-                        dcc.Tab(id="cases_tab", label="Cases", value="cases_tab"),
-                    ],
-                    value="leads_tab",
-                )
-            ],
+            id="tabs",
             className="row tabs_div",
+            children=[
+                dcc.Link(dcc.Markdown("Opportunities"), href="/opportunities"),
+                dcc.Link(dcc.Markdown("Leads"), href="/leads"),
+                dcc.Link(dcc.Markdown("Cases"), href="/cases"),
+            ],
         ),
         dcc.Store(  # opportunities df
             id="opportunities_df",
@@ -48,8 +44,8 @@ app.layout = html.Div(
         dcc.Store(
             id="cases_df", data=sf_manager.get_cases().to_json(orient="split")
         ),  # cases df
-        # Tab content
-        html.Div(id="tab_content", className="row", style={"margin": "2% 3%"}),
+        dcc.Location(id="url", refresh=False),
+        html.Div(id="tab_content"),
         html.Link(
             href="https://use.fontawesome.com/releases/v5.2.0/css/all.css",
             rel="stylesheet",
@@ -68,17 +64,43 @@ app.layout = html.Div(
     style={"margin": "0%"},
 )
 
+# Update the index
+@app.callback(
+    [
+        dash.dependencies.Output("tab_content", "children"),
+        dash.dependencies.Output("tabs", "children"),
+    ],
+    [dash.dependencies.Input("url", "pathname")],
+)
+def display_page(pathname):
+    print("display_page({})".format(pathname))
+    tabs = [
+        dcc.Link(dcc.Markdown("Opportunities"), href="/opportunities"),
+        dcc.Link(dcc.Markdown("Leads"), href="/leads"),
+        dcc.Link(dcc.Markdown("Cases"), href="/cases"),
+    ]
+    if pathname == "/opportunities":
+        tabs[0] = dcc.Link(
+            dcc.Markdown("**&#9632 Opportunities**"), href="/opportunities"
+        )
+        return opportunities.layout, tabs
+    elif pathname == "/cases":
+        tabs[2] = dcc.Link(dcc.Markdown("**&#9632 Cases**"), href="/leads")
+        return cases.layout, tabs
+    tabs[1] = dcc.Link(dcc.Markdown("**&#9632 Leads**"), href="/leads")
+    return leads.layout, tabs
 
-@app.callback(Output("tab_content", "children"), [Input("tabs", "value")])
-def render_content(tab):
-    if tab == "opportunities_tab":
-        return opportunities.layout
-    elif tab == "cases_tab":
-        return cases.layout
-    elif tab == "leads_tab":
-        return leads.layout
-    else:
-        return opportunities.layout
+
+# @app.callback(Output("tab_content", "children"), [Input("tabs", "value")])
+# def render_content(tab):
+#     if tab == "opportunities_tab":
+#         return opportunities.layout
+#     elif tab == "cases_tab":
+#         return cases.layout
+#     elif tab == "leads_tab":
+#         return leads.layout
+#     else:
+#         return opportunities.layout
 
 
 if __name__ == "__main__":
