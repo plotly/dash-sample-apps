cf4a8509671fece9bccf0011544b6e2b34a48c14
celinehuang <celine756@hotmail.com>
2019-08-02 11:11:06 -0400

    add flask to app.py and  server to index.py

--- apps/dash-salesforce-crm/index.py
+++ apps/dash-salesforce-crm/index.py
@@ -2,7 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-from app import sf_manager, app
+from app import sf_manager, app, server
 from panels import opportunities, cases, leads
 
 app.layout = html.Div(
