94d08db40ad05fd889f6535f81d1582e3b5852ce
HammadTheOne <30986043+HammadTheOne@users.noreply.github.com>
2019-08-08 15:51:13 -0400

    Update Dash logos in all dashBio apps (#265)
    * New Plotly/Dash logo.
    
    * Update logo and header.
    
    * Update Clustergram logo and header
    
    * Update README.MD
    
    * Update logo and readme.
    
    * Update README.MD
    
    * Update ManhattanPlot logo/header
    
    * Update mol3d logo/header
    
    * Update needleplot headers/logos
    
    * Update oncoprint logo/header
    
    * Update Speck logo/header
    
    * Updated headers and logo's
    
    * Whitespace fixes
    
    * Update app.R
    
    * Update app.R
    
    * Create plotly-dash-bio-logo.png
    
    * Added logo.png to assets for Alignment-Viewer
    
    * Fixed css issue
    
    * Update general-app-page.css

--- apps/dashr-needleplot/app.R
+++ apps/dashr-needleplot/app.R
@@ -116,13 +116,12 @@ app$layout(
           htmlA(
             id = "dashbio-logo",
             children = list(
-              htmlImg(
-                src = "assets/dashbio_logo_transparent.png" 
-              )
+              htmlImg(src='assets/plotly-dash-bio-logo.png', height = '36', width = '190',
+                      style = list('top' = '10', 'margin-left' = '10px'))
             ),
             href = "/Portal"
           ),
-          htmlH2("Dash Needle Plot"),
+          htmlH2("Needle Plot"),
           htmlA(
             id = "gh-link",
             children = list("View on GitHub"),
