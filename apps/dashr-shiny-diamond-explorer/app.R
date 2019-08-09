678602b378cb6220da49550e0cd7f765d48f6083
Mila Roisin <mila.roisin@gmail.com>
2019-08-08 22:28:03 -0400

    [#177] DE App nearly complete -- running issues on multiple callbacks with input and outputs, to be debugged

--- apps/dashr-shiny-diamond-explorer/app.R
+++ apps/dashr-shiny-diamond-explorer/app.R
@@ -1,11 +1,11 @@
-appName <- Sys.getenv("DASH_APP_NAME")
-if (appName != "") {
-  pathPrefix <- sprintf("/%s/", appName)
+appName <- Sys.getenv('DASH_APP_NAME')
+if (appName != '') {
+  pathPrefix <- sprintf('/%s/', appName)
   
   Sys.setenv(DASH_ROUTES_PATHNAME_PREFIX = pathPrefix,
              DASH_REQUESTS_PATHNAME_PREFIX = pathPrefix)
   
-  setwd(sprintf("/app/apps/%s", appName))
+  setwd(sprintf('/app/apps/%s', appName))
 }
 
 library(dash)
@@ -15,8 +15,14 @@ library(plotly)
 
 ################# LOAD DATA, FUNCTIONS, & CREATE GLOBAL OBJECTS ########
 
-data(diamonds, package = "ggplot2")
+data(diamonds, package = 'ggplot2')
 nms <- names(diamonds)
+
+#Adding Reactive data info, dataset is built in diamonds data
+
+dataset <- reactive({
+  diamonds[sample(nrow(diamonds), input$sampleSize),]
+})
 
 #rendering dropdown lists
 lapply(nms, function(x) {
@@ -26,74 +32,74 @@ lapply(nms, function(x) {
 ######### CREATE GLOBAL OBJECTS ####
 
 DropDownMenuOptions <- list(
-  list(label = "None", value = "None"),
-  list(label = "carat", value = "carat"),
-  list(label = "cut", value = "cut"),
-  list(label = "color", value = "color"),
-  list(label = "clarity", value = "clarity"),
-  list(label = "depth", value = "depth"),
-  list(label = "table", value = "table"),
-  list(label = "price", value = "price"),
-  list(label = "x", value = "x"),
-  list(label = "y", value = "y"),
-  list(label = "z", value = "z")
+  list(label = 'None', value = 'None'),
+  list(label = 'carat', value = 'carat'),
+  list(label = 'cut', value = 'cut'),
+  list(label = 'color', value = 'color'),
+  list(label = 'clarity', value = 'clarity'),
+  list(label = 'depth', value = 'depth'),
+  list(label = 'table', value = 'table'),
+  list(label = 'price', value = 'price'),
+  list(label = 'x', value = 'x'),
+  list(label = 'y', value = 'y'),
+  list(label = 'z', value = 'z')
 )
 
-SampleSlider <- dccSlider(id = "sample-slider",
+SampleSlider <- dccSlider(id = 'sample-slider',
                           min = 1,
                           max = 53940,
                           marks = list(
-                            "1" = "1", 
-                            "10000" = list("label" = "10K"),
-                            "20000" = list("label" = "20K"),
-                            "30000" = list("label" = "30K"),
-                            "40000" = list("label" = "40K"),
-                            "53940" = "53940"),
+                            '1' = '1', 
+                            '10000' = list('label' = '10K'),
+                            '20000' = list('label' = '20K'),
+                            '30000' = list('label' = '30K'),
+                            '40000' = list('label' = '40K'),
+                            '53940' = '53940'),
                           value = 1000
 )
-heightSlider <- dccSlider(id = "height-slider",
+heightSlider <- dccSlider(id = 'height-slider',
                           min = 100,
                           max = 2000,
                           marks = list(
-                            "100" = "100",
-                            "290" = "290", 
-                            "480" = "480",
-                            "670" = "670",
-                            "860" = "860",
-                            "1050" = list("label" = "1,050"),
-                            "1240" = list("label" = "1,240"),
-                            "1430" = list("label" = "1,430"),
-                            "1620" = list("label" = "1,620"),
-                            "1810" = list("label" = "1,810"),
-                            "2000" = list("label" = "2,000")),
+                            '100' = '100',
+                            '290' = '290', 
+                            '480' = '480',
+                            '670' = '670',
+                            '860' = '860',
+                            '1050' = list('label' = '1,050'),
+                            '1240' = list('label' = '1,240'),
+                            '1430' = list('label' = '1,430'),
+                            '1620' = list('label' = '1,620'),
+                            '1810' = list('label' = '1,810'),
+                            '2000' = list('label' = '2,000')),
                           value = 1000
 )
 ##### CREATE LAYOUT VARIABLES #######
 
-pageTitle <- htmlH2("Diamonds Explorer")
+pageTitle <- htmlH2('Diamonds Explorer')
 plotlyLogo <-
-  htmlA(list(htmlImg(id = "banner-image", src = "assets/image.png")), className = "logo",
-        href = "https://dashr.plot.ly")
-xDropDown <- dccDropdown(id = "x-dropdown",
+  htmlA(list(htmlImg(id = 'banner-image', src = 'assets/image.png')), className = 'logo',
+        href = 'https://dashr.plot.ly')
+xDropDown <- dccDropdown(id = 'x-dropdown',
                          options = DropDownMenuOptions,
-                         value = "carat",
+                         value = 'carat',
                          clearable = FALSE)
-yDropDown <- dccDropdown(id = "y-dropdown",
+yDropDown <- dccDropdown(id = 'y-dropdown',
                          options = DropDownMenuOptions,
-                         value = "price",
+                         value = 'price',
                          clearable = FALSE)
-ColorDropDown <- dccDropdown(id="color-dropdown",
+ColorDropDown <- dccDropdown(id='color-dropdown',
                              options = DropDownMenuOptions,
-                             value = "clarity",
+                             value = 'clarity',
                              clearable = FALSE)
 
-FacetRowDropDown <- dccDropdown(id="facet-row-dropdown",
+FacetRowDropDown <- dccDropdown(id='facet-row-dropdown',
                               options = DropDownMenuOptions,
-                              value = "clarity",
+                              value = 'clarity',
                               clearable = FALSE)
-FacetColumnDropDown <- dccDropdown(id ="facet-column-dropdown",
+FacetColumnDropDown <- dccDropdown(id ='facet-column-dropdown',
                                options = DropDownMenuOptions,
-                               value = "None",
+                               value = 'None',
                                clearable = FALSE)
 ###### APP START #######
 app = Dash$new()
@@ -103,51 +109,76 @@ app$layout(
   htmlDiv(list(
       plotlyLogo,
       pageTitle
-      ), className = "twelve columns"
+      ), className = 'twelve columns'
   ),
   htmlDiv(list(
-    htmlb("SampleSize"),
+    htmlb('Sample Size'),
     htmlDiv(SampleSlider, style = list('marginBottom' = 40)
     ),
-    htmlB("X"),
+    htmlB('X'),
     xDropDown,
     htmlBr(),
-    htmlB("Y"),
+    htmlB('Y'),
     yDropDown,
     htmlBr(),
-    htmlB("Color"),
+    htmlB('Color'),
     ColorDropDown,
     htmlBr(),
-    htmlB("FacetRow"),
+    htmlB('FacetRow'),
     FacetRowDropDown,
     htmlBr(),
-    htmlB("FacetColumn"),
+    htmlB('FacetColumn'),
     FacetColumnDropDown,
     htmlBr(),
-    htmlB("Height of plot (in pixels)"),
+    htmlB('Height of plot (in pixels)'),
     heightSlider
-  ), className = "three columns"
+  ), className = 'three columns'
   ),
   htmlDiv(list(
-    dccGraph(id = "scatter-purple"),
-    dccGraph(id = "scatter-blue"),
-    dccGraph(id = "scatter-indigo"),
-    dccGraph(id = "scatter-teal"),
-    dccGraph(id = "scatter-green"),
-    dccGraph(id = "scatter-light-green"),
-    dccGraph(id = "scatter-yellow")
+    dccGraph(id = 'scatter-purple'),
+    dccGraph(id = 'scatter-blue'),
+    dccGraph(id = 'scatter-indigo'),
+    dccGraph(id = 'scatter-teal'),
+    dccGraph(id = 'scatter-green'),
+    dccGraph(id = 'scatter-light-green'),
+    dccGraph(id = 'scatter-yellow')
   ),
-  className = "eight columns"
+  className = 'eight columns'
   )
 )
 ################ CALLBACKS #####################
-#app$callback(
+app$callback(output = list(id = 'scatter-purple', property = 'figure'), 
+             params = list(
+               input(id = 'sample-slider', property = 'value'),
+               input(id = 'height-slider', property = 'value')),
+             
+function(x, y, color, facet_row, facet_col) {
+    
+#graph built with ggplot syntax - note that ggplot layout height/width deprecated, find equivalent
+  p <- ggplot(
+    dataset(), 
+    aes_s(
+      x = x, 
+      y = y, color = color)) + 
+    geom_point()
+
+  # if at least one facet column/row is specified, add it
+  facets <- paste(
+    input$facet_row,
+    '~', 
+    input$facet_col)
   
-#)
+  if (facets != '. ~ .')
+    p <- p + facet_grid(facets)
+
+%>% 
+    
+  layout(height = input$plotHeight, autosize=TRUE)
+})
              
 ########CONDITIONAL STATEMENT FOR APP RUNNING ON CLOUD SERVER & LOCAL
-if (appName != "") {
-  app$run_server(host = "0.0.0.0", port = Sys.getenv('PORT', 8050)) 
+if (appName != '') {
+  app$run_server(host = '0.0.0.0', port = Sys.getenv('PORT', 8050)) 
 } else {
   app$run_server(showcase = TRUE)
 }
