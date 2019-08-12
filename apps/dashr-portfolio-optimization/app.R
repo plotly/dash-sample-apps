0bdc8e577d479e022f8179e9bf4b85edfccf34e5
Lucas Goldstone <lu.goldstone@gmail.com>
2019-08-12 11:44:10 -0400

    Add initial layout and function for portfolio optimization app

---
+++ apps/dashr-portfolio-optimization/app.R
@@ -0,0 +1,233 @@
+library(dash)
+library(dashCoreComponents)
+library(dashHtmlComponents)
+library(data.table)
+
+source("helperFunctions.R")
+
+# Load default data
+load("portfolioData.RData")
+# load list of all symbols
+allSymbols <- readRDS("allSymbols.rds")
+
+
+app <- Dash$new()
+
+app$layout(
+  htmlDiv(
+    className = "container scalable",
+    children = list(
+      htmlDiv(
+        id = "banner-div",
+        className = "banner",
+        children = list(
+          htmlH1(id = "appTitle", "Portfolio Optimization"),
+          htmlImg(src = "assets/plotly_logo_white.png")
+        )
+      ),
+      htmlDiv(
+        id = "app-body",
+        #className = "",
+        children = list(
+          htmlDiv(
+            id = "left-div",
+            children = list(
+              htmlDiv(
+                id = "controls-div",
+                list(
+                  htmlDiv(
+                    className = "control",
+                    children = list(
+                      htmlH6("Number of Permutations"),
+                      dccSlider(
+                        id = "nPermutationsSlider",
+                        value = 500000,
+                        min = 0,
+                        max = 500000,
+                        step = 1000,
+                        marks = list(
+                          "1" = 1,
+                          "100000" = 100000,
+                          "200000" = 200000,
+                          "300000" = 300000,
+                          "400000" = 400000,
+                          "500000" = 500000
+                        )
+                      )
+                    )
+                  ),
+                  htmlDiv(
+                    className = "control",
+                    children = list(
+                      htmlH6("Symbols"),
+                      dccDropdown(
+                        id = "symbolsDropdown",
+                        options = lapply(
+                          allSymbols,
+                          function(symbol){
+                            list(label = symbol, value = symbol)
+                          }
+                        ),
+                        multi=TRUE,
+                        value = colnames(portfolioData$rportfolios)
+                      )
+                    )
+                  ),
+                  htmlDiv(
+                    className = "control",
+                    children = list(
+                      htmlH6("Random Permutation Method"),
+                      dccDropdown(
+                        id = "pMethodDropdown",
+                        options = list(
+                          list(label = "Sample", value = "sample"),
+                          list(label = "Simplex", value = "simplex"),
+                          list(label = "Grid", value = "grid")
+                        ),
+                        value = "sample"
+                      )
+                    )
+                  ),
+                  htmlButton(id = "resample-button", children = "resample"),
+                  dccLoading(
+                    dccStore(id = "data-store", data = portfolioData),
+                  )
+                )
+              ),
+              htmlDiv(
+                className = "plot-outer",
+                id = "diversification-plot-outer",
+                dccLoading(
+                  id = "diversification-loading",
+                  dccGraph(
+                    id = "diversification-plot",
+                    figure = generateDiversificationPlot(portfolioData)
+                  )
+                )
+              )
+            )
+          ),
+          htmlDiv(
+            id = "plots-div",
+            children = list(
+              htmlDiv(
+                className = "plot-outer",
+                id = "frontier-plot-outer",
+                dccLoading(
+                  id = "frontier-loading",
+                  dccGraph(
+                    id = "frontier-plot",
+                    figure = generateFrontierPlot(portfolioData)
+                  )
+                )
+              ),
+              htmlDiv(
+                className = "plot-outer",
+                id = "history-plot-outer",
+                dccLoading(
+                  id = "history-loading",
+                  children = dccGraph(
+                    id = "history-plot",
+                    figure = generateHistoryPlot(portfolioData)
+                  )
+                )
+              )            
+            )
+          )
+        )
+      )
+    ) 
+  )
+)
+
+#app$callback(
+  #output("frontier-plot", "figure"),
+  #list(
+    #input("resample-button", "n_clicks"),
+    #state("nPermutationsSlider", "value"),
+    #state("symbolsDropdown", "value"),
+    #state("pMethodDropdown", "value")
+  #),
+  #function(n_clicks, n_permutations, symbolList, rp_method){
+    #if (unlist(n_clicks) > 0){
+      #returns.data <- getSymbolData(unlist(symbolList))
+      #portfolioData <- generatePortfolios(returns.data, n_permutations, rp_method)
+      #generateFrontierPlot(portfolioData)
+    #}
+  #}
+#)
+
+app$callback(
+  output("data-store", "data"),
+  list(
+    input("resample-button", "n_clicks"),
+    state("nPermutationsSlider", "value"),
+    state("symbolsDropdown", "value"),
+    state("pMethodDropdown", "value")
+    #input("nPermutationsSlider", "value"),
+    #input("symbolsDropdown", "value"),
+    #input("pMethodDropdown", "value")
+  ),
+  function(n_clicks, n_permutations, symbolList, rp_method){
+    if (unlist(n_clicks) > 0){
+      d <- getSymbolData(unlist(symbolList))
+      generatePortfolios(d, n_permutations, rp_method)      
+    }
+  }
+)
+
+app$callback(
+  output("frontier-plot", "figure"),
+  list(
+    input("data-store", "data")
+  ),
+  function(d){
+    d <- list(
+      feasible.sd = as.numeric(d$feasible.sd),
+      feasible.means = as.numeric(d$feasible.means),
+      feasible.sr = as.numeric(d$feasible.sr),
+      eff.frontier = as.data.frame(rbindlist(d$eff.frontier)),
+      eff.frontier.wc = as.data.frame(rbindlist(d$eff.frontier.wc))
+    )
+    generateFrontierPlot(d)
+  }
+)
+
+app$callback(
+  output("diversification-plot", "figure"),
+  list(
+    input("data-store", "data"),
+    state("symbolsDropdown", "value")
+  ),
+  function(d, symbolnames){
+    fw <- as.data.frame(rbindlist(d$frontier.weights))
+    colnames(fw) <- symbolnames
+    d <- list(
+      frontier.weights = fw 
+    )
+    generateDiversificationPlot(d)
+  }
+)
+
+app$callback(
+  output("history-plot", "figure"),
+  list(
+    input("data-store", "data"),
+    state("symbolsDropdown", "value")
+  ),
+  function(d, symbolnames){
+    pdata <- as.data.frame(rbindlist(d$price.data$price.data))
+    pdata <- pdata[2:nrow(pdata),]
+    colnames(pdata) <- symbolnames
+    rownames(pdata) <- unlist(d$dates)
+  d <- list(
+    price.data = list(
+      price.data = pdata 
+    )
+  )
+  generateHistoryPlot(d)
+  }
+)
+
+app$run_server()
+
---
+++ apps/dashr-portfolio-optimization/helperFunctions.R
@@ -0,0 +1,244 @@
+library(PortfolioAnalytics)
+library(quantmod)
+library(PerformanceAnalytics)
+library(zoo)
+library(plotly)
+
+# Get data
+getSymbolData <- function(symbolList){
+  getSymbols(symbolList)
+  # Assign to dataframe
+  # Get adjusted prices
+  p.data <- zoo(get(gsub("^", "", symbolList[1], fixed = TRUE))[,6])
+  for (i in 2:length(symbolList)){
+    p.data <- merge.zoo(p.data, get(gsub("^", "", symbolList[i], fixed = TRUE))[,6])
+  }
+  colnames(p.data) <- symbolList
+  # calculate returns
+  returns.data <- na.omit(CalculateReturns(p.data))
+  colnames(returns.data) <- symbolList
+  list(returns.data = returns.data, price.data = p.data)
+}
+
+# Generate random portfolios and efficient frontiers
+generatePortfolios <- function(d, n_permutations, rp_method = c("sample", "simplex", "grid")){
+  if (class(d) == "list"){
+    returns.data <- d$returns.data
+  } else {
+    returns.data <- d
+  }
+  # Save mean return vector and sample covariance matrix
+  meanReturns <- colMeans(returns.data)
+  covMat <- cov(returns.data)
+  # Create portfolio specification
+  # Start with the names of the assets
+  port <- portfolio.spec(assets = colnames(returns.data))
+  # Add constraints to portfolio
+  # Box
+  port <- add.constraint(port, type = "box", min = 0.05, max = 0.8)
+  # Leverage
+  port <- add.constraint(portfolio = port, type = "full_investment")
+  #port <- add.constraint(portfolio = port, type = "leverage")
+  # Generate random portfolios
+  rportfolios <- random_portfolios(port, permutations = n_permutations, rp_method = rp_method)
+
+  # Get minimum variance portfolio
+  minvar.port <- add.objective(port, type = "risk", name = "var")
+  # Optimize
+  minvar.opt <- optimize.portfolio(returns.data, minvar.port, optimize_method = "random", 
+                                   rp = rportfolios)
+  # Generate maximum return portfolio
+  maxret.port <- add.objective(port, type = "return", name = "mean")
+  # Optimize
+  maxret.opt <- optimize.portfolio(returns.data, maxret.port, optimize_method = "random", 
+                                   rp = rportfolios)
+  # Generate vector of returns
+  minret <- 0.06/100
+  maxret <- maxret.opt$weights %*% meanReturns
+  vec <- seq(minret, as.numeric(maxret), length.out = 100)
+
+  # Generate efficient frontiers
+  eff.frontier <- data.frame(Risk = rep(NA, length(vec)),
+                             Return = rep(NA, length(vec)), 
+                             SharpeRatio = rep(NA, length(vec)))
+
+  frontier.weights <- mat.or.vec(nr = length(vec), nc = ncol(returns.data))
+  colnames(frontier.weights) <- colnames(returns.data)
+
+  for(i in 1:length(vec)){
+    eff.port <- add.constraint(port, type = "return", name = "mean", return_target = vec[i])
+    eff.port <- add.objective(eff.port, type = "risk", name = "var")
+    eff.port <- optimize.portfolio(returns.data, eff.port, optimize_method = "ROI")
+    
+    eff.frontier$Risk[i] <- sqrt(t(eff.port$weights) %*% covMat %*% eff.port$weights)
+    eff.frontier$Return[i] <- eff.port$weights %*% meanReturns
+    eff.frontier$Sharperatio[i] <- eff.port$Return[i] / eff.port$Risk[i]
+    frontier.weights[i,] = eff.port$weights
+    
+    cat("\r", paste(round(i/length(vec) * 100, 0), "% done..."))
+  }
+
+  # repeat w/ weight concentration
+  eff.frontier.wc <- data.frame(Risk = rep(NA, length(vec)),
+                             Return = rep(NA, length(vec)), 
+                             SharpeRatio = rep(NA, length(vec)))
+
+  frontier.weights.wc <- mat.or.vec(nr = length(vec), nc = ncol(returns.data))
+  colnames(frontier.weights.wc) <- colnames(returns.data)
+
+  for(i in 1:length(vec)){
+    eff.port <- add.constraint(port, type = "return", name = "mean", return_target = vec[i])
+    eff.port <- add.objective(eff.port, type = "risk", name = "var")
+    eff.port <- add.objective(eff.port, type = "weight_concentration", name = "HHI",
+                              conc_aversion = 0.001)
+
+    eff.port <- optimize.portfolio(returns.data, eff.port, optimize_method = "ROI")
+    eff.frontier.wc$Risk[i] <- sqrt(t(eff.port$weights) %*% covMat %*% eff.port$weights)
+    eff.frontier.wc$Return[i] <- eff.port$weights %*% meanReturns
+    eff.frontier.wc$Sharperatio[i] <- eff.port$Return[i] / eff.port$Risk[i]
+    frontier.weights.wc[i,] = eff.port$weights
+    cat("\r", paste(round(i/length(vec) * 100, 0), "% done..."))
+  }
+
+  feasible.sd <- apply(rportfolios, 1, function(x){
+    return(sqrt(matrix(x, nrow = 1) %*% covMat %*% matrix(x, ncol = 1)))
+  })
+
+  feasible.means <- apply(rportfolios, 1, function(x){
+    return(x %*% meanReturns)
+  })
+
+  feasible.sr <- feasible.means / feasible.sd
+  dates <- rownames(as.data.frame(returns.data))
+
+  list(
+    feasible.means = feasible.means,
+    feasible.sd = feasible.sd,
+    feasible.sr = feasible.sr,
+    eff.frontier = eff.frontier,
+    eff.frontier.wc = eff.frontier.wc,
+    rportfolios = rportfolios,
+    frontier.weights = frontier.weights,
+    frontier.weights.wc = frontier.weights.wc,
+    #eff.port = eff.port,
+    price.data = d,
+    dates = dates 
+  )
+}
+
+
+# Generate plots:
+generateFrontierPlot <- function(portfolioData){
+  feasible.sd <- as.numeric(portfolioData$feasible.sd)
+  feasible.means <- as.numeric(portfolioData$feasible.means)
+  feasible.sr <- as.numeric(portfolioData$feasible.sr)
+  eff.frontier <- as.data.frame(portfolioData$eff.frontier)
+  eff.frontier.wc <- as.data.frame(portfolioData$eff.frontier.wc)
+  p <- plot_ly(
+    x = feasible.sd, y = feasible.means, color = feasible.sr, 
+    mode = "markers", type = "scattergl", showlegend = FALSE,
+    marker = list(
+      size = 3, opacity = 0.5, colorbar = list(title = "Sharpe Ratio")
+    )
+  ) %>% 
+    add_trace(inherit = FALSE, data = eff.frontier, 
+              x = eff.frontier$Risk, y = eff.frontier$Return, mode = "markers", 
+              type = "scattergl", showlegend = F, 
+              marker = list(color = "#F7C873", size = 5)) %>% 
+    add_trace(inherit = FALSE, data = eff.frontier.wc,
+              x = eff.frontier.wc$Risk, y = eff.frontier.wc$Return, mode = "markers", 
+              type = "scattergl", showlegend = F, 
+              marker = list(color = "#ff471a", size = 5)) %>% 
+    layout(title = "Random Portfolios with Plotly",
+           yaxis = list(title = "Mean Returns", tickformat = ".2%", showgrid = FALSE),
+           xaxis = list(title = "Standard Deviation (Risk)", tickformat = ".2%", showgrid = FALSE),
+           plot_bgcolor = "#F8F8F8",
+           paper_bgcolor = "#F8F8F8",
+           annotations = list(
+             list(x = eff.frontier[75, "Risk"], y = eff.frontier[75, "Return"], 
+                  ax = -30, ay = -30, 
+                  yshift = 10, xshift = 10,
+                  text = "<b>Efficient frontier</b>", 
+                  font = list(color = "black", size = 15),
+                  arrowcolor = "black"),
+             list(x = eff.frontier.wc[70, "Risk"], y = eff.frontier.wc[70, "Return"],
+                  ax = 60, ay = 60, yshift = -10, xshift = -10,
+                  text = "<b>Efficient Frontier\n(with weight conc.)</b>",
+                  font = list(color = "black", size = 15), 
+                  arrowcolor = "black")
+           ))
+    p
+}
+
+generateDiversificationPlot <- function(portfolioData){
+  frontier.weights <- portfolioData$frontier.weights
+  colnamefactor <- as.factor(gsub("\\..*$", "", colnames(frontier.weights)))
+  q <- plot_ly()
+  for (column in dim(frontier.weights)[2]:1){
+    q <- q %>%
+      add_trace(
+        y = frontier.weights[, column],
+        type = "bar",
+        width = 1,
+        color = colnamefactor[column],
+        colors = "Paired",
+        name = gsub("\\..*$", "", colnames(frontier.weights)[column])
+      )
+  }
+  q <- q %>%
+    layout(title = "Portfolio weights across frontier", barmode = "stack",
+           plot_bgcolor = "#F8F8F8",
+           paper_bgcolor = "#F8F8F8",
+           xaxis = list(title = "Index"),
+           yaxis = list(title = "Weights(%)", tickformat = ".0%"))
+  q
+}
+
+generateHistoryPlot <- function(portfolioData){
+  d <- as.data.frame(portfolioData$price.data$price.data)
+  colnamefactor <- as.factor(gsub("\\..*$", "", colnames(d)))
+  p <- plot_ly()
+  for (column in 1:dim(d)[2]){
+    p <- p %>%
+      add_trace(
+        x = rownames(d),
+        #x = as.Date(portfolioData$dates),
+        y = d[,column],
+        type = "scatter",
+        mode = "lines",
+        color = colnamefactor[column],
+        colors = "Paired",
+        name = gsub("\\..*$", "", colnames(d)[column])
+      )
+  }
+  p <- p %>%
+    layout(
+      title = "Historical Performance",
+      xaxis = list(
+        showgrid = FALSE,
+        type = "date",
+        format = "%Y"
+        ),
+      yaxis = list(
+        showgrid = FALSE, 
+        title = "Adjusted Price", 
+        tickformat = "$"
+      ),
+      plot_bgcolor = "#F8F8F8",
+      paper_bgcolor = "#F8F8F8"
+    )
+}
+
+getAllSymbols <- function(){
+  c(stockSymbols()[,1], list("^GSPC"))
+}
+
+# Generate Default data
+# 500000 permutations
+#symbolList <- c("MSFT", "SBUX", "IBM", "AAPL", "^GSPC", "AMZN")
+#x <- getSymbolData(symbolList)
+#portfolioData <- generatePortfolios(x, n_permutations = 500000, rp_method = "sample")
+#save(portfolioData, file = "portfolioData.RData")
+
+#allSymbols <- getAllSymbols()
+#saveRDS(allSymbols, file = "allSymbols.rds")
