4adcb5aa031b0b07c7d6cd241895a8d106afdd35
matthiaslmz <matthiaslmz@hotmail.com>
2019-08-21 14:25:32 -0400

    update files for deployment

---
+++ apps/dashr-shiny-undata-advanced/app.R
@@ -0,0 +1,132 @@
+appName <- Sys.getenv("DASH_APP_NAME")
+if (appName != "") {
+  pathPrefix <- sprintf("/%s/", appName)
+  Sys.setenv(
+    DASH_ROUTES_PATHNAME_PREFIX = pathPrefix,
+    DASH_REQUESTS_PATHNAME_PREFIX = pathPrefix
+  )
+  setwd(sprintf("/app/apps/%s", appName))
+}
+
+library(dash)
+library(dashCoreComponents)
+library(dashHtmlComponents)
+library(plotly)
+
+ideal <- read.csv("Data/UN_IdealPoints.csv", stringsAsFactors = F)
+
+app <- Dash$new()
+
+app$layout(
+  htmlA(list(
+    htmlImg(id = "banner-image", src = "assets/image.png")
+  ), className = "logo",
+  href = "https://dashr-docs.herokuapp.com/"),
+  htmlDiv(list(htmlH1('Ideal Points Estimation'))),
+  htmlDiv(list(htmlH4('Country Name(s) of Interest'))),
+  htmlDiv(list(
+    htmlDiv(
+      list(
+        dccDropdown(
+          id = 'country-dropdown',
+          className = 'app-controls-block-dropdown',
+          options = lapply(as.character(unique(ideal$Name)), function(x) {
+            list(label = x, value = x)
+          }),
+          value = "United States of America",
+          multi = TRUE,
+          clearable = TRUE,
+          searchable = TRUE
+        ),
+        htmlBr(),
+        htmlDiv(list(dccGraph(id = 'barplot'))),
+        htmlBr(),
+        htmlP(
+          "Data: Bailey, Michael, Anton  Strezhnev and Erik Voeten. Forthcoming. 'Estimating Dynamic State Preferences from United Nations Voting Data.' Journal of Conflict Resolution."
+        )
+      ),
+      style = list('marginBottom' = 50),
+      className = "four columns"
+    ),
+    htmlDiv(list(dccGraph(id = 'lineplot')),
+            className = 'seven columns')
+  ), className = "row")
+)
+
+app$callback(
+  output = list(id = "lineplot", property = "figure"),
+  params = list(input(id = "country-dropdown", property = "value")),
+  function(selected_country) {
+    filtered_df <- ideal %>% filter(Name %in% selected_country)
+
+    p <- plot_ly(
+      filtered_df,
+      x = ~ Year,
+      y = ~ Ideal.point,
+      color = ~ Name,
+      hovertemplate = paste(
+        'Year: %{x}',
+        '<br>Ideal.point: %{y}',
+        '<br>Name:',
+        filtered_df$Name
+      ),
+      type = "scatter",
+      mode = 'lines'
+    ) %>%
+      layout(
+        title = "Ideal Points for Countries",
+        yaxis = list(title = 'Ideology'),
+        showlegend = FALSE
+      )
+
+    #Manually access and modify plot's underlying spec.
+    pl <- plotly_build(p)$x
+    pl$layout$annotations <- list()
+
+    #Manually adding annotations
+    for(i in 1:length(pl$data)){
+      pl$layout$annotations[[i]] <- list(
+        text = pl$data[[i]]$name,  # The text label of the annotation, e.g. "Canada"
+        font = list(color = pl$data[[i]]$line$color), # Match the font color to the line color
+        showarrow = FALSE, # Don't show the annotation arrow
+        y = pl$data[[i]]$y[[length(pl$data[[i]]$y)]], # set the y position of the annotation to the last point of the line
+        yref = "y1", # the "y" coordinates above are with respect to the yaxis
+        x = 1, # set the x position of the graph to the right hand side of the graph
+        xref = "paper", # the x coordinates are with respect to the "paper", where 1 means the right hand side of the graph and 0 means the left hand side
+        xanchor = "left" # position the x coordinate with respect to the left of the text
+      )
+    }
+    # increase the size of the right margin to accommodate more room for the annotation labels
+    pl$layout$margin$r <- 170
+
+    return(as_widget(pl))
+  }
+)
+
+app$callback(
+  output = list(id = "barplot", property = "figure"),
+  params = list(input(id = "country-dropdown", property = "value")),
+  function(selected_country) {
+    filtered_df <- ideal %>%
+      filter(Name %in% selected_country) %>%
+      group_by(Name) %>%
+      summarise(terms = n())
+
+    p <- plot_ly(
+      filtered_df,
+      x = ~ terms,
+      y = ~ Name,
+      type = "bar",
+      orientation = 'horizontal'
+    ) %>%
+      layout(xaxis = list(title = 'Terms (in years)'),
+             yaxis = list(title = ''))
+    return(p)
+  }
+)
+
+if (appName != "") {
+  app$run_server(host = "0.0.0.0", port = Sys.getenv('PORT', 8050))
+} else {
+  app$run_server(showcase = TRUE)
+}
--- apps/dashr-shiny-undata-advanced/init.R
+++ apps/dashr-shiny-undata-advanced/init.R
@@ -7,34 +7,8 @@ r["CRAN"] <- "http://cloud.r-project.org"
 options(repos=r)
 
 # ======================================================================
-
 # packages go here
-install.packages("remotes")
-install.packages("ggplot2")
-
-# installs crayon, digest, htmltools, mime, sourcetools, xtable
-remotes::install_version("shiny", version = "1.2.0", repos = "http://cloud.r-project.org", upgrade="never")
-
-# installs askpass, assertthat, base64enc, cli, colorspace, crosstalk, curl, data.table, dplyr, fansi, ggplot2, glue, gtable, hexbin, htmlwidgets, httr, labeling, lattice, lazyeval, mgcv, munsell, nlme, openssl, pillar, pkgconfig, plogr, plyr, purrr, RColorBrewer, reshape2, scales, stringi, stringr, sys, tibble, tidyr, tidyselect, utf8, viridisLite, withr, yaml
-remotes::install_version("plotly", version = "4.9.0", repos = "http://cloud.r-project.org", upgrade="never")
-
-install.packages("https://cloud.r-project.org/src/contrib/assertthat_0.2.1.tar.gz", type="source", repos=NULL)
-install.packages("https://cloud.r-project.org/src/contrib/xml2_1.2.0.tar.gz", type="source", repos=NULL)
-install.packages("https://cloud.r-project.org/src/contrib/triebeard_0.3.0.tar.gz", type="source", repos=NULL)
-install.packages("https://cloud.r-project.org/src/contrib/Archive/urltools/urltools_1.7.2.tar.gz", type="source", repos=NULL)
-install.packages("https://cloud.r-project.org/src/contrib/jsonlite_1.6.tar.gz", type="source", repos=NULL)
-install.packages("https://cloud.r-project.org/src/contrib/webutils_0.6.tar.gz", type="source", repos=NULL)
-install.packages("https://cloud.r-project.org/src/contrib/brotli_1.2.tar.gz", type="source", repos=NULL)
-install.packages("https://cloud.r-project.org/src/contrib/reqres_0.2.2.tar.gz", type="source", repos=NULL)
-install.packages("https://cloud.r-project.org/src/contrib/uuid_0.1-2.tar.gz", type="source", repos=NULL)
-install.packages("https://cloud.r-project.org/src/contrib/base64enc_0.1-3.tar.gz", type="source", repos=NULL)
-install.packages("https://cloud.r-project.org/src/contrib/codetools_0.2-16.tar.gz", type="source", repos=NULL)
-install.packages("https://cloud.r-project.org/src/contrib/globals_0.12.4.tar.gz", type="source", repos=NULL)
-install.packages("https://cloud.r-project.org/src/contrib/Archive/future/future_1.11.1.1.tar.gz", type="source", repos=NULL)
-
-#dashR, dashCore and dashHTML 
-
-remotes::install_github("plotly/dash-html-components", ref="1.0.0-cran")
-remotes::install_github("plotly/dash-core-components", ref="1.0.0-cran")
-remotes::install_github("plotly/dashR", ref="0.1.0-cran")
-
+remotes::install_github("plotly/dashR", dependencies=TRUE)
+remotes::install_github("plotly/dash-html-components")
+remotes::install_github("plotly/dash-core-components")
+install.packages("plotly")
