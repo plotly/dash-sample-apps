4860130ad79dbe043b35de1800e5842ee40519a5
matthiaslmz <matthiaslmz@hotmail.com>
2019-08-30 15:19:27 -0400

    update code

--- apps/dashr-shiny-mapsclick/app.R
+++ apps/dashr-shiny-mapsclick/app.R
@@ -1,66 +1,64 @@
 appName <- Sys.getenv("DASH_APP_NAME")
 if (appName != "") {
   pathPrefix <- sprintf("/%s/", appName)
-  
-  Sys.setenv(DASH_ROUTES_PATHNAME_PREFIX = pathPrefix,
-             DASH_REQUESTS_PATHNAME_PREFIX = pathPrefix)
-  
+  Sys.setenv(
+    DASH_ROUTES_PATHNAME_PREFIX = pathPrefix,
+    DASH_REQUESTS_PATHNAME_PREFIX = pathPrefix
+  )
   setwd(sprintf("/app/apps/%s", appName))
 }
 
-library(plotly)
 library(dash)
 library(dashCoreComponents)
 library(dashHtmlComponents)
+library(plotly)
 
-#################################### LOAD DATA & CREATE GLOBAL OBJECTS #############################
-
-
-####################################################################################################
-
-
-
-#################################### APP START #####################################################
-
-app <- Dash$new(name = "dashr-shiny-mapsclick")
-# Initiate application
-
-####################################################################################################
-
-
-
-
-#################################### CREATE LAYOUT VARIABLES #######################################
-
-plotlyLogo <-
-  htmlA(list(htmlImg(id = 'banner-image', src = 'assets/image.png')), className = 'logo',
-        href = 'https://dashr.plot.ly')
-
-pageTitle <- g
-
-#################################### CREATE LAYOUT###################################################
+app <- Dash$new()
 
 app$layout(
+  htmlA(list(
+    htmlImg(id = "banner-image", src = "assets/image.png")
+  ), className = "logo",
+  href = "https://dashr.plot.ly/"),
   htmlDiv(list(
-    plotlyLogo,
-    pageTitle,
-  ), className = "twelve columns"),
-  
+    dccGraph(
+      id = 'mapplot',
+      figure = plot_ly(
+        z = state.area,
+        text = state.name,
+        locations = state.abb,
+        type = 'choropleth',
+        locationmode = 'USA-states'
+      ) %>%
+        layout(geo = list(
+          scope = 'usa',
+          projection = list(type = 'albers usa'),
+          lakecolor = toRGB('white')
+        ))
+    ),
+    htmlDiv(id = "clickdataOutput")
+  ))
 )
-
-
-
-#################################### CALLBACKS START ###############################################
 
 app$callback(
-  output = list(id = , property =),
-  params = list(input(id = , property = ))
+  output = list(id = "clickdataOutput", property = "children"),
+  params = list(input(id = "mapplot", property = "clickData")),
+  function(click_data) {
+    if (is.null(click_data[[1]])) {
+      d <- "Click on a state to view event data"
+    } else {
+      d <- paste(
+        "Curve Number:", click_data$points[[1]]$curveNumber,
+        "Point Number:", click_data$points[[1]]$pointNumber,
+        "z:", click_data$points[[1]]$z
+      )
+    }
+    return(d)
+  }
 )
 
-####################################################################################################
-
 if (appName != "") {
-  app$run_server(host = "0.0.0.0", port = Sys.getenv('PORT', 8050)) 
+  app$run_server(host = "0.0.0.0", port = Sys.getenv('PORT', 8050))
 } else {
-  app$run_server()
+  app$run_server(showcase = TRUE)
 }
