6716f0d05f3ad74afe2cc795456e1d88563b8974
CanerIrfanoglu <canerirfanoglu@gmail.com>
2019-09-09 16:58:59 -0400

    Made app functional

--- apps/dashr-shiny-linked-histogram/app.R
+++ apps/dashr-shiny-linked-histogram/app.R
@@ -1,12 +1,13 @@
 appName <- Sys.getenv("DASH_APP_NAME")
 if (appName != "") {
   pathPrefix <- sprintf("/%s/", appName)
-  
+
   Sys.setenv(DASH_ROUTES_PATHNAME_PREFIX = pathPrefix,
              DASH_REQUESTS_PATHNAME_PREFIX = pathPrefix)
-  
+
   setwd(sprintf("/app/apps/%s", appName))
 }
+
 
 library(plotly)
 library(dash)
@@ -14,191 +15,174 @@ library(dashCoreComponents)
 library(dashHtmlComponents)
 
 
-#################################### LOAD DATA & CREATE GLOBAL OBJECTS #############################
+# Load data ---------------------------
 
-#cars {dataset - speed and stopping distances of cars}
 data(cars)
-head(cars)
-summary(cars)
 
-#convenience function for computing xbin/ybin object given a number of bins
 
+# Helper function(s) ---------------------------
+
+# Generates min, max, size for xbins/ybins attributes of plot_ly histogram
 compute_bins <- function(x, n) {
   list(
     start = min(x),
-    end = min(x),
-    size = (max(x) - min(x)) / n)
+    end = max(x),
+    size = (max(x) - min(x)) / n
+  )
 }
 
 
-x <- cars$speed
+# Helper variables ---------------------------
 
-y <- cars$dist
-
-#marker objects 
+# Marker objects
 m <- list(color = toRGB("black"))
-m2 <- list(color = toRGN("black", 0.2))
-
-####################################################################################################
+m2 <- list(color = toRGB("black", 0.2))
 
 
+# App Start ---------------------------
 
-#################################### APP START #####################################################
-
-app <- Dash$new(name = "dashr-shiny-linked-histogram")
 # Initiate application
+app <- Dash$new(name = "dashr-shiny-linked-histogram")
 
-####################################################################################################
 
-#################################### CREATE LAYOUT VARIABLES #######################################
+# Create Layout Variables ---------------------------
 
-plotlyLogo <-
-  htmlA(list(htmlImg(id = 'banner-image', src = 'assets/image.png')), className = 'logo',
-        href = 'https://dashr.plot.ly')
+plotlyLogo <- htmlA(
+  list(htmlImg(
+  id = 'banner-image', src = 'assets/image.png')),
+  className = 'logo',
+  href = 'https://dashr.plot.ly')
 
-pageTitle <- htmlH2("Linked highlighting with plotly and DashR")
+pageTitle <- htmlH2("Linked highlighting with plotly and Dash for R")
 
 firstP <- htmlB("Number of x bins")
 
 secondP <- htmlB("Number of y bins")
 
-xSlider <- dccSlider(id = "x-slider",
-                     min = 1,
-                     max = 50,
-                     marks = list(
-                       '1' = '1',
-                       '6' = '6',
-                       '11' = '11',
-                       '16' = '16',
-                       '21' = '21',
-                       '26' = '26',
-                       '31' = '31',
-                       '36' = '36',
-                       '41' = '41',
-                       '46' = '46',
-                       '50' = '50'
-                       ),
-                     value = 20)
+xSlider <- dccSlider(
+  id = "x-slider",
+  min = 1,
+  max = 50,
+  marks = setNames(c(as.list(seq(1, 50, 5)), as.list(50)),
+                  c(seq(1, 50, 5), 50)),
+  value = 20
+)
 
 ySlider <- dccSlider(
   id = "y-slider",
   min = 1,
   max = 50,
-  marks = list(
-    '1' = '1',
-    '6' = '6',
-    '11' = '11',
-    '16' = '16',
-    '21' = '21',
-    '26' = '26',
-    '31' = '31',
-    '36' = '36',
-    '41' = '41',
-    '46' = '46',
-    '50' = '50'
-  ),
+  marks = setNames(c(as.list(seq(1, 50, 5)), as.list(50)),
+                   c(seq(1, 50, 5), 50)),
   value = 20
 )
 
-xHistogram -> dccGraph(
+xHistogram <- dccGraph(
   id = 'x-histogram',
-  figure = list(
-    data = cars,
-    type = 'bar',
-  )
+  p <- plot_ly(x = cars$speed, type = "histogram", autobinx = FALSE,
+               xbins = compute_bins(cars$speed, length(unique(cars$speed))), marker = m2) %>%
+         layout("yaxis" = list("title" = "count")),
+  figure = p
 )
 
-scatterGraph -> dccGraph(
-  id = 'scatter-graph',
-  figure = list(
-    data = cars,
-    layout = list(
-      xaxis = list ('title' = 'speed'),
-      yaxis = list('title' = 'dist'),
-    )
-  )
-  
-)
-
-yHistogram -> dccGraph(
+yHistogram <- dccGraph(
   id = 'y-histogram',
-  figure = list(
-    data = cars,
-    type = 'bar',
+  p <- plot_ly(y = cars$dist, type = "histogram", autobiny = F,
+               ybins = compute_bins(cars$dist, length(unique(cars$dist))), marker = m2) %>%
+         layout("xaxis" = list("title" = "count")),
+  figure = p
   )
-  
+
+scatterGraph <- dccGraph(
+  id = 'scatter-graph',
+  figure = plot_ly(cars, x = ~speed, y = ~dist,
+                   mode = "markers", marker = m) %>%
+    layout(dragmode = "select")
 )
-#################################### CREATE LAYOUT###################################################
+
+
+# Create Layout ---------------------------
 
 app$layout(htmlDiv(
   list(
     plotlyLogo,
     pageTitle,
-    xHistogram,
-    firstP,
     htmlBr(),
-    htmlDiv(list(xSlider), style=list('marginBottom'= 50, className = 'five columns')),
-    secondP,
-    htmlBr(),
-    htmlDiv(list(ySlider), style=list('marginBottom'= 50, className = 'seven columns')),
-    scatterGraph,
-    yHistogram,
+    htmlDiv(
+      className = "row",
+      style = list("width" = "650px"),
+      list(
+        htmlDiv(xHistogram,
+                className = "eight columns" ),
+        htmlDiv(list(firstP, xSlider, htmlBr(), secondP, ySlider),
+                className = 'four columns')
+      )
+    ),
+    htmlDiv(
+      className = "row",
+      style = list("width" = "650px"),
+      list(
+        htmlDiv(scatterGraph,
+                className = "seven columns"),
+        htmlDiv(
+          yHistogram,
+          #style = list('marginBottom'= 50),
+          className = 'five columns')
+      )
+    )
   )
 ))
 
-#################################### CALLBACKS START ###############################################
 
+# Callbacks Start ---------------------------
+
+# the 'x' histogram
 app$callback(
   output = list(id = 'x-histogram', property = 'figure'),
-  params = list(
-        input(id = 'x-slider', property = 'value')),
+  params = list(input(id = 'x-slider', property = 'value'),
+                input(id = 'scatter-graph', property = 'selectedData')),
 
-function(xbins) {
-xbins <- compute_bins(x)
-p <- plot_ly(x = x, type = "histogram", autobinx = F,
-             xbins = xbins, marker = m2)
+  function(xbins, selected) {
+    p <- plot_ly(x = cars$speed, type = "histogram", autobiny = F,
+                 xbins = compute_bins(cars$speed, xbins), marker = m2)
 
-# obtain plotlyjs selection
-
-s <- event_data("plotly_selected")
-
-# if points are selected, subset the data, and highlight
-if (length(s) > 0) {
-  p <- add_trace(p, x = s$x, type = "histogram", autobinx = F,
-                 xbins = xbins, marker = m)
-}
-p %>%
-  config(displayModeBar = F, showLink = F) %>%
-  layout(showlegend = F, barmode = "overlay", yaxis = list(title = "count"),
-         xaxis = list(title = "", showticklabels = F))
+    # if points are selected, subset the data, and highlight
+    if (!is.null(selected[[1]])) {
+      p <- add_trace(p, x = fromJSON(toJSON(selected$points))$x, type = "histogram", autobiny = F,
+                     xbins = compute_bins(cars$speed, xbins), marker = m)
+      p <- config(p, displayModeBar = F, showLink = F) %>%
+           layout(showlegend = F, barmode = "overlay",
+             yaxis = list(title = "count"),
+             xaxis = list(title = "", showticklabels = F))
+    }
+    return(p)
 })
 
+# the 'y' histogram
 app$callback(
-    output = list(id = 'y-histogram', property = 'figure'),
-    input = list(id = 'y-slider', property = 'value'),
+  output = list(id = 'y-histogram', property = 'figure'),
+  params = list(input(id = 'y-slider', property = 'value'),
+                input(id = 'scatter-graph', property = 'selectedData')),
 
-function(ybins)  {
+  function(ybins, selected) {
+    p <- plot_ly(y = cars$dist, type = "histogram", autobiny = F,
+                 ybins = compute_bins(cars$dist, ybins), marker = m2)
 
-ybins <- compute_bins(y, input$ybins)
-p <- plot_ly(y = y, type = "histogram", autobiny = F,
-             ybins = ybins, marker = m2)
-s <- event_data("plotly_selected")
-if (length(s$y) > 0) {
-  p <- add_trace(p, y = y, type = "histogram", autobiny = F,
-                 ybins = ybins, marker = m)
-}
-p %>%
-  config(displayModeBar = F, showLink = F) %>%
-  layout(showlegend = F, barmode = "overlay", xaxis = list(title = "count"),
-         yaxis = list(title = "", showticklabels = F))
-})
+    # if points are selected, subset the data, and highlight
+    if (!is.null(selected[[1]])) {
+      p <- add_trace(p, y = fromJSON(toJSON(selected$points))$y, type = "histogram", autobiny = F,
+                     ybins = compute_bins(cars$dist, ybins), marker = m)
+      p <- config(p, displayModeBar = F, showLink = F) %>%
+        layout(showlegend = F, barmode = "overlay",
+               yaxis = list(title = "count"),
+               xaxis = list(title = "", showticklabels = F))
+    }
+    return(p)
+  })
 
- 
-
-####################################################################################################
 
 if (appName != "") {
-  app$run_server(host = "0.0.0.0", port = Sys.getenv('PORT', 8050)) 
+  app$run_server(host = "0.0.0.0", port = Sys.getenv('PORT', 8050))
 } else {
   app$run_server()
 }
