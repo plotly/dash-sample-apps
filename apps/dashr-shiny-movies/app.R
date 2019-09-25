f238432ebae83bf232a9ee38dccdd9d323d35dec
CanerIrfanoglu <canerirfanoglu@gmail.com>
2019-09-25 16:34:18 -0400

    Edits to app.R

--- apps/dashr-shiny-movies/app.R
+++ apps/dashr-shiny-movies/app.R
@@ -1,12 +1,12 @@
 appName <- Sys.getenv("DASH_APP_NAME")
 if (appName != "") {
   pathPrefix <- sprintf("/%s/", appName)
-  
+
   Sys.setenv(
     DASH_ROUTES_PATHNAME_PREFIX = pathPrefix,
     DASH_REQUESTS_PATHNAME_PREFIX = pathPrefix
   )
-  
+
   setwd(sprintf("/app/apps/%s", appName))
 }
 
@@ -14,94 +14,97 @@ library(dash)
 library(dashCoreComponents)
 library(dashHtmlComponents)
 library(plotly)
-
-######### LOAD DATA, CREATE FUNCTIONS & GLOBAL OBJECTS ##############
-
-library(ggplot2movies)
+library(ggplot2movies) # contains movies data
 
 
-minx <- min(movies$rating)
-maxx <- max(movies$rating)
+# Load & Prep Data ------------------------------
 
-############################################# APP START ##########################
+moviesDf <- as.data.frame(ggplot2movies::movies)
 
+
+# App Start ------------------------------
+
+# Initiate application
 app <- Dash$new()
 
-################################### LAYOUT VARIABLES ##########################
+
+# Create Layout Variables ------------------------------
 
 pageTitle <- htmlH2("Movie Ratings!")
 
-plotlyLogo <-
-  htmlA(list(htmlImg(id = "banner-image", src = "assets/image.png")), className = "logo",
-        href = "https://dashr-docs.herokuapp.com/")
+plotlyLogo <- htmlA(
+  list(htmlImg( id = "banner-image", src = "assets/image.png")),
+  className = "logo",
+  href = "https://dashr-docs.herokuapp.com/")
 
-firstP <- htmlDiv(htmlLabel("Number of bins:"), htmlBr())
+
+firstP <- htmlDiv(htmlLabel("Number of bins:"))
+
 
 slider <- dccSlider(
   id = "movies-slider",
   min = 1,
   max = 50,
-  marks = list(
-    '1' = '1',
-    '6' = '6',
-    '11' = '11',
-    '16' = '16',
-    '21' = '21',
-    '26' = '26',
-    '31' = '31',
-    '36' = '36',
-    '41' = '41',
-    '46' = '46',
-    '50' = '50'
-  ),
+  marks = setNames(c(as.list(seq(1, 50, 5)), as.list(50)),
+                   c(seq(1, 50, 5), 50)),
   value = 10
 )
 
-##################################################################################################
+
+
+# Create Layout ------------------------------
+
 app$layout(htmlDiv(
   list(
     plotlyLogo,
     pageTitle,
     firstP,
-    htmlDiv(list(slider), style=list('marginBottom' = 50), className = "five columns"),
-    htmlDiv(list(dccGraph(id = 'histogram')), className = 'seven columns')
+    htmlDiv(list(
+      htmlDiv(list(slider),
+              style = list("marginBottom" = 50),
+              className = "three columns"),
+      htmlDiv(list(dccGraph(id = "histogram")),
+              className = "six columns")),
+      className = "row"
+    )
   )
 ))
 
-################## CALLBACKS ##################
+
+# Callbacks Start ------------------------------
 
 app$callback(output = list(id = "histogram", property = "figure"),
              params = list(input(id = "movies-slider", property = "value")),
-             
-             function(bins) {
-               size <- (maxx - minx) / bins
-               
-               # a simple histogram of movie ratings
-               p <- plot_ly(
-                 movies,
-                 x = movies$rating,
-                 autobinx = FALSE,
-                 type = "histogram",
-                 xbins = list(
-                   start = minx,
-                   end = maxx,
-                   size = size
-                 )
-               ) %>% layout(xaxis = list(
-                 title = "Ratings",
-                 range = c(minx, maxx),
-                 autotick = FALSE,
-                 tick0 = minx,
-                 dtick = size
-               ))
-               return(p)
-             })
 
-################## CONDITIONAL STATEMENT FOR APP RUNNING ON CLOUD SERVER & LOCAL #########################
+  function(bins) {
+
+    minx <- min(movies$rating)
+    maxx <- max(movies$rating)
+    size <- (maxx - minx) / bins
+
+    # Simple histogram of movie ratings
+    p <- plot_ly(
+      movies,
+      x = movies$rating,
+      autobinx = FALSE,
+      type = "histogram",
+      xbins = list(
+        start = minx,
+        end = maxx,
+        size = size
+      )
+    ) %>% layout(xaxis = list(
+      title = "Ratings",
+      range = c(minx, maxx),
+      autotick = FALSE,
+      tick0 = minx,
+      dtick = size
+      ))
+    return(p)
+})
 
 if (appName != "") {
-  app$run_server(host = "0.0.0.0", port = Sys.getenv('PORT', 8050))
+  app$run_server(host = "0.0.0.0", port = Sys.getenv("PORT", 8050))
 } else {
   app$run_server()
 }
-
