ca9224d0c48ddc0002f0ff79e041bd938da12e7f
Arvid Hahné <15071547+maanrijp@users.noreply.github.com>
2019-10-16 07:15:07 +0200

    removed unused var server
    it does not do anything

--- apps/dash-financial-report/app.py
+++ apps/dash-financial-report/app.py
@@ -15,7 +15,6 @@ from pages import (
 app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
 )
-server = app.server
 
 # Describe the layout/ UI of the app
 app.layout = html.Div(
