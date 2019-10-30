3b2508f5b3a1a8d2d9023c58ea1a1d6b9b3abb49
Johan Lundström <johan@vild.ly>
2019-10-30 12:23:02 +0100

    progress on LDA precomputing
    Former-commit-id: aee5fa83c8cc273c38dd43b23b68e30e7519ba2f

--- apps/dash-nlp/ldacomplaints.py
+++ apps/dash-nlp/ldacomplaints.py
@@ -48,6 +48,7 @@ def lda_analysis(df, stop_words):
         return tokens
 
     # Clean up and take only rows where we have text
+    print(df.head())
     df = df[pd.notnull(df["Consumer complaint narrative"])]
     docs = list(df["Consumer complaint narrative"].values)
 
@@ -55,10 +56,18 @@ def lda_analysis(df, stop_words):
 
     processed_docs = list(map(cleanup_text, docs))
     print("len(processed_docs)", len(processed_docs))
+    if len(processed_docs) < 11:
+        print("NOT ENOUGH DOCS TO RUN LDA")
+        return(None, None, None, None)
 
     dictionary = gensim.corpora.Dictionary(processed_docs)
-    dictionary.filter_extremes(no_below=10, no_above=0.95, keep_n=100000)
+    #dictionary.filter_extremes(no_below=10, no_above=0.95, keep_n=100000)
     bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+    print("len(bow_corpus)", len(bow_corpus))
+    print("dictionary", len(list(dictionary.keys())))
+    if len(list(dictionary.keys())) < 1:
+        print("NOT ENOUGH DICTS TO RUN LDA")
+        return(None, None, None, None)
 
     lda_model = gensim.models.LdaModel(
         bow_corpus, num_topics=5, id2word=dictionary, passes=10
@@ -70,8 +79,8 @@ def lda_analysis(df, stop_words):
         texts=docs,
         dates=list(df["Date received"].values),
     )
-    print(len(df_topic_sents_keywords))
-    print(df_topic_sents_keywords.head())
+    print("len(df_topic_sents_keywords)", len(df_topic_sents_keywords))
+    print("df_topic_sents_keywords.head()", df_topic_sents_keywords.head())
     df_dominant_topic = df_topic_sents_keywords.reset_index()
     df_dominant_topic.columns = [
         "Document_No",
@@ -102,7 +111,11 @@ def tsne_analysis(ldamodel, corpus):
     topic_nums = np.argmax(df_topics, axis=1)
 
     # tSNE Dimension Reduction
-    tsne_model = TSNE(n_components=2, verbose=1, random_state=0, angle=0.99, init="pca")
-    tsne_lda = tsne_model.fit_transform(df_topics)
+    try:
+        tsne_model = TSNE(n_components=2, verbose=1, random_state=0, angle=0.99, init="pca")
+        tsne_lda = tsne_model.fit_transform(df_topics)
+    except:
+        print("TSNE_ANALYSIS WENT HORRIBLY WRONG")
+        return(topic_nums, None)
 
     return (topic_nums, tsne_lda)
