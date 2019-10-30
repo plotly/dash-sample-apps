aee5fa83c8cc273c38dd43b23b68e30e7519ba2f
Johan Lundström <johan@vild.ly>
2019-10-30 12:23:02 +0100

    progress on LDA precomputing

--- apps/dash-nlp/precomputing.py
+++ apps/dash-nlp/precomputing.py
@@ -2,7 +2,9 @@ import pathlib
 import pandas as pd
 from ldacomplaints import lda_analysis
 from wordcloud import STOPWORDS
+import multiprocessing
 import re
+import time
 
 DATA_PATH = pathlib.Path(__file__).parent.resolve()
 EXTERNAL_STYLESHEETS = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
@@ -27,69 +29,94 @@ def add_stopwords(selected_bank):
     return STOPWORDS
 
 
+def precomputer_one_lda(bank):
+    print("crunching LDA for: ", bank, "...")
+    add_stopwords(bank)
+    bank_df = GLOBAL_DF[GLOBAL_DF["Company"] == bank]
+    #try:
+    tsne_lda, lda_model, topic_num, df_dominant_topic = lda_analysis(
+        bank_df, list(STOPWORDS)
+    )
+    #except:
+    #    print("SOMETHING WENT HORRIBLY WRONG WITH", bank)
+    #    print(bank_df)
+    #    #res = input("what happened?")
+    #    return {}
+    if tsne_lda is None:
+        print("crunching LDA for: ", bank, ". DONE (SAD)")
+        return {bank:"NOT ENOUGH DOCS/DICTS TO RUN LDA"}
+    topic_top3words = [
+        (i, topic)
+        for i, topics in lda_model.show_topics(formatted=False)
+        for j, (topic, wt) in enumerate(topics)
+        if j < 3
+    ]
+
+    df_top3words_stacked = pd.DataFrame(topic_top3words, columns=["topic_id", "words"])
+    df_top3words = df_top3words_stacked.groupby("topic_id").agg(", \n".join)
+    df_top3words.reset_index(level=0, inplace=True)
+
+    print(len(tsne_lda))
+    print(len(df_dominant_topic))
+    tsne_df = pd.DataFrame(
+        {
+            "tsne_x": tsne_lda[:, 0],
+            "tsne_y": tsne_lda[:, 1],
+            "topic_num": topic_num,
+            "doc_num": df_dominant_topic["Document_No"],
+        }
+    )
+
+    results_bank = {
+        str(bank): {
+            "tsne_df": tsne_df.to_json(),
+            "df_dominant_topic": df_dominant_topic.to_json(),
+        }
+    }
+    print("crunching LDA for: ", bank, ". DONE")
+    return results_bank
+
+
 def precompute_all_lda():
-    """ QD function for precomputing all necessary LDA results
-     to allow much faster load times when the app runs. """
+    print("precomupting LDA for all banks...")
+    start_time = time.time()
     file = open("precomupted", "w")
     file.close()
     failed_banks = []
     counter = 0
     bank_names = GLOBAL_DF["Company"].value_counts().keys().tolist()
+
     results = {}
+    nbr_workers = 4
+    print("Making a worker pool with: %d workers" % nbr_workers)
+    print("Starting the run at:", start_time)
+    pool = multiprocessing.Pool(processes=nbr_workers)
+    results = pool.map(precomputer_one_lda, bank_names)
+    #actually_do_stuff = True
+    #for bank in bank_names:
+    #    if bank == "Elderlife Financial Services, LLC":
+    #        actually_do_stuff = True
+    #    if actually_do_stuff:
+    #    results[bank] = precomputer_one_lda(bank)
+    end_time = time.time()
 
-    for bank in bank_names:
-        # try:
-        file = open("precomupted", "a")
-        print("crunching LDA for: ", bank)
-        add_stopwords(bank)
-        bank_df = GLOBAL_DF[GLOBAL_DF["Company"] == bank]
-        tsne_lda, lda_model, topic_num, df_dominant_topic = lda_analysis(
-            bank_df, list(STOPWORDS)
-        )
+    print("It took: %s" % end_time - start_time)
 
-        topic_top3words = [
-            (i, topic)
-            for i, topics in lda_model.show_topics(formatted=False)
-            for j, (topic, wt) in enumerate(topics)
-            if j < 3
-        ]
+    print("Saving data to file...")
+    file = open("precomupted", "a")
+    for result in results:
+        file.write(str(result))
+    file.close()
+    print("Saving data to file. DONE")
+    save_time = time.time()
+    print("It took: %s" % save_time - end_time)
 
-        df_top3words_stacked = pd.DataFrame(
-            topic_top3words, columns=["topic_id", "words"]
-        )
-        df_top3words = df_top3words_stacked.groupby("topic_id").agg(", \n".join)
-        df_top3words.reset_index(level=0, inplace=True)
-
-        print(len(tsne_lda))
-        print(len(df_dominant_topic))
-        tsne_df = pd.DataFrame(
-            {
-                "tsne_x": tsne_lda[:, 0],
-                "tsne_y": tsne_lda[:, 1],
-                "topic_num": topic_num,
-                "doc_num": df_dominant_topic["Document_No"],
-            }
-        )
-
-        results_bank = {
-            str(bank): {
-                "tsne_df": tsne_df.to_json(),
-                "df_dominant_topic": df_dominant_topic.to_json(),
-            }
-        }
-        # results[bank] = {"tsne_df":tsne_df.to_json()}
-        file.write(str(results_bank))
-        file.close()
-        counter += 1
-        # except:
-        #    print("SOMETHING WENT HORRIBLY WRONG WITH BANK: ", bank)
-        #    failed_banks.append(bank)
     print("DONE")
     print("did %d banks" % counter)
     print("failed %d:" % len(failed_banks))
     for fail in failed_banks:
         print(fail)
 
-
 if __name__ == "__main__":
+    #precomputer_one_lda("The CMI Group, Inc.")
     precompute_all_lda()
