9473b953e36e2fba3e58bec1b686b576c1fd4c7b
sstripps1 <55420381+sstripps1@users.noreply.github.com>
2020-01-15 15:08:20 -0500

    Update app.py
    Former-commit-id: be4fab423b584421face248b62eb1b3c792e0743

--- apps/dash-lunar-lander/app.py
+++ apps/dash-lunar-lander/app.py
@@ -11,6 +11,7 @@ import plotly.graph_objects as go
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.layout = html.Div(
     [
