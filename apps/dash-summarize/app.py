8e03162124a7097060554c66ca4a6bb4b90e4204
xhlulu <xinghan@plot.ly>
2020-06-26 17:57:58 -0400

    Black app

--- apps/dash-summarize/app.py
+++ apps/dash-summarize/app.py
@@ -8,7 +8,7 @@ from dash.dependencies import Input, Output, State
 from transformers import BartTokenizer, BartForConditionalGeneration
 import torch
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = "cuda" if torch.cuda.is_available() else "cpu"
 # device = "cpu"
 print(f"Device: {device}")
 
@@ -27,73 +27,109 @@ server = app.server
 
 controls = dbc.Card(
     [
-        dbc.FormGroup([
-            dbc.Label("Output Length (# Tokens)"),
-            dcc.Slider(
-                id="max-length",
-                min=10, max=50, value=30,
-                marks={i: str(i) for i in range(10, 51, 10)},
-            ),
-        ]),
-        dbc.FormGroup([
-            dbc.Label("Beam Size"),
-            dcc.Slider(
-                id="num-beams",
-                min=2, max=6, value=4,
-                marks={i: str(i) for i in [2, 4, 6]},
-            ),
-        ]),
-        dbc.FormGroup([
-            dbc.Spinner([
-                dbc.Button("Summarize", id="button-run"),
-                html.Div(id="time-taken")
-            ])
-        ]),
+        dbc.FormGroup(
+            [
+                dbc.Label("Output Length (# Tokens)"),
+                dcc.Slider(
+                    id="max-length",
+                    min=10,
+                    max=50,
+                    value=30,
+                    marks={i: str(i) for i in range(10, 51, 10)},
+                ),
+            ]
+        ),
+        dbc.FormGroup(
+            [
+                dbc.Label("Beam Size"),
+                dcc.Slider(
+                    id="num-beams",
+                    min=2,
+                    max=6,
+                    value=4,
+                    marks={i: str(i) for i in [2, 4, 6]},
+                ),
+            ]
+        ),
+        dbc.FormGroup(
+            [
+                dbc.Spinner(
+                    [
+                        dbc.Button("Summarize", id="button-run"),
+                        html.Div(id="time-taken"),
+                    ]
+                )
+            ]
+        ),
     ],
-    body=True, 
-    style={'height': '275px'}
+    body=True,
+    style={"height": "275px"},
 )
 
 
 # Define Layout
-app.layout = dbc.Container(fluid=True, children=[
-    html.H1("Dash Automatic Summarization (with DistilBART)"),
-    html.Hr(),
-    dbc.Row([
-        dbc.Col(width=5, children=[
-            controls,
-            dbc.Card(body=True, children=[
-                dbc.FormGroup([
-                    dbc.Label('Summarized Content'),
-                    dcc.Textarea(
-                        id="summarized-content",
-                        style={"width": "100%", 'height': 'calc(75vh - 275px)'},
-                    )
-                ]),
-            ])
-        ]),
-
-        dbc.Col(width=7, children=[
-            dbc.Card(body=True, children=[
-                dbc.FormGroup([
-                    dbc.Label('Original Text (Paste here)'),
-                    dcc.Textarea(
-                        id="original-text", 
-                        style={"width": "100%", 'height': '75vh'}
-                    ),
-                ])
-            ])
-        ]),
-    ])
-])
+app.layout = dbc.Container(
+    fluid=True,
+    children=[
+        html.H1("Dash Automatic Summarization (with DistilBART)"),
+        html.Hr(),
+        dbc.Row(
+            [
+                dbc.Col(
+                    width=5,
+                    children=[
+                        controls,
+                        dbc.Card(
+                            body=True,
+                            children=[
+                                dbc.FormGroup(
+                                    [
+                                        dbc.Label("Summarized Content"),
+                                        dcc.Textarea(
+                                            id="summarized-content",
+                                            style={
+                                                "width": "100%",
+                                                "height": "calc(75vh - 275px)",
+                                            },
+                                        ),
+                                    ]
+                                )
+                            ],
+                        ),
+                    ],
+                ),
+                dbc.Col(
+                    width=7,
+                    children=[
+                        dbc.Card(
+                            body=True,
+                            children=[
+                                dbc.FormGroup(
+                                    [
+                                        dbc.Label("Original Text (Paste here)"),
+                                        dcc.Textarea(
+                                            id="original-text",
+                                            style={"width": "100%", "height": "75vh"},
+                                        ),
+                                    ]
+                                )
+                            ],
+                        )
+                    ],
+                ),
+            ]
+        ),
+    ],
+)
 
 
 @app.callback(
-    [Output("summarized-content", "value"), 
-     Output("time-taken", "children")],
-    [Input("button-run", "n_clicks"), 
-     Input("max-length", "value"),
-     Input("num-beams", "value")],
+    [Output("summarized-content", "value"), Output("time-taken", "children")],
+    [
+        Input("button-run", "n_clicks"),
+        Input("max-length", "value"),
+        Input("num-beams", "value"),
+    ],
     [State("original-text", "value")],
 )
 def summarize(n_clicks, max_len, num_beams, original_text):
@@ -102,18 +138,22 @@ def summarize(n_clicks, max_len, num_beams, original_text):
 
     t0 = time.time()
 
-    inputs = tokenizer.batch_encode_plus([original_text], max_length=1024, return_tensors="pt")
+    inputs = tokenizer.batch_encode_plus(
+        [original_text], max_length=1024, return_tensors="pt"
+    )
     inputs = inputs.to(device)
 
     # Generate Summary
     summary_ids = model.generate(
-        inputs["input_ids"], 
-        num_beams=num_beams, 
-        max_length=max_len, 
-        early_stopping=True
+        inputs["input_ids"],
+        num_beams=num_beams,
+        max_length=max_len,
+        early_stopping=True,
     )
     out = [
-        tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+        tokenizer.decode(
+            g, skip_special_tokens=True, clean_up_tokenization_spaces=False
+        )
         for g in summary_ids
     ]
 
