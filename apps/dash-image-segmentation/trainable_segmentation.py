82b3b7bdb6cab668fce64f77e56a5385f31925e7
Emmanuelle Gouillart <emma@plot.ly>
2020-07-28 11:56:55 +0200

    remove unused code

--- apps/dash-image-segmentation/trainable_segmentation.py
+++ apps/dash-image-segmentation/trainable_segmentation.py
@@ -127,89 +127,6 @@ def multiscale_basic_features(
     return np.array(features, dtype=np.float32)
 
 
-class TrainableSegmenter(object):
-    """
-    Estimator for classifying pixels.
-
-    Parameters
-    ----------
-    clf : classifier object, optional
-        classifier object, exposing a ``fit`` and a ``predict`` method as in
-        scikit-learn's API, for example an instance of
-        ``RandomForestClassifier`` or ``LogisticRegression`` classifier.
-    features_func : function, optional
-        function computing features on all pixels of the image, to be passed
-        to the classifier. The output should be of shape
-        ``(m_features, *labels.shape)``. If None,
-        :func:`skimage.segmentation.multiscale_basic_features` is used.
-
-    Methods
-    -------
-    compute_features
-    fit
-    predict
-    """
-
-    def __init__(self, clf=None, features_func=None):
-        if clf is None:
-            try:
-                from sklearn.ensemble import RandomForestClassifier
-
-                self.clf = RandomForestClassifier(n_estimators=100, n_jobs=-1)
-            except ImportError:
-                raise ImportError(
-                    "Please install scikit-learn or pass a classifier instance"
-                    "to TrainableSegmenter."
-                )
-        else:
-            self.clf = clf
-        self.features_func = features_func
-        self.features = None
-
-    def compute_features(self, image):
-        if self.features_func is None:
-            self.features_func = multiscale_basic_features
-        self.features = self.features_func(image)
-
-    def fit(self, labels, image=None):
-        """
-        Train classifier using partially labeled (annotated) image.
-
-        Parameters
-        ----------
-        labels : ndarray of ints
-            Labeled array of shape compatible with ``image`` (same shape for a
-            single-channel image). Labels >= 1 correspond to the training set and
-            label 0 to unlabeled pixels to be segmented.
-        image : ndarray
-            Input image, which can be grayscale or multichannel, and must have a
-            number of dimensions compatible with ``self.features_func``.
-        """
-        if self.features is None:
-            self.compute_features(image)
-        output, clf = fit_segmenter(labels, self.features, self.clf)
-        self.segmented_image = output
-
-    def predict(self, image):
-        """
-        Segment new image using trained internal classifier.
-
-        Parameters
-        ----------
-        image : ndarray
-            Input image, which can be grayscale or multichannel, and must have a
-            number of dimensions compatible with ``self.features_func``.
-
-        Raises
-        ------
-        NotFittedError if ``self.clf`` has not been fitted yet (use ``self.fit``).
-        """
-        if self.features_func is None:
-            self.features_func = multiscale_basic_features
-        features = self.features_func(image)
-        return predict_segmenter(features, self.clf)
-
-
 def fit_segmenter(labels, features, clf):
     """
     Segmentation using labeled parts of the image and a classifier.
@@ -264,12 +181,12 @@ def predict_segmenter(features, clf):
         scikit-learn's API, for example an instance of
         ``RandomForestClassifier`` or ``LogisticRegression`` classifier. The
         classifier must be already trained, for example with
-        :func:`skimage.segmentation.fit_segmenter`.
+        :func:`fit_segmenter`.
     features_func : function, optional
         function computing features on all pixels of the image, to be passed
         to the classifier. The output should be of shape
         ``(m_features, *labels.shape)``. If None,
-        :func:`skimage.segmentation.multiscale_basic_features` is used.
+        :func:`multiscale_basic_features` is used.
 
     Returns
     -------
