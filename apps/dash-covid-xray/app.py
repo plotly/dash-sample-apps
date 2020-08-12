a6581f7ae2f996747cbbae07110d15230d1a3ebc
Emmanuelle Gouillart <emma@plot.ly>
2020-08-12 23:30:05 +0200

    blacken

--- apps/dash-covid-xray/app.py
+++ apps/dash-covid-xray/app.py
@@ -7,11 +7,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 from dash_canvas.utils import array_to_data_url
 from nilearn import image
-from skimage import draw, segmentation, filters, exposure, color, measure
-from skimage import img_as_ubyte
-import datashader.transfer_functions as tf
-import datashader as ds
-import pandas as pd
+from skimage import draw, filters, exposure, measure
 from scipy import ndimage
 from time import time
 
@@ -23,12 +19,8 @@ img_2 = np.copy(np.moveaxis(img, -1, 1))
 
 l_h = img.shape[-1]
 l_lat = img.shape[0]
-print("L_h", l_h)
-print("L_lat", l_lat)
-print(img_1.shape)
 
 size_factor = abs(mat[2, 2] / mat[0, 0])
-print(size_factor)
 
 slices_1 = [array_to_data_url(img_1[i]) for i in range(img_1.shape[0])]
 slices_2 = [
@@ -43,9 +35,7 @@ verts, faces, _, _ = measure.marching_cubes(very_filtered_img, 200, step_size=3)
 x, y, z = verts.T
 i, j, k = faces.T
 fig_mesh = go.Figure()
-fig_mesh.add_trace(
-    go.Mesh3d(x=z, y=y, z=x, opacity=0.2, i=k, j=j, k=i)
-)
+fig_mesh.add_trace(go.Mesh3d(x=z, y=y, z=x, opacity=0.2, i=k, j=j, k=i))
 
 hi = exposure.histogram(med_img)
 
@@ -59,8 +49,15 @@ def path_to_indices(path):
     return np.array(indices_str, dtype=float)
 
 
-def make_figure(filename_uri, width, height, row=None, col=None, size_factor=1,
-                dragmode="drawclosedpath"):
+def make_figure(
+    filename_uri,
+    width,
+    height,
+    row=None,
+    col=None,
+    size_factor=1,
+    dragmode="drawclosedpath",
+):
     fig = go.Figure()
     # Add trace
     fig.add_trace(go.Scatter(x=[], y=[]))
@@ -99,7 +96,7 @@ def make_figure(filename_uri, width, height, row=None, col=None, size_factor=1,
     fig.add_shape(
         type="line",
         x0=0,
-        x1=width // 20 ,
+        x1=width // 20,
         y0=row,
         y1=row,
         line=dict(width=3, color="red"),
@@ -108,7 +105,7 @@ def make_figure(filename_uri, width, height, row=None, col=None, size_factor=1,
     fig.add_shape(
         type="line",
         x0=width - width // 20,
-        x1=width ,
+        x1=width,
         y0=row,
         y1=row,
         line=dict(width=3, color="red"),
@@ -167,7 +164,9 @@ app.layout = html.Div(
                     value=len(img_1) // 2,
                     updatemode="drag",
                 ),
-                html.H6('Slide to find the occlusion and draw a path around its contour'),
+                html.H6(
+                    "Slide to find the occlusion and draw a path around its contour"
+                ),
             ],
             className="app-background",
         ),
@@ -176,8 +175,10 @@ app.layout = html.Div(
                 dcc.Graph(
                     id="graph-2",
                     figure=make_figure(
-                        slices_2[len(img_2) // 2], width=630, height=45 * size_factor,
-                        dragmode='drawrect'
+                        slices_2[len(img_2) // 2],
+                        width=630,
+                        height=45 * size_factor,
+                        dragmode="drawrect",
                     ),
                 ),
                 dcc.Slider(
@@ -188,20 +189,22 @@ app.layout = html.Div(
                     value=len(img_2) // 2,
                     updatemode="drag",
                 ),
-                html.H6('Draw a rectangle to determine the min and max height of the occlusion'),
+                html.H6(
+                    "Draw a rectangle to determine the min and max height of the occlusion"
+                ),
             ],
             className="app-background",
         ),
-         html.Div(
+        html.Div(
             [
                 dcc.Graph(
                     id="graph-histogram",
                     figure=px.bar(
                         x=hi[1],
                         y=hi[0],
-                        title='Histogram of intensity values - please select first a volume of interest by annotating slices',
-                        labels={'x':'intensity', 'y':'count'},
-                        template='plotly_white'
+                        title="Histogram of intensity values - please select first a volume of interest by annotating slices",
+                        labels={"x": "intensity", "y": "count"},
+                        template="plotly_white",
                     ),
                     config={
                         "modeBarButtonsToAdd": [
@@ -215,13 +218,8 @@ app.layout = html.Div(
             ],
             className="app-background",
         ),
-         html.Div(
-            [
-                dcc.Graph(
-                    id="graph-helper",
-                    figure=fig_mesh
-                ),
-            ],
+        html.Div(
+            [dcc.Graph(id="graph-helper", figure=fig_mesh),],
             className="app-background",
         ),
         dcc.Store(id="small-slices", data=slices_1),
@@ -236,60 +234,62 @@ app.layout = html.Div(
 
 
 @app.callback(
-    Output("graph-histogram", "figure"),
-    [Input("annotations", "data")],
+    Output("graph-histogram", "figure"), [Input("annotations", "data")],
 )
 def update_histo(annotations):
-    if annotations is None or annotations.get('x') is None or annotations.get('z') is None:
+    if (
+        annotations is None
+        or annotations.get("x") is None
+        or annotations.get("z") is None
+    ):
         return dash.no_update
     # Horizontal mask
-    path = path_to_indices(annotations['z']["path"])
+    path = path_to_indices(annotations["z"]["path"])
     rr, cc = draw.polygon(path[:, 1], path[:, 0])
     mask = np.zeros((l_lat, l_lat))
     mask[rr, cc] = 1
     mask = ndimage.binary_fill_holes(mask)
     # top and bottom
-    top = int(annotations['x']['y0']/size_factor)
-    bottom = int(annotations['x']['y1']/size_factor)
+    top = int(annotations["x"]["y0"] / size_factor)
+    bottom = int(annotations["x"]["y1"] / size_factor)
     print(top, bottom)
     intensities = med_img[top:bottom, mask].ravel()
     hi = exposure.histogram(intensities)
     fig = px.bar(
-        x=hi[1], 
+        x=hi[1],
         y=hi[0],
-        title='Histogram of intensity values - select a range of values to segment the occlusion',
-        labels={'x':'intensity', 'y':'count'}
-        )
-    fig.update_layout(dragmode='select')
+        title="Histogram of intensity values - select a range of values to segment the occlusion",
+        labels={"x": "intensity", "y": "count"},
+    )
+    fig.update_layout(dragmode="select")
     return fig
 
 
 @app.callback(
-    [Output("segmentation-slices", "data"),
-     Output("graph-helper", "figure")],
+    [Output("segmentation-slices", "data"), Output("graph-helper", "figure")],
     [Input("graph-histogram", "selectedData")],
-    [State("annotations", "data"),
-     State("graph-helper", "figure")]
+    [State("annotations", "data"), State("graph-helper", "figure")],
 )
 def update_segmentation_slices(selected, annotations, fig_mesh3d):
-    if annotations is None or annotations.get('x') is None or annotations.get('z') is None:
-        return dash.no_update, dash.no_update
     if (
-        selected is not None
-        and "range" in selected
+        annotations is None
+        or annotations.get("x") is None
+        or annotations.get("z") is None
     ):
-        v_min, v_max = selected['range']['x']
+        return dash.no_update, dash.no_update
+    if selected is not None and "range" in selected:
+        v_min, v_max = selected["range"]["x"]
         t_start = time()
         img_mask = np.logical_and(med_img > v_min, med_img <= v_max)
         # Horizontal mask
-        path = path_to_indices(annotations['z']["path"])
+        path = path_to_indices(annotations["z"]["path"])
         rr, cc = draw.polygon(path[:, 1], path[:, 0])
         mask = np.zeros((l_lat, l_lat))
         mask[rr, cc] = 1
         mask = ndimage.binary_fill_holes(mask)
         # top and bottom
-        top = int(annotations['x']['y0']/size_factor)
-        bottom = int(annotations['x']['y1']/size_factor)
+        top = int(annotations["x"]["y0"] / size_factor)
+        bottom = int(annotations["x"]["y1"] / size_factor)
         img_mask = np.logical_and(med_img > v_min, med_img <= v_max)
         img_mask[:top] = False
         img_mask[bottom:] = False
@@ -305,19 +305,21 @@ def update_segmentation_slices(selected, annotations, fig_mesh3d):
         i, j, k = faces.T
         fig = go.Figure(fig_mesh3d)
         print(len(x))
-        trace=go.Mesh3d(x=z, y=y, z=x, color='red', opacity=0.8, i=k, j=j, k=i)
+        trace = go.Mesh3d(x=z, y=y, z=x, color="red", opacity=0.8, i=k, j=j, k=i)
         if len(fig.data) > 1:
             print(fig.data[1])
-            fig.data[1]['x'] = z
-            fig.data[1]['y'] = y
-            fig.data[1]['z'] = x
-            fig.data[1]['i'] = k
-            fig.data[1]['j'] = j
-            fig.data[1]['k'] = i
+            fig.data[1]["x"] = z
+            fig.data[1]["y"] = y
+            fig.data[1]["z"] = x
+            fig.data[1]["i"] = k
+            fig.data[1]["j"] = j
+            fig.data[1]["k"] = i
         else:
             fig.add_trace(trace)
         t_start = time()
-        slices_dict = {str(i): array_to_data_url(img_mask_color[i]) for i in range(top, bottom)}
+        slices_dict = {
+            str(i): array_to_data_url(img_mask_color[i]) for i in range(top, bottom)
+        }
         t_end = time()
         print("binary string", t_end - t_start)
         return (slices_dict, fig)
@@ -327,27 +329,18 @@ def update_segmentation_slices(selected, annotations, fig_mesh3d):
 
 @app.callback(
     Output("annotations", "data"),
-    [Input("graph", "relayoutData"),
-     Input("graph-2", "relayoutData"),
-    ],
-    [State('annotations', 'data')])
+    [Input("graph", "relayoutData"), Input("graph-2", "relayoutData"),],
+    [State("annotations", "data")],
+)
 def update_store(relayout, relayout2, annotations):
     if relayout is None:
         return dash.no_update
-    if (
-        relayout is not None
-        and "shapes" in relayout
-        and len(relayout["shapes"]) > 1
-    ):
+    if relayout is not None and "shapes" in relayout and len(relayout["shapes"]) > 1:
         shape = relayout["shapes"][-1]
-        annotations['z'] = shape
-    if (
-        relayout2 is not None
-        and "shapes" in relayout2
-        and len(relayout2["shapes"]) > 1
-    ):
+        annotations["z"] = shape
+    if relayout2 is not None and "shapes" in relayout2 and len(relayout2["shapes"]) > 1:
         shape = relayout2["shapes"][-1]
-        annotations['x'] = shape
+        annotations["x"] = shape
     return annotations
 
 
@@ -379,12 +372,12 @@ function(n_slider, n_slider_2, seg_slices, slices, fig, annotations){
 """,
     output=Output("graph", "figure"),
     inputs=[
-        Input("slider", "value"), 
+        Input("slider", "value"),
         Input("slider-2", "value"),
-        Input("segmentation-slices", "data"),],
+        Input("segmentation-slices", "data"),
+    ],
     state=[
         State("small-slices", "data"),
-        
         State("graph", "figure"),
         State("annotations", "data"),
     ],
