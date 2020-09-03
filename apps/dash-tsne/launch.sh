4d0e485398615663fdd6b245ca42dba7b6f0f6dc
xhlulu <xinghan@plot.ly>
2020-09-03 16:39:48 -0400

    Fix downloading for t-sne
    Former-commit-id: 24b147a40399b1a0a2262435cdc5036542125a0c

--- apps/dash-tsne/launch.sh
+++ apps/dash-tsne/launch.sh
@@ -1,3 +1,5 @@
-wget https://github.com/plotly/datasets/releases/download/dash-sample-apps/dash-tsne.zip
-unzip dash-tsne.zip
+wget -N https://github.com/plotly/datasets/raw/master/dash-sample-apps/dash-tsne/data.zip
+wget -N https://github.com/plotly/datasets/raw/master/dash-sample-apps/dash-tsne/demo_embeddings.zip
+unzip -nq ./data.zip
+unzip -nq ./demo_embeddings.zip
 gunicorn app:server --workers 4
