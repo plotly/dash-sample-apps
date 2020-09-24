f7825920481392f606cab2525c9d8337906b4a49
Emmanuelle Gouillart <emma@plot.ly>
2020-09-24 11:48:11 +0200

    improved app

--- apps/dash-covid-xray/app.py
+++ apps/dash-covid-xray/app.py
@@ -11,6 +11,8 @@ from skimage import draw, filters, exposure, measure
 from scipy import ndimage
 from time import time
 
+# ------------- I/O and data massaging ---------------------------------------------------
+
 img = image.load_img("assets/radiopaedia_org_covid-19-pneumonia-7_85703_0-dcm.nii")
 mat = img.affine
 img = img.get_data()
@@ -58,25 +60,18 @@ def make_figure(
     size_factor=1,
     dragmode="drawclosedpath",
 ):
-    fig = go.Figure()
-    # Add trace
-    fig.add_trace(go.Scatter(x=[], y=[]))
-    # Add images
-    fig.add_layout_image(
-        dict(
-            source=filename_uri,
-            xref="x",
-            yref="y",
-            x=0,
-            y=0,
-            sizex=width,
-            sizey=height * size_factor,
-            sizing="stretch",
-            layer="below",
-            opacity=1,
-        )
+    """
+    Build plotly figure with image slice and visual cues at specific positions.
+    """
+    fig = go.Figure(go.Image(source=filename_uri))
+    fig.update_traces(dy=size_factor)
+    fig.update_layout(
+        template=None,
+        margin=dict(t=10, b=0, l=0, r=0),
+        dragmode=dragmode,
+        newshape_line_color="cyan",
+        height=400,
     )
-    fig.update_layout(template=None, margin=dict(t=10, b=0, l=0, r=0))
     fig.update_xaxes(
         showgrid=False, range=(0, width), showticklabels=False, zeroline=False
     )
@@ -87,8 +82,7 @@ def make_figure(
         showticklabels=False,
         zeroline=False,
     )
-    fig.update_xaxes(showgrid=False)
-    fig.update_yaxes(showgrid=False)
+    # Visual cues of position of other slice
     if row is None:
         row = height // 2
     if col is None:
@@ -111,14 +105,18 @@ def make_figure(
         line=dict(width=3, color="red"),
         fillcolor="pink",
     )
-
-    fig.update_layout(dragmode=dragmode, newshape_line_color="cyan", height=400)
     return fig
+
+
+def largest_connected_component(mask):
+    labels, _ = ndimage.label(mask)
+    sizes = np.bincount(labels.ravel())[1:]
+    return labels == (np.argmax(sizes) + 1)
 
 
 def mask_to_color(img):
     mask = np.zeros(img.shape + (4,), dtype=np.uint8)
-    mask[img] = [255, 0, 0, 128]
+    mask[img] = [255, 0, 0, 100]
     return mask
 
 
@@ -153,7 +151,8 @@ app.layout = html.Div(
                             "drawclosedpath",
                             "drawrect",
                             "eraseshape",
-                        ]
+                        ],
+                        "scrollZoom": True,
                     },
                 ),
                 dcc.Slider(
@@ -165,7 +164,7 @@ app.layout = html.Div(
                     updatemode="drag",
                 ),
                 html.H6(
-                    "Slide to find the occlusion and draw a path around its contour"
+                    "1 - Slide to find the occlusion and draw a path around its contour"
                 ),
             ],
             className="app-background",
@@ -179,7 +178,9 @@ app.layout = html.Div(
                         width=630,
                         height=45 * size_factor,
                         dragmode="drawrect",
+                        size_factor=size_factor,
                     ),
+                    config={"scrollZoom": True},
                 ),
                 dcc.Slider(
                     id="slider-2",
@@ -190,7 +191,7 @@ app.layout = html.Div(
                     updatemode="drag",
                 ),
                 html.H6(
-                    "Draw a rectangle to determine the min and max height of the occlusion"
+                    "2 - Draw a rectangle to determine the min and max height of the occlusion"
                 ),
             ],
             className="app-background",
@@ -228,6 +229,7 @@ app.layout = html.Div(
         dcc.Store(id="x-pos", data=l_lat // 2),
         dcc.Store(id="annotations", data={}),
         dcc.Store(id="segmentation-slices", data={}),
+        dcc.Store(id="segmentation-slices-2", data={}),
     ],
     className="twelve columns",
 )
@@ -252,21 +254,25 @@ def update_histo(annotations):
     # top and bottom
     top = int(annotations["x"]["y0"] / size_factor)
     bottom = int(annotations["x"]["y1"] / size_factor)
-    print(top, bottom)
     intensities = med_img[top:bottom, mask].ravel()
     hi = exposure.histogram(intensities)
     fig = px.bar(
         x=hi[1],
         y=hi[0],
-        title="Histogram of intensity values - select a range of values to segment the occlusion",
+        title="3 - Histogram of intensity values - select a range of values to segment the occlusion",
         labels={"x": "intensity", "y": "count"},
     )
     fig.update_layout(dragmode="select")
+    fig.update_layout(images=[])
     return fig
 
 
 @app.callback(
-    [Output("segmentation-slices", "data"), Output("graph-helper", "figure")],
+    [
+        Output("segmentation-slices", "data"),
+        Output("segmentation-slices-2", "data"),
+        Output("graph-helper", "figure"),
+    ],
     [Input("graph-histogram", "selectedData")],
     [State("annotations", "data"), State("graph-helper", "figure")],
 )
@@ -276,7 +282,7 @@ def update_segmentation_slices(selected, annotations, fig_mesh3d):
         or annotations.get("x") is None
         or annotations.get("z") is None
     ):
-        return dash.no_update, dash.no_update
+        return (dash.no_update,) * 3
     if selected is not None and "range" in selected:
         v_min, v_max = selected["range"]["x"]
         t_start = time()
@@ -294,20 +300,20 @@ def update_segmentation_slices(selected, annotations, fig_mesh3d):
         img_mask[:top] = False
         img_mask[bottom:] = False
         img_mask[top:bottom, np.logical_not(mask)] = False
+        img_mask = largest_connected_component(img_mask)
         img_mask_color = mask_to_color(img_mask)
         t_end = time()
         print("build the mask", t_end - t_start)
         t_start = time()
+        # Update 3d viz
         verts, faces, _, _ = measure.marching_cubes(img_mask, 0.5, step_size=3)
         t_end = time()
         print("marching cubes", t_end - t_start)
         x, y, z = verts.T
         i, j, k = faces.T
         fig = go.Figure(fig_mesh3d)
-        print(len(x))
         trace = go.Mesh3d(x=z, y=y, z=x, color="red", opacity=0.8, i=k, j=j, k=i)
         if len(fig.data) > 1:
-            print(fig.data[1])
             fig.data[1]["x"] = z
             fig.data[1]["y"] = y
             fig.data[1]["z"] = x
@@ -317,14 +323,19 @@ def update_segmentation_slices(selected, annotations, fig_mesh3d):
         else:
             fig.add_trace(trace)
         t_start = time()
+        # Build lists of binary strings for segmented slices
         slices_dict = {
             str(i): array_to_data_url(img_mask_color[i]) for i in range(top, bottom)
         }
+        slices_dict_2 = {
+            str(i): array_to_data_url(img_mask_color[:, i])
+            for i in range(img_mask_color.shape[1])
+        }
         t_end = time()
         print("binary string", t_end - t_start)
-        return (slices_dict, fig)
+        return (slices_dict, slices_dict_2, fig)
     else:
-        return dash.no_update, dash.no_update
+        return (dash.no_update,) * 3
 
 
 @app.callback(
@@ -351,22 +362,21 @@ function(n_slider, n_slider_2, seg_slices, slices, fig, annotations){
         zpos = n_slider;
         xpos = n_slider_2;
         let fig_ = {...fig};
-        fig_.layout.images[0].source = slices[zpos];
-        if (fig_.layout.images.length  == 1 && zpos in seg_slices){
-            fig_.layout.images.push({...fig.layout.images[0]});
-            fig_.layout.images[1].source = seg_slices[zpos];
+        fig_.data[0].source = slices[zpos];
+        if (fig_.data.length  == 1 && zpos in seg_slices){
+            fig_.data.push({...fig.data[0]});
+            fig_.data[1].source = seg_slices[zpos];
         }
-        if (fig_.layout.images.length  > 1 && zpos in seg_slices){
-            fig_.layout.images[1].source = seg_slices[zpos];
+        if (fig_.data.length  > 1 && zpos in seg_slices){
+            fig_.data[1].source = seg_slices[zpos];
         }
-        //fig_.layout.shapes = [fig.layout.shapes[0], fig.layout.shapes[1]];
         fig_.layout.shapes[0].y0 = xpos;
         fig_.layout.shapes[0].y1 = xpos;
         fig_.layout.shapes[1].y0 = xpos;
         fig_.layout.shapes[1].y1 = xpos;
-        //if (n_slider.toString() in annotations){
-        //    fig_.layout.shapes.push(annotations[n_slider.toString()]);
-        //}
+        if ('z' in annotations){
+            fig_.layout.shapes.push(annotations['z']);
+            }
         return fig_;
     }
 """,
@@ -383,24 +393,43 @@ function(n_slider, n_slider_2, seg_slices, slices, fig, annotations){
     ],
 )
 
+
 app.clientside_callback(
     """
-function(n_slider, n_slider_2, slices_2, fig_2){
+function(n_slider, n_slider_2, seg_slices_2, slices_2, fig_2, annotations){
         var size_factor = 8.777;
         zpos = n_slider;
         xpos = n_slider_2;
         let fig_2_ = {...fig_2};
-        fig_2_.layout.images[0].source = slices_2[xpos];
+        fig_2_.data[0].source = slices_2[xpos];
+        if (fig_2_.data.length  == 1 && xpos in seg_slices_2){
+            fig_2_.data.push({...fig_2.data[0]});
+            fig_2_.data[1].source = seg_slices_2[xpos];
+        }
+        if (fig_2_.data.length  > 1 && zpos in seg_slices_2){
+            fig_2_.data[1].source = seg_slices_2[xpos];
+        }
         fig_2_.layout.shapes[0].y0 = zpos * size_factor;
         fig_2_.layout.shapes[0].y1 = zpos * size_factor;
         fig_2_.layout.shapes[1].y0 = zpos * size_factor;
         fig_2_.layout.shapes[1].y1 = zpos * size_factor;
+        if ('x' in annotations){
+            fig_2_.layout.shapes.push(annotations['x']);
+            }
         return fig_2_;
     }
 """,
     output=Output("graph-2", "figure"),
-    inputs=[Input("slider", "value"), Input("slider-2", "value"),],
-    state=[State("small-slices-2", "data"), State("graph-2", "figure"),],
+    inputs=[
+        Input("slider", "value"),
+        Input("slider-2", "value"),
+        Input("segmentation-slices-2", "data"),
+    ],
+    state=[
+        State("small-slices-2", "data"),
+        State("graph-2", "figure"),
+        State("annotations", "data"),
+    ],
 )
 
 
