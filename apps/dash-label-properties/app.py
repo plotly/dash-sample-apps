479d627a5263ef30642d8d038128ca5ca3454b44
surchs <sebastian.urchs@gmail.com>
2020-10-28 19:16:22 -0400

    dash-bootstrap-components for layout - added dbc to requirements.txt - added dbc stylesheet to app - remove old stylesheets that are no longer needed - moved app and content definition to the top of the file - refactored `data` to `table` to avoid shadowed variable name from `highlight_filter` scope and for clarity - broke layout into components and fit in dbc structure with annotations - added plotly logo - added a README.md file

--- apps/dash-label-properties/app.py
+++ apps/dash-label-properties/app.py
@@ -2,6 +2,7 @@ import dash
 from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 from dash.exceptions import PreventUpdate
 import dash_table
 from dash_table.Format import Format
@@ -16,6 +17,45 @@ import PIL
 from skimage import color, img_as_ubyte
 from plotly import colors
 from textwrap import dedent
+
+
+external_stylesheets = [dbc.themes.BOOTSTRAP]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+# Read the image that will be segmented
+filename = (
+    "https://upload.wikimedia.org/wikipedia/commons/a/ac/Monocyte_no_vacuoles.JPG"
+)
+img = io.imread(filename, as_gray=True)[:660:2, :800:2]
+labels = measure.label(img < filters.threshold_otsu(img))
+props = measure.regionprops(labels, img)
+
+# Define table columns
+list_columns = [
+    "label",
+    "area",
+    "perimeter",
+    "eccentricity",
+    "euler_number",
+    "mean_intensity",
+]
+columns = [
+    {"name": label_name, "id": label_name}
+    if precision is None
+    else {
+        "name": label_name,
+        "id": label_name,
+        "type": "numeric",
+        "format": Format(precision=precision),
+    }
+    for label_name, precision in zip(list_columns, (None, None, 4, 4, None, 3))
+]
+table = pd.DataFrame(
+    [[getattr(prop, col) for col in list_columns] for prop in props],
+    columns=list_columns,
+)
 
 
 def image_with_contour(img, labels, mode="lines", shape=None):
@@ -51,7 +91,7 @@ def image_with_contour(img, labels, mode="lines", shape=None):
     print("mode is", mode)
     opacity = 0.4 if mode is None else 1
 
-    fig = px.imshow(img, binary_backend="jpg",)
+    fig = px.imshow(img, binary_backend="jpg")
     fig.add_contour(
         z=labels,
         contours=dict(start=0, end=labels.max() + 1, size=1, coloring=mode),
@@ -61,97 +101,140 @@ def image_with_contour(img, labels, mode="lines", shape=None):
         opacity=opacity,
     )
     # Remove axis ticks and labels
-    fig.update_layout(
-        xaxis=dict(
-            showgrid=False,
-            zeroline=False,
-            showline=False,
-            ticks="",
-            showticklabels=False,
-        ),
-        yaxis=dict(
-            showgrid=False,
-            zeroline=False,
-            showline=False,
-            ticks="",
-            showticklabels=False,
-        ),
-    )
+    fig.update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
 
     return fig
 
 
-# Image to segment
-filename = (
-    "https://upload.wikimedia.org/wikipedia/commons/a/ac/Monocyte_no_vacuoles.JPG"
-)
-img = io.imread(filename, as_gray=True)[:660:2, :800:2]
-labels = measure.label(img < filters.threshold_otsu(img))
+# Define Modal
+howto = """
+    Hover over objects to highlight their properties in the table,
+    select cell in table to highlight object in image, or
+    filter objects in the table to display a subset of objects.
 
-height, width = img.shape
-canvas_width = 600
-props = measure.regionprops(labels, img)
-
-# Define table columns
-list_columns = [
-    "label",
-    "area",
-    "perimeter",
-    "eccentricity",
-    "euler_number",
-    "mean_intensity",
-]
-columns = [{"name": i, "id": i} for i in list_columns]
-columns[2]["format"] = Format(precision=4)
-columns[2]["type"] = "numeric"
-columns[3]["format"] = Format(precision=4)
-columns[3]["type"] = "numeric"
-columns[5]["format"] = Format(precision=3)
-columns[5]["type"] = "numeric"
-
-data = pd.DataFrame(
-    [[getattr(prop, col) for col in list_columns] for prop in props],
-    columns=list_columns,
-)
-
-app = dash.Dash(__name__)
-server = app.server
-
-app.layout = html.Div(
+    Learn more about [DataTable filtering syntax](https://dash.plot.ly/datatable/filtering)
+    for selecting ranges of properties.
+    """
+modal = dbc.Modal(
     [
-        html.Div(
-            [
-                html.Div(
+        dbc.ModalHeader("How to use this app"),
+        dbc.ModalBody(
+            dbc.Row(
+                dbc.Col(
                     [
-                        html.H4("Explore objects properties"),
-                        dcc.Graph(
-                            id="graph",
-                            figure=image_with_contour(img, labels, mode=None),
-                        ),
-                    ],
-                    className="six columns",
-                ),
-                html.Div(
-                    [
+                        dcc.Markdown(howto, id="howto-md"),
                         html.Img(
-                            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
-                            width="30px",
-                        ),
-                        html.A(
-                            id="gh-link",
-                            children=["View on GitHub"],
-                            href="http://github.com/plotly/canvas-portal/"
-                            "blob/master/apps/object-properties/app.py",
+                            id="help",
+                            src="assets/properties.gif",
+                            width="80%",
                             style={
-                                "color": "black",
-                                "border": "solid 1px black",
-                                "float": "left",
+                                "border": "2px solid black",
+                                "display": "block",
+                                "margin-left": "auto",
+                                "margin-right": "auto",
                             },
                         ),
+                    ]
+                )
+            )
+        ),
+        dbc.ModalFooter(dbc.Button("Close", id="howto-close", className="howto-bn",)),
+    ],
+    id="modal",
+    size="lg",
+    style={"font-size": "small"},
+)
+
+# Buttons
+button_gh = dbc.Button(
+    "Learn more",
+    id="howto-open",
+    outline=True,
+    color="secondary",
+    # Turn off lowercase transformation for class .button in stylesheet
+    style={"textTransform": "none"},
+)
+
+button_howto = dbc.Button(
+    "View Code on github",
+    outline=True,
+    color="primary",
+    href="https://github.com/plotly/dash-sample-apps/tree/master/apps/dash-image-annotation",
+    id="gh-link",
+    style={"text-transform": "none"},
+)
+
+# Define Header Layout
+header = dbc.Navbar(
+    dbc.Container(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(
+                        html.A(
+                            html.Img(
+                                src=app.get_asset_url("dash-logo-new.png"),
+                                height="30px",
+                            ),
+                            href="https://plotly.com/dash/",
+                        )
+                    ),
+                    dbc.Col(dbc.NavbarBrand("Object Properties App")),
+                    modal,
+                ],
+                align="center",
+            ),
+            dbc.Row(
+                dbc.Col(
+                    [
+                        dbc.NavbarToggler(id="navbar-toggler"),
+                        dbc.Collapse(
+                            dbc.Nav(
+                                [dbc.NavItem(button_howto), dbc.NavItem(button_gh)],
+                                className="ml-auto",
+                                navbar=True,
+                            ),
+                            id="navbar-collapse",
+                            navbar=True,
+                        ),
+                    ]
+                ),
+                align="center",
+            ),
+        ],
+        fluid=True,
+    ),
+    color="dark",
+    dark=True,
+)
+
+# Define Cards
+image_card = dbc.Card(
+    [
+        dbc.CardHeader(html.H2("Explore object properties")),
+        dbc.CardBody(
+            dbc.Row(
+                dbc.Col(
+                    dcc.Graph(
+                        id="graph", figure=image_with_contour(img, labels, mode=None),
+                    )
+                )
+            )
+        ),
+    ]
+)
+
+table_card = dbc.Card(
+    [
+        dbc.CardHeader(html.H2("Data Table")),
+        dbc.CardBody(
+            dbc.Row(
+                dbc.Col(
+                    [
                         dash_table.DataTable(
                             id="table-line",
                             columns=columns,
-                            data=data.to_dict("records"),
+                            data=table.to_dict("records"),
                             filter_action="native",
                             row_deletable=True,
                             style_table={"overflowY": "scroll"},
@@ -160,35 +243,19 @@ app.layout = html.Div(
                         ),
                         dcc.Store(id="cache", data=labels),
                         html.Div(id="row", hidden=True, children=None),
-                    ],
-                    className="six columns",
-                ),
-            ],
-            className="row",
-        ),
-        html.H4("How to use this app (see below)"),
-        dcc.Markdown(
-            dedent(
-                """
-    Hover over objects to highlight their properties in the table,
-    select cell in table to highlight object in image, or
-    filter objects in the table to display a subset of objects.
-
-    Learn more about [DataTable filtering syntax](https://dash.plot.ly/datatable/filtering)
-    for selecting ranges of properties.
-    """
+                    ]
+                )
             )
         ),
-        html.Img(
-            id="help",
-            src="assets/properties.gif",
-            width="80%",
-            style={
-                "border": "2px solid black",
-                "display": "block",
-                "margin-left": "auto",
-                "margin-right": "auto",
-            },
+    ]
+)
+
+app.layout = html.Div(
+    [
+        header,
+        dbc.Container(
+            [dbc.Row([dbc.Col(image_card, md=5), dbc.Col(table_card, md=7)])],
+            fluid=True,
         ),
     ]
 )
@@ -261,6 +328,29 @@ def highlight_filter(indices, cell_index, data, current_labels, previous_row):
         new_labels *= mask
     fig = image_with_contour(img, new_labels, mode=None)
     return [fig, new_labels, previous_row]
+
+
+@app.callback(
+    Output("modal", "is_open"),
+    [Input("howto-open", "n_clicks"), Input("howto-close", "n_clicks")],
+    [State("modal", "is_open")],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
+
+# we use a callback to toggle the collapse on small screens
+@app.callback(
+    Output("navbar-collapse", "is_open"),
+    [Input("navbar-toggler", "n_clicks")],
+    [State("navbar-collapse", "is_open")],
+)
+def toggle_navbar_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
 
 
 if __name__ == "__main__":
