85fe047057979b1c6168483b469468bce9bbb67f
surchs <sebastian.urchs@gmail.com>
2020-11-09 16:17:33 -0500

    CircleCI and better hover - removed hoverinfo for background image - made the content of the hover dependent on the selected columns in the datatable - remove current label cache. Is not necessary because relevant information is available from table data - fixed bug where contour was drawn based on row index rather than label. - updated requirements.txt to pin CircleCI compatible versions (thanks @jonmmease) - removed typecheck in image_with_contour since we supply the PIL objects

--- apps/dash-label-properties/app.py
+++ apps/dash-label-properties/app.py
@@ -47,29 +47,33 @@ prop_table = measure.regionprops_table(
 table = pd.DataFrame(prop_table)
 # Format the Table columns
 columns = [
-    {"name": label_name, "id": label_name}
+    {"name": label_name, "id": label_name, "selectable": True}
     if precision is None
     else {
         "name": label_name,
         "id": label_name,
         "type": "numeric",
         "format": Format(precision=precision),
+        "selectable": True,
     }
     for label_name, precision in zip(prop_names, (None, None, 4, 4, None, 3))
 ]
+# Select the columns that are selected when the app starts
+initial_columns = ["label", "area"]
+
 img = img_as_ubyte(color.gray2rgb(img))
 img = PIL.Image.fromarray(img)
 
 
-def image_with_contour(img, active_labels, data_table, mode="lines", shape=None):
+def image_with_contour(
+    img, active_labels, data_table, active_columns, mode="lines", shape=None
+):
     """
     Figure with contour plot of labels superimposed on background image.
 
     Parameters
     ----------
-    img : URL, dataURI or ndarray
-        Background image. If a numpy array, it is transformed into a PIL
-        Image object.
+    img : PIL Image object.
     active_labels : list
         the currently visible labels in the datatable
     data_table : pandas.DataFrame
@@ -77,16 +81,6 @@ def image_with_contour(img, active_labels, data_table, mode="lines", shape=None)
     shape: tuple, optional
         Shape of the arrays, to be provided if ``img`` is not a numpy array.
     """
-    try:
-        sh_y, sh_x = shape if shape is not None else img.shape
-    except AttributeError:
-        print(
-            """the shape of the image must be provided with the
-                 ``shape`` parameter if ``img`` is not a numpy array"""
-        )
-    if type(img) == np.ndarray:
-        img = img_as_ubyte(color.gray2rgb(img))
-        img = PIL.Image.fromarray(img)
     # Filter the label array based on the currently visible labels
     mask = np.in1d(label_array.ravel(), active_labels, invert=True).reshape(
         label_array.shape
@@ -96,7 +90,7 @@ def image_with_contour(img, active_labels, data_table, mode="lines", shape=None)
     print("mode is", mode)
     opacity = 0.4 if mode is None else 1
 
-    fig = px.imshow(img, binary_string=True, binary_backend="jpg")
+    fig = px.imshow(img, binary_string=True, binary_backend="jpg",)
     # Overlay the current labels
     _, hex_list = zip(*colors.PLOTLY_SCALES["Viridis"])
     label_data_array = xr.DataArray(np.flipud(masked_labels))
@@ -108,35 +102,39 @@ def image_with_contour(img, active_labels, data_table, mode="lines", shape=None)
 
     fig.add_image(colormodel="rgba", source=label_multi_str, opacity=opacity)
 
+    # Disable the hover information for figure so far
+    fig.update_traces(hoverinfo="skip", hovertemplate=None)
+
     # Overlay the colored label info with a hidden (opaque) scatter trace to display hoverinfo
     # see also: https://scikit-image.org/docs/dev/auto_examples/segmentation/plot_regionprops.html#sphx-glr-auto-examples-segmentation-plot-regionprops-py
-    overlay_columns = data_table.columns[1:]
     for rid, row in data_table.iterrows():
         label = row.label
         contour = measure.find_contours(label_array == label, 0.5)[0]
         y, x = contour.T
-        hoverinfo = "<br>".join(
-            [
-                f"{prop_name}: {prop_val:.3f}"
-                if np.issubdtype(type(prop_val), "float")
-                else f"{prop_name}: {prop_val:d}"
-                for prop_name, prop_val in row[overlay_columns].iteritems()
-            ]
-        )
-        fig.add_trace(
-            go.Scatter(
-                x=x,
-                y=y,
-                opacity=0,
-                name=label,
-                mode="lines",
-                line=dict(color="grey"),
-                fill="toself",
-                customdata=[label] * len(x),
-                showlegend=False,
-                hovertemplate=hoverinfo,
-                hoveron="points+fills",
+        hoverinfo = (
+            "<br>".join(
+                [
+                    # All numbers are passed as floats. If there are no decimals, cast to int for visibility
+                    f"{prop_name}: {f'{int(prop_val):d}' if prop_val.is_integer() else f'{prop_val:.3f}'}"
+                    if np.issubdtype(type(prop_val), "float")
+                    else f"{prop_name}: {prop_val}"
+                    for prop_name, prop_val in row[active_columns].iteritems()
+                ]
             )
+            + " <extra></extra>"  # Add this to remove the trace name. See e.g. https://plotly.com/python/reference/#scatter-hovertemplate
+        )
+        fig.add_scatter(
+            x=x,
+            y=y,
+            opacity=0,
+            name=label,
+            mode="lines",
+            line=dict(color="#3D9970"),
+            fill="toself",
+            customdata=[label] * len(x),
+            showlegend=False,
+            hovertemplate=hoverinfo,
+            hoveron="points+fills",
         )
 
     # Remove axis ticks and labels and have the image fill the container
@@ -258,7 +256,7 @@ image_card = dbc.Card(
                     dcc.Graph(
                         id="graph",
                         figure=image_with_contour(
-                            img, current_labels, table, mode=None
+                            img, current_labels, table, initial_columns, mode=None
                         ),
                     )
                 )
@@ -280,11 +278,12 @@ table_card = dbc.Card(
                             data=table.to_dict("records"),
                             filter_action="native",
                             row_deletable=True,
+                            column_selectable="multi",
+                            selected_columns=initial_columns,
                             style_table={"overflowY": "scroll"},
                             fixed_rows={"headers": False, "data": 0},
                             style_cell={"width": "85px"},
                         ),
-                        dcc.Store(id="cache-label-list", data=current_labels),
                         html.Div(id="row", hidden=True, children=None),
                     ]
                 )
@@ -325,20 +324,17 @@ def higlight_row(string):
 
 
 @app.callback(
-    [
-        Output("graph", "figure"),
-        Output("cache-label-list", "data"),
-        Output("row", "children"),
-    ],
+    [Output("graph", "figure"), Output("row", "children"),],
     [
         Input("table-line", "derived_virtual_indices"),
         Input("table-line", "active_cell"),
         Input("table-line", "data"),
+        Input("table-line", "selected_columns"),
     ],
-    [State("cache-label-list", "data"), State("row", "children"),],
+    [State("row", "children"),],
     prevent_initial_call=True,
 )
-def highlight_filter(indices, cell_index, data, active_labels, previous_row):
+def highlight_filter(indices, cell_index, data, active_columns, previous_row):
     """
     Updates figure and labels array when a selection is made in the table.
 
@@ -348,10 +344,15 @@ def highlight_filter(indices, cell_index, data, active_labels, previous_row):
     When the set of filtered labels changes, or when a row is deleted.
     """
     _table = pd.DataFrame(data)
+    filtered_labels = _table.loc[indices, "label"].values
+    filtered_table = _table.query("label in @filtered_labels")
+    fig = image_with_contour(
+        img, filtered_labels, filtered_table, active_columns, mode=None
+    )
+
     if cell_index and cell_index["row"] != previous_row:
-        label = indices[cell_index["row"]] + 1
+        label = filtered_labels[cell_index["row"]]
         mask = (label_array == label).astype(np.float)
-        fig = image_with_contour(img, active_labels, _table, mode=None)
         # Add the outline of the selected label as a contour to the figure
         fig.add_contour(
             z=mask,
@@ -362,10 +363,9 @@ def highlight_filter(indices, cell_index, data, active_labels, previous_row):
             opacity=0.8,
             hoverinfo="skip",
         )
-        return [fig, active_labels, cell_index["row"]]
-    filtered_labels = _table.loc[indices, "label"].values
-    fig = image_with_contour(img, filtered_labels, _table, mode=None)
-    return [fig, filtered_labels, previous_row]
+        return [fig, cell_index["row"]]
+
+    return [fig, previous_row]
 
 
 @app.callback(
@@ -392,4 +392,4 @@ def toggle_navbar_collapse(n, is_open):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
