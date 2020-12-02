bb6a442dd3e0ece0e178036db59329a5f0969752
Jon Mease <jon.mease.dev@gmail.com>
2020-12-02 15:48:57 -0500

    Add MAPBOX token string
    Former-commit-id: 7d4f235f5f09b9e847a19b4686637f22338b4741

--- apps/dash-holoviews-taxi/mapbox_token.py
+++ apps/dash-holoviews-taxi/mapbox_token.py
@@ -4,13 +4,5 @@ import os
 def get_mapbox_token():
     if os.environ.get("MAPBOX_TOKEN", None):
         return os.environ["MAPBOX_TOKEN"]
-    elif os.path.exists(".mapbox_token"):
-        with open(".mapbox_token", "rt") as f:
-            return f.read().strip()
     else:
-        raise ValueError(
-            "Could not load mapbox token\n"
-            "Either set the MAPBOX_TOKEN environment variable to your token string \n"
-            "or create a file named .mapbox_token containing your token in the\n"
-            "top-level project directory"
-        )
+        return "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG50amg0dnJieG4ifQ.Zme1-Uzoi75IaFbieBDl3A"
