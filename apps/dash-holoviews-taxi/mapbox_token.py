3600400b825db0caff10198c5200a6db3e0f7977
Jon Mease <jon.mease.dev@gmail.com>
2020-12-02 17:25:20 -0500

    Remove unused dataset and token scripts
    Former-commit-id: ec4eb8a939425f02552db5a323621ce2cdffc599

--- apps/dash-holoviews-taxi/mapbox_token.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def get_mapbox_token():
-    if os.environ.get("MAPBOX_TOKEN", None):
-        return os.environ["MAPBOX_TOKEN"]
-    else:
-        return "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG50amg0dnJieG4ifQ.Zme1-Uzoi75IaFbieBDl3A"
