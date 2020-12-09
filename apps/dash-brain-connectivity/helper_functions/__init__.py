e2e20b93faf3e0493452ffae5f2e966426b0a143
surchs <sebastian.urchs@gmail.com>
2020-12-09 09:31:29 -0500

    run black again

--- apps/dash-brain-connectivity/helper_functions/__init__.py
+++ apps/dash-brain-connectivity/helper_functions/__init__.py
@@ -1,1 +1,8 @@
-from .connectivity import compute_connectivity_matrix, make_circos, make_heatmap, thr_conn_mat, compute_region_table, add_region_shape
+from .connectivity import (
+    compute_connectivity_matrix,
+    make_circos,
+    make_heatmap,
+    thr_conn_mat,
+    compute_region_table,
+    add_region_shape,
+)
