92ddc138a20f5656154dffab7c138ca426fc7bd8
xhlulu <xhlperso@gmail.com>
2021-02-17 12:31:00 -0500

    apply black

--- apps/dbc-soccer-analytics/TrackingDataPreProcessing.py
+++ apps/dbc-soccer-analytics/TrackingDataPreProcessing.py
@@ -1,57 +1,59 @@
 import pandas as pd
 import numpy as np
 
-#raw_df = pd.read_csv(cv_file, names=header_list, error_bad_lines=False, dtype=str)
-raw_df1 = pd.read_csv('data/Sample_Game_2_RawTrackingData_Away_Team.csv')
-#sample every 5th row
+# raw_df = pd.read_csv(cv_file, names=header_list, error_bad_lines=False, dtype=str)
+raw_df1 = pd.read_csv("data/Sample_Game_2_RawTrackingData_Away_Team.csv")
+# sample every 5th row
 raw_df1 = raw_df1.iloc[::7, :]
 
-raw_df2 = pd.read_csv('data/Sample_Game_2_RawTrackingData_Home_Team.csv')
+raw_df2 = pd.read_csv("data/Sample_Game_2_RawTrackingData_Home_Team.csv")
 raw_df2 = raw_df2.iloc[::7, :]
 
 column = 1
-df = pd.DataFrame(columns=['half', 'frame', 'time', 'x', 'y'])
+df = pd.DataFrame(columns=["half", "frame", "time", "x", "y"])
 # x range needs adjusted depending on how many columns there are. Should really calculate this not eyeball items
 # and do it manually. But hey it's just for a demo not ongoing
-for x in range(0,13):
+for x in range(0, 13):
     column = column + 2
-    df_temp= raw_df1.iloc[: , [0,1,2, column, column+1]].copy()
-    df_temp.columns=['half', 'frame', 'time', 'x','y']
-    df_temp['jersey_number']=raw_df1.columns[column]
+    df_temp = raw_df1.iloc[:, [0, 1, 2, column, column + 1]].copy()
+    df_temp.columns = ["half", "frame", "time", "x", "y"]
+    df_temp["jersey_number"] = raw_df1.columns[column]
     df = pd.concat([df, df_temp]).reset_index(drop=True)
-df['team'] = 'Away'
-df.loc[df['jersey_number'] == '0', 'team'] = 'Ball'
-df.loc[df['x'].isna(), 'x'] = None
-df.loc[df['y'].isna(), 'y'] = None
-df = df[df['x'].notna()]
-df.drop(df.loc[df['half']=='Period'].index, inplace=True)
+df["team"] = "Away"
+df.loc[df["jersey_number"] == "0", "team"] = "Ball"
+df.loc[df["x"].isna(), "x"] = None
+df.loc[df["y"].isna(), "y"] = None
+df = df[df["x"].notna()]
+df.drop(df.loc[df["half"] == "Period"].index, inplace=True)
 
 column = 1
-df2 = pd.DataFrame(columns=['half', 'frame', 'time', 'x', 'y'])
-for x in range(0,12):
+df2 = pd.DataFrame(columns=["half", "frame", "time", "x", "y"])
+for x in range(0, 12):
     column = column + 2
-    df_temp2= raw_df2.iloc[: , [0,1,2, column, column+1]].copy()
-    df_temp2.columns=['half', 'frame', 'time', 'x','y']
-    df_temp2['jersey_number']=raw_df2.columns[column]
+    df_temp2 = raw_df2.iloc[:, [0, 1, 2, column, column + 1]].copy()
+    df_temp2.columns = ["half", "frame", "time", "x", "y"]
+    df_temp2["jersey_number"] = raw_df2.columns[column]
     df2 = pd.concat([df2, df_temp2]).reset_index(drop=True)
-df2['team'] = 'Home'
-df2.loc[df2['x'].isna(), 'x'] = .5
-df2.loc[df2['y'].isna(), 'y'] = .5
-df2 = df2[df2['x'].notna()]
-df2.drop(df2.loc[df2['half']=='Period'].index, inplace=True)
+df2["team"] = "Home"
+df2.loc[df2["x"].isna(), "x"] = 0.5
+df2.loc[df2["y"].isna(), "y"] = 0.5
+df2 = df2[df2["x"].notna()]
+df2.drop(df2.loc[df2["half"] == "Period"].index, inplace=True)
 
 df = df.iloc[1:]
-df['frame'] = df['frame'].apply(pd.to_numeric, errors='coerce')
-df = df.sort_values(by=['frame'])
+df["frame"] = df["frame"].apply(pd.to_numeric, errors="coerce")
+df = df.sort_values(by=["frame"])
 
 df2 = df2.iloc[1:]
-df2['frame'] = df2['frame'].apply(pd.to_numeric, errors='coerce')
-df2= df2.sort_values(by=['frame'])
+df2["frame"] = df2["frame"].apply(pd.to_numeric, errors="coerce")
+df2 = df2.sort_values(by=["frame"])
 
 df_export = pd.concat([df, df2]).reset_index(drop=True)
-df_export= df_export.sort_values(by=['frame'])
-df_export['time'] = df_export['time'].apply(pd.to_numeric, errors='coerce')
-df_export['time'] = df_export['time'].div(60).round(4)
-export_file_name = input('Please enter a name for the file to be exported (ending with .csv): ')
-export_file_name = 'data/'+export_file_name
+df_export = df_export.sort_values(by=["frame"])
+df_export["time"] = df_export["time"].apply(pd.to_numeric, errors="coerce")
+df_export["time"] = df_export["time"].div(60).round(4)
+export_file_name = input(
+    "Please enter a name for the file to be exported (ending with .csv): "
+)
+export_file_name = "data/" + export_file_name
 df_export.to_csv(export_file_name, index=False)
--- apps/dbc-soccer-analytics/initial_figures.py
+++ apps/dbc-soccer-analytics/initial_figures.py
@@ -7,7 +7,7 @@ def initial_figure_simulator():
     fig = px.scatter(x=[0, 0, 105, 105], y=[69, -2, 69, -2])
     fig.update_layout(xaxis=dict(range=[-4, 110]))
     fig.update_layout(yaxis=dict(range=[-4, 72]))
-    fig.update_traces(marker=dict(color='white', size=6))
+    fig.update_traces(marker=dict(color="white", size=6))
 
     # Remove side color scale and hide zero and gridlines
     fig.update_layout(
@@ -22,15 +22,16 @@ def initial_figure_simulator():
     # Disable axis ticks and labels
     fig.update_xaxes(showticklabels=False)
     fig.update_yaxes(showticklabels=False)
-    fig.update_xaxes(title_text='')
-    fig.update_yaxes(title_text='')
+    fig.update_xaxes(title_text="")
+    fig.update_yaxes(title_text="")
     fig.update_layout(margin=dict(l=80, r=80, b=10, t=20))
-    fig.update_layout(modebar=dict(bgcolor='rgba(0, 0, 0, 0)'))
-    image_file = 'assets/Pitch.png'
+    fig.update_layout(modebar=dict(bgcolor="rgba(0, 0, 0, 0)"))
+    image_file = "assets/Pitch.png"
     image_path = os.path.join(os.getcwd(), image_file)
 
     # Import and use pre-fabricated football pitch image
     from PIL import Image
+
     img = Image.open(image_path)
 
     fig.add_layout_image(
@@ -44,34 +45,33 @@ def initial_figure_simulator():
             sizey=71,
             sizing="stretch",
             opacity=0.7,
-            layer="below")
+            layer="below",
+        )
     )
 
-    fig.update_yaxes(
-                scaleanchor="x",
-                scaleratio=1)
+    fig.update_yaxes(scaleanchor="x", scaleratio=1)
 
     fig.update_layout(autosize=True)
 
-    fig.update_layout(template='plotly_dark',
-                      xaxis=dict(
-                          showgrid=False,
-                          showticklabels=False),
-                      plot_bgcolor='rgba(0, 0, 0, 0)',
-                      paper_bgcolor='rgba(0, 0, 0, 0)'
-                      )
+    fig.update_layout(
+        template="plotly_dark",
+        xaxis=dict(showgrid=False, showticklabels=False),
+        plot_bgcolor="rgba(0, 0, 0, 0)",
+        paper_bgcolor="rgba(0, 0, 0, 0)",
+    )
     return fig
+
 
 # Create initial placeholder figure for event plot
 def initial_figure_events():
     fig = px.scatter(x=[0], y=[0])
 
-    fig.update_traces(marker=dict(color='white', size=6))
+    fig.update_traces(marker=dict(color="white", size=6))
     fig.update_layout(yaxis=dict(range=[-3650, 3650]))
     fig.update_layout(xaxis=dict(range=[-5300, 5300]))
     fig.update_layout(margin=dict(l=10, r=100, b=10, t=45))
 
-    fig.update_layout(modebar=dict(bgcolor='rgba(0, 0, 0, 0)'))
+    fig.update_layout(modebar=dict(bgcolor="rgba(0, 0, 0, 0)"))
 
     # Remove side color scale and hide zero and gridlines
     fig.update_layout(
@@ -80,27 +80,25 @@ def initial_figure_events():
         yaxis=dict(showgrid=False, zeroline=False),
     )
 
-    fig.update_layout(template='plotly_dark',
-                      xaxis=dict(
-                          showgrid=False,
-                          showticklabels=False),
-                      plot_bgcolor='rgba(0, 0, 0, 0)',
-                      paper_bgcolor='rgba(0, 0, 0, 0)'
-                      )
+    fig.update_layout(
+        template="plotly_dark",
+        xaxis=dict(showgrid=False, showticklabels=False),
+        plot_bgcolor="rgba(0, 0, 0, 0)",
+        paper_bgcolor="rgba(0, 0, 0, 0)",
+    )
 
     # Disable axis ticks and labels
     fig.update_xaxes(showticklabels=False)
     fig.update_yaxes(showticklabels=False)
-    fig.update_xaxes(title_text='')
-    fig.update_yaxes(title_text='')
+    fig.update_xaxes(title_text="")
+    fig.update_yaxes(title_text="")
     fig.update_xaxes(fixedrange=True)
-    fig.update_yaxes(
-                scaleanchor="x",
-                scaleratio=1)
+    fig.update_yaxes(scaleanchor="x", scaleratio=1)
     fig.update_layout(margin=dict(l=10, r=30, b=30, t=30), autosize=True)
-    image_file = 'assets/Pitch.png'
+    image_file = "assets/Pitch.png"
 
     from PIL import Image
+
     img = Image.open(image_file)
     fig.add_layout_image(
         dict(
@@ -113,37 +111,52 @@ def initial_figure_events():
             sizey=7340,
             sizing="stretch",
             opacity=0.8,
-            layer="below")
+            layer="below",
+        )
     )
 
     return fig
+
 
 # Create initial placeholder figure for radar plot
 def initial_figure_radar():
     fig = go.Figure()
     r_values = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    fig.add_trace(go.Scatterpolar(
-        r=r_values,
-        theta= ['Set<br>Plays', 'Passes', 'Balls<br>Lost', 'Recoveries', 'Challenges', 'Shots', 'Interceptions', 'Crosses', 'Long<br>Balls', 'Free<br>Kicks'],
-        fill='toself',
-        opacity=.50
-    ))
+    fig.add_trace(
+        go.Scatterpolar(
+            r=r_values,
+            theta=[
+                "Set<br>Plays",
+                "Passes",
+                "Balls<br>Lost",
+                "Recoveries",
+                "Challenges",
+                "Shots",
+                "Interceptions",
+                "Crosses",
+                "Long<br>Balls",
+                "Free<br>Kicks",
+            ],
+            fill="toself",
+            opacity=0.50,
+        )
+    )
 
-    fig.update_layout(template='plotly_dark', plot_bgcolor='rgba(0, 0, 0, 0)',
-                      paper_bgcolor='rgba(0, 0, 0, 0)')
+    fig.update_layout(
+        template="plotly_dark",
+        plot_bgcolor="rgba(0, 0, 0, 0)",
+        paper_bgcolor="rgba(0, 0, 0, 0)",
+    )
 
-    fig.update_layout(modebar=dict(bgcolor='rgba(0, 0, 0, 0)'))
+    fig.update_layout(modebar=dict(bgcolor="rgba(0, 0, 0, 0)"))
 
     fig.update_layout(
         polar=dict(
-            bgcolor='#282828',
-            radialaxis=dict(
-                visible=True,
-                range=[0, 1],
-                showticklabels=False,
-            )),
-            showlegend=False
+            bgcolor="#282828",
+            radialaxis=dict(visible=True, range=[0, 1], showticklabels=False,),
+        ),
+        showlegend=False,
     )
     fig.update_layout(autosize=True)
     fig.update_layout(margin=dict(l=60, r=60, b=30, t=45))
