67bda20309b4b1006e6f50617e60ff7f0ae1448b
xhlulu <xinghan@plot.ly>
2021-03-29 14:56:11 -0400

    apply black

--- apps/dash-airfoil-design/app_components.py
+++ apps/dash-airfoil-design/app_components.py
@@ -4,39 +4,16 @@ import dash_html_components as html
 ### Operating things
 operating_slider_components = [
     html.Div(id="alpha_slider_output"),
-    dcc.Slider(
-        id="alpha_slider_input",
-        min=-20,
-        max=20,
-        step=1e-6,
-        value=8.755,
-    ),
+    dcc.Slider(id="alpha_slider_input", min=-20, max=20, step=1e-6, value=8.755,),
     html.Div(id="height_slider_output"),
-    dcc.Slider(
-        id="height_slider_input",
-        min=0,
-        max=1,
-        step=1e-6,
-        value=0
-    ),
+    dcc.Slider(id="height_slider_input", min=0, max=1, step=1e-6, value=0),
     dcc.Checklist(
         id="operating_checklist",
-        options=[
-            {
-                'label': " Ground Effect",
-                'value': "ground_effect"
-            }
-        ],
-        value=[]
+        options=[{"label": " Ground Effect", "value": "ground_effect"}],
+        value=[],
     ),
     html.Div(id="streamline_density_slider_output"),
-    dcc.Slider(
-        id="streamline_density_slider_input",
-        min=0.1,
-        max=5,
-        step=0.1,
-        value=1
-    ),
+    dcc.Slider(id="streamline_density_slider_input", min=0.1, max=5, step=0.1, value=1),
 ]
 
 ### Shape things
@@ -70,13 +47,15 @@ for side in sides:
             if n_kulfan_inputs_per_side == 3:
                 slider_init = [-0.107972, 0.061474, -0.055904][i]
 
-        kulfan_slider_components.extend([
-            html.Div(id=slider_id + "_output"),
-            dcc.Slider(
-                id=slider_id + "_input",
-                min=slider_min,
-                max=slider_max,
-                step=1e-6,  # continuous
-                value=slider_init
-            )
-        ])
+        kulfan_slider_components.extend(
+            [
+                html.Div(id=slider_id + "_output"),
+                dcc.Slider(
+                    id=slider_id + "_input",
+                    min=slider_min,
+                    max=slider_max,
+                    step=1e-6,  # continuous
+                    value=slider_init,
+                ),
+            ]
+        )
