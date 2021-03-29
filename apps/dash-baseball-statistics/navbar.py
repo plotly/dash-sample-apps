545b324f95ef33fecb6e4ee237b4933f713bfb43
xhlulu <xinghan@plot.ly>
2021-03-29 17:34:42 -0400

    Fix URL paths
    Former-commit-id: e190010aefcd50adfe9c5b5161ca3266c69817f2

--- apps/dash-baseball-statistics/navbar.py
+++ apps/dash-baseball-statistics/navbar.py
@@ -1,17 +1,23 @@
 # Import Bootstrap from Dash
+import os
+
 import dash_bootstrap_components as dbc
 
+
+app_name = os.getenv("DASH_APP_PATH", "/dash-baseball-statistics")
 
 # Navigation Bar fucntion
 def Navbar():
     navbar = dbc.NavbarSimple(
         children=[
-            dbc.NavItem(dbc.NavLink("Team Analysis", href="/team")),
-            dbc.NavItem(dbc.NavLink("Batting Analysis", href="/player")),
-            dbc.NavItem(dbc.NavLink("Pitching/Fielding Analysis", href="/field")),
+            dbc.NavItem(dbc.NavLink("Team Analysis", href=f"{app_name}/team")),
+            dbc.NavItem(dbc.NavLink("Batting Analysis", href=f"{app_name}/player")),
+            dbc.NavItem(
+                dbc.NavLink("Pitching/Fielding Analysis", href=f"{app_name}/field")
+            ),
         ],
         brand="Home",
-        brand_href="/",
+        brand_href=f"{app_name}",
         sticky="top",
         color="light",
         dark=False,
