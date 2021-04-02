1c8d0916a5f6db1fa8f9138f1ca5bc68451fae3b
xhlulu <xhlperso@gmail.com>
2021-04-02 18:33:00 -0400

    apply black
    Former-commit-id: 92b1c04fb3a1a02e84c02cc6e8ba45b31b18c3e4

--- apps/dash-pileup-demo/layout_helper.py
+++ apps/dash-pileup-demo/layout_helper.py
@@ -6,25 +6,20 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 
-def run_standalone_app(
-        layout,
-        callbacks,
-        header_colors,
-        filename
-):
+def run_standalone_app(layout, callbacks, header_colors, filename):
     """Run demo app (tests/dashbio_demos/*/app.py) as standalone app."""
     app = dash.Dash(__name__)
     app.scripts.config.serve_locally = True
     # Handle callback to component with id "fullband-switch"
-    app.config['suppress_callback_exceptions'] = True
+    app.config["suppress_callback_exceptions"] = True
 
     # Get all information from filename
-    app_name = os.getenv('DASH_APP_NAME', '')
-    if app_name == '':
+    app_name = os.getenv("DASH_APP_NAME", "")
+    if app_name == "":
         app_name = os.path.basename(os.path.dirname(filename))
-    app_name = app_name.replace('dash-', '')
+    app_name = app_name.replace("dash-", "")
 
-    app_title = "{}".format(app_name.replace('-', ' ').title())
+    app_title = "{}".format(app_name.replace("-", " ").title())
 
     # Assign layout
     app.layout = app_page_layout(
@@ -42,71 +37,67 @@ def run_standalone_app(
     return app
 
 
-def app_page_layout(page_layout,
-                    app_title="Dash Bio App",
-                    app_name="",
-                    light_logo=True,
-                    standalone=False,
-                    bg_color="#506784",
-                    font_color="#F3F6FA"):
+def app_page_layout(
+    page_layout,
+    app_title="Dash Bio App",
+    app_name="",
+    light_logo=True,
+    standalone=False,
+    bg_color="#506784",
+    font_color="#F3F6FA",
+):
     return html.Div(
-        id='main_page',
+        id="main_page",
         children=[
-            dcc.Location(id='url', refresh=False),
+            dcc.Location(id="url", refresh=False),
             html.Div(
-                id='app-page-header',
+                id="app-page-header",
                 children=[
                     html.A(
-                        id='dashbio-logo', children=[
+                        id="dashbio-logo",
+                        children=[
                             html.Img(
-                                src='data:image/png;base64,{}'.format(
+                                src="data:image/png;base64,{}".format(
                                     base64.b64encode(
                                         open(
-                                            './assets/plotly-dash-bio-logo.png', 'rb'
+                                            "./assets/plotly-dash-bio-logo.png", "rb"
                                         ).read()
                                     ).decode()
                                 )
-                            )],
-                        href="/Portal" if standalone else "/dash-bio"
-                    ),
-                    html.H2(
-                        app_title
-                    ),
-
-                    html.A(
-                        id='gh-link',
-                        children=[
-                            'View on GitHub'
+                            )
                         ],
+                        href="/Portal" if standalone else "/dash-bio",
+                    ),
+                    html.H2(app_title),
+                    html.A(
+                        id="gh-link",
+                        children=["View on GitHub"],
                         href="http://github.com/plotly/dash-bio/"
                         "blob/master/tests/dashbio_demos/dash-{}/app.py".format(
                             app_name
                         ),
-                        style={'color': 'white' if light_logo else 'black',
-                               'border': 'solid 1px white' if light_logo else 'solid 1px black'}
+                        style={
+                            "color": "white" if light_logo else "black",
+                            "border": "solid 1px white"
+                            if light_logo
+                            else "solid 1px black",
+                        },
                     ),
-
                     html.Img(
-                        src='data:image/png;base64,{}'.format(
+                        src="data:image/png;base64,{}".format(
                             base64.b64encode(
                                 open(
-                                    './assets/GitHub-Mark-{}64px.png'.format(
-                                        'Light-' if light_logo else ''
+                                    "./assets/GitHub-Mark-{}64px.png".format(
+                                        "Light-" if light_logo else ""
                                     ),
-                                    'rb'
+                                    "rb",
                                 ).read()
                             ).decode()
                         )
-                    )
+                    ),
                 ],
-                style={
-                    'background': bg_color,
-                    'color': font_color,
-                }
+                style={"background": bg_color, "color": font_color,},
             ),
-            html.Div(
-                id='app-page-content',
-                children=page_layout
-            )
+            html.Div(id="app-page-content", children=page_layout),
         ],
     )
