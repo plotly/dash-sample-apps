aa5474c7f94702d5cd9037b3b9e099121f644294
xhlulu <xhlperso@gmail.com>
2021-04-19 13:48:43 -0400

    Move gulp and package.json

--- apps/dash-port-analytics/gulp/gulpfile.js
+++ apps/dash-port-analytics/gulp/gulpfile.js
@@ -5,13 +5,13 @@ const sass = require('gulp-sass');
 // compile scss into css
 function style() {
   // outputStyle props: nested, expanded, compact, compressed
-  return gulp.src('./scss/**/*.scss')
+  return gulp.src('../scss/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest('../assets/css'));
 };
 
 function watch(){
-  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('../scss/**/*.scss', style);
 };
 
 exports.style = style;
