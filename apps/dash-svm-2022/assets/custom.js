a62417751edf270de4030a9d616fea5abc03edff
T.ii <39115400+IcToxi@users.noreply.github.com>
2022-04-07 00:37:04 -0700

    Create custom.js

---
+++ apps/dash-svm-2022/assets/custom.js
@@ -0,0 +1,6 @@
+window.dash_clientside = Object.assign({}, window.dash_clientside, {
+    clientside: {
+        canvas_toggle: (n, c) => c === '#509188' ? '#FF7070' : '#509188',
+        datasets_params_store: (...rest) => rest[0] > 0 && Object.values(rest).splice(1)
+    }
+});
