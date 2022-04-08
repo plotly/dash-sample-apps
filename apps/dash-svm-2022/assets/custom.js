0beebce28b3eceee743d6837973cf3613c70deab
T.ii <39115400+IcToxi@users.noreply.github.com>
2022-04-07 20:09:23 -0700

    Update custom.js

--- apps/dash-svm-2022/assets/custom.js
+++ apps/dash-svm-2022/assets/custom.js
@@ -1,7 +1,6 @@
 window.dash_clientside = Object.assign({}, window.dash_clientside, {
     clientside: {
         canvas_toggle: (n, c) => c === '#509188' ? '#FF7070' : '#509188',
-        datasets_params_store: (...rest) => rest[0] > 0 && Object.values(rest).splice(1)
-        // I actually want it to have a cache right after initialization.
+        datasets_params_store: (...rest) => rest[0] > 0 ? Object.values(rest).splice(1) : window.dash_clientside.no_update
     }
 });
