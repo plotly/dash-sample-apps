4ab7c8b277b447a291b90e3dbc78f2e12259463b
admin <51248046+danton267@users.noreply.github.com>
2022-05-25 14:46:53 +0100

    final

--- apps/dash-wind-streaming/constants.py
+++ apps/dash-wind-streaming/constants.py
@@ -2,7 +2,6 @@ import os
 
 app_color = {"graph_bg": "#082255", "graph_line": "#007ACE"}
 
-# DB_FILE = pathlib.Path(__file__).resolve().parent.joinpath("./data/wind-data.db").resolve()
 DB_FILE = "data/wind-data.db"
 
 GRAPH_INTERVAL = int(os.environ.get("GRAPH_INTERVAL", 5000))
