086285ec99c609e0d57a1b8077efb0b5c945982d
A Beresford <aberesf@gmail.com>
2024-01-31 12:37:00 -0500

    test1

--- apps/dash-clinical-analytics/app.py
+++ apps/dash-clinical-analytics/app.py
@@ -55,7 +55,7 @@ day_list = [
     "Sunday",
 ]
 
-check_in_duration = df["Check-In Time"].describe(datetime_is_numeric=True)
+check_in_duration = df["Check-In Time"].describe()
 
 # Register all departments for callbacks
 all_departments = df["Department"].unique().tolist()
